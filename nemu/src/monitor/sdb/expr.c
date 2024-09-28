08df6c31a36836105bbdbce7146d2aaa032c2fb6
tracer-ysyx <tracer@ysyx.org>
2024-09-28 12:02:09 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:09 up 28 days, 22:38,  1 user,  load average: 0.23, 0.15, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -211,7 +211,7 @@ int main_op_pos(int p,int q){
     if (tokens[i].type == TK_DEC) {continue;}
     else if (tokens[i].type=='('){
         // num of parentheses must be equal
-        i = find_parentheses_match(p+1,q);
+        i = find_parentheses_match(i+1,q);
     }else if (op_valid(tokens[i].type)){
        op_pos = (op_pos<0)?i:op_order(op_pos,i);
     }else{
