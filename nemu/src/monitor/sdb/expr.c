722aa3d0f281bdab08ced52d371082e06bc802a8
tracer-ysyx <tracer@ysyx.org>
2024-09-29 23:01:50 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:50 up 30 days,  9:38,  1 user,  load average: 0.17, 0.05, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -348,11 +348,12 @@ int eval(int p,int q){
     int n = sprintf(str,"%d",res); 
     // TODO: support HEX
     strncpy(tokens[expr_end].str,str,n);
+    for (int i=p;i<=expr_end;i++){
+        tokens[i].type = TK_NOTYPE;
+        memset(tokens[i].str,0,32*sizeof(char));
+    }
     tokens[expr_end].type = TK_DEC;
     // replace [p,expr) with TK_NOTYPE
-    for (int i=p;i<expr_end;i++){
-        tokens[i].type = TK_NOTYPE;
-    }
     return (expr_end>q)?res:eval(expr_end,q);
   } else {
     // binary operator
