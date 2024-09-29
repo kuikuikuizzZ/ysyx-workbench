76aafecc089b14062f361e50bce864944b301219
tracer-ysyx <tracer@ysyx.org>
2024-09-29 21:17:44 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:44 up 30 days,  7:53,  1 user,  load average: 0.13, 0.05, 0.06

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -102,7 +102,7 @@ static int cmd_x(char *args) {
           break;
         case 3: 
           res = vaddr_read(address+i,2);
-          p+=snprintf(p, 2, "%02x", res);  
+          p+=snprintf(p, 2, "%04x", res);  
           res = vaddr_read(address+i+2,1);
           p+=snprintf(p, 6, "%04x", res); 
           break; 
