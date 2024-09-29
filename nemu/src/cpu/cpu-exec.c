c692f271f32d6f69d9b67077a79e6ed307131f0a
tracer-ysyx <tracer@ysyx.org>
2024-09-30 00:25:29 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:25:29 up 30 days, 11:01,  1 user,  load average: 0.14, 0.05, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -33,7 +33,7 @@ static bool g_print_step = false;
 void device_update();
 
 // src/monitor/sdb/watchpoint.c
-extern int new_wp();
+extern int new_wp(word_t i,char* args);
 extern void delete_wp(int no);
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
@@ -42,6 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc)); 
+  new_wp(0,"ss");
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
