c6d1f3f095bb1495c91cd516d895d567a07cbb26
tracer-ysyx <tracer@ysyx.org>
2024-10-05 03:42:49 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:42:49 up 35 days, 14:19,  1 user,  load average: 0.12, 0.06, 0.07

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -96,7 +96,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -Werror -o /tmp/.expr");
+    int ret = system("gcc /tmp/.code.c -Werror='div-by-zero' -o /tmp/.expr");
     if (ret != 0) {
       memset(buf,0,buf_end+1);
       buf_end=0;
