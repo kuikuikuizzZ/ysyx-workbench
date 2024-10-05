fa66ceaee458cb7c894512d0a398fb92fb1db84d
tracer-ysyx <tracer@ysyx.org>
2024-10-05 09:40:59 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:40:59 up 35 days, 20:17,  1 user,  load average: 0.12, 0.06, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,7 +42,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc)); 
 #ifdef CONFIG_WATCHPOINT
-  if (wps_diff()){
+  if (nemu_state.state != NEMU_END && wps_diff()){
+
       nemu_state.state = NEMU_STOP;
   }
 #endif
