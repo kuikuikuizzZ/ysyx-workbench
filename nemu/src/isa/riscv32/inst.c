1f1ac4f4501334f9e1e9624fc4341db1953f7fb9
tracer-ysyx <tracer@ysyx.org>
2024-10-30 01:16:06 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:16:06 up 2 days,  7:19,  1 user,  load average: 0.07, 0.02, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -127,7 +127,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
 
   INSTPAT_END();
-  printf("pc: %x a0: %x a5 %x ra: %x\n",s->pc,R(10),R(15),R(1));
+  printf("pc: %x a0: %x a5 %x s0: %x, s1:%x\n",s->pc,R(10),R(15),R(8),R(9));
   R(0) = 0; // reset $zero to 0
   return 0;
 }
