909137afa7e1b94e6d6e9530046d44fd4bda1e41
tracer-ysyx <tracer@ysyx.org>
2024-10-29 20:54:03 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:03 up 2 days,  2:56,  1 user,  load average: 0.17, 0.07, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -48,7 +48,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     default: panic("unsupported type = %d", type);
   }
   if (s->pc==0x80000010)
-    printf("imm: %x, \n",*imm);
+    printf("imm: %x, %llx, %llx, %llx, %llx\n",*imm,(BITS(i, 31, 31) << 20),(BITS(i,30,21)<<1),(BITS(i,20,20)<<11),(BITS(i,19,12)<<12));
   return;
 }
 
