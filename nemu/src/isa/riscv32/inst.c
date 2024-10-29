5367cac85d7a784d53f4c779f8b31b14cfbc30a6
tracer-ysyx <tracer@ysyx.org>
2024-10-29 23:25:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:54 up 2 days,  5:28,  1 user,  load average: 0.08, 0.05, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_R,TYPE_J, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_R,TYPE_J, TYPE_U, TYPE_S, TYPE_B,
   TYPE_N, // none
 };
 
@@ -33,6 +33,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i,30,21)<<1) | (BITS(i,20,20)<<11) | (BITS(i,19,12)<<12); } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7)<<11)| (BITS(i, 30, 25)<<5)| (BITS(i, 11, 8)<<1); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -44,6 +45,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_R: src1R(); src2R();         break; 
     case TYPE_U:                   immU(); break;
     case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
@@ -104,8 +106,8 @@ static int decode_exec(Decode *s) {
 
 
   // B Type
-  INSTPAT("??????? ????? ????? 000 ????? 110 0011", beq     , S, s->dnpc=(src1==src2)?s->pc+imm:s->pc+4);
-  INSTPAT("??????? ????? ????? 001 ????? 110 0011", bne     , S, s->dnpc=(src1!=src2)?s->pc+imm:s->pc+4);
+  INSTPAT("??????? ????? ????? 000 ????? 110 0011", beq     , B, s->dnpc=(src1==src2)?s->pc+imm:s->pc+4);
+  INSTPAT("??????? ????? ????? 001 ????? 110 0011", bne     , B, s->dnpc=(src1!=src2)?s->pc+imm:s->pc+4);
 
 
 
