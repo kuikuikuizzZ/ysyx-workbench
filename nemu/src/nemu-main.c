8daa7f393a874e10aad5192b20a1a51db421e27f
tracer-ysyx <tracer@ysyx.org>
2024-10-30 11:32:13 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:13 up 2 days, 17:35,  1 user,  load average: 0.02, 0.07, 0.08

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
   #ifdef CONFIG_TARGET_AM
   am_init_monitor();
   #else
-    init_monitor(argc, argv);
+    // init_monitor(argc, argv);
   #endif
 
   // test expr in debug phase
