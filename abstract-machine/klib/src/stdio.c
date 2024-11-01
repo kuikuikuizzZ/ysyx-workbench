dcb6699dd1f2fe28d56439c3a0b6923a51433d3c
tracer-ysyx <tracer@ysyx.org>
2024-11-02 00:02:39 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:02:39 up 12:43,  1 user,  load average: 0.21, 0.09, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,38 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  int i = 0;
+  int rc = 0;
+  va_list argp;
+  va_start(argp,fmt);
+
+  for (i=0;fmt[i] != '\0';i++){
+      if (fmt[i]=='%'){
+        i++;
+        switch (fmt[i])
+        {
+        case 'd':
+          int *out_int = va_arg(argp,int *); 
+          char s[12];
+          itoa(*out_int,s);
+          putstr(s);
+          rc+= strlen(s);
+          break;
+        case 's':
+          char** out_string = va_arg(argp,char**);
+          putstr(*out_string);
+          rc+= strlen(*out_string);
+          break;
+        default:
+          break;
+        }
+      } else{
+         putch(fmt[i]);
+         rc++;
+      }
+  }
+  va_end(argp);
+  return rc;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,6 +29,32 @@ int atoi(const char* nptr) {
   return x;
 }
 
+void reverse(char s[]){
+  int n = strlen(s);
+  int i,j,c;
+  for (i=0,j=n-1;i<j;i++,j--){
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+void itoa(int n , char s[]){
+  int sign;
+  if ((sign=n)<0){
+    n = -n;
+  }
+  int i=0;
+  do{
+    s[i++] = n%10+'0';
+  }while ((n/=10)>0);
+  if (sign<0){
+    s[i++]='-';
+  }
+  s[i] = '\0';
+  reverse(s);
+}
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
