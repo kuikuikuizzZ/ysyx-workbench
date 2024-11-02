76fa194d2aa5cf50452c691e67d54448e34f2252
tracer-ysyx <tracer@ysyx.org>
2024-11-02 09:13:05 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:13:05 up 21:54,  1 user,  load average: 0.13, 0.07, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -57,12 +57,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-    // char * ss = (char*)s;
-    // for (int i=0;i<n;i++){
-    //   ss[i]=(char)c;
-    // }
-    // return s;
-    panic("Not implemented");
+    char * ss = (char*)s;
+    for (int i=0;i<n;i++){
+      ss[i]=(char)c;
+    }
+    return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -73,16 +72,22 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  // char *s1 = out;
-  // const char *s2 = in;
-  // strncpy(s1,s2,n);
-  // return s1;
-  panic("Not implemented");
-
+  char *s1 = out;
+  const char *s2 = in;
+  strncpy(s1,s2,n);
+  return s1;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  return 0;
+  const char *ss1 = s1;
+  const char *ss2 = s2;
+  int i = 0;
+  for (i=0;ss1[i]==ss2[i];i++){
+    if (i==(n-1)) {
+      return 0;
+    }
+  }
+  return ss1[i]-ss2[i];
 }
 
 #endif
