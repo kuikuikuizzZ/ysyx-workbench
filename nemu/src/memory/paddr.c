abc43e775dc87b0ccc0a047c9e9b7a2a7edab14d
tracer-ysyx <tracer@ysyx.org>
2024-11-06 15:26:12 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:26:12 up 5 days,  4:07,  1 user,  load average: 0.03, 0.04, 0.06

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -37,6 +37,10 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 }
 
 static void out_of_bound(paddr_t addr) {
+  #ifdef CONFIG_ITRACE
+   
+  #endif
+
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
