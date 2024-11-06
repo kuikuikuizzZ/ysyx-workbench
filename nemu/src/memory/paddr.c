3f0e8de5d0c274c0a515079eec8227b6de484a90
tracer-ysyx <tracer@ysyx.org>
2024-11-06 17:49:30 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:30 up 5 days,  6:30,  1 user,  load average: 0.08, 0.06, 0.05

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -46,8 +46,6 @@ static void out_of_bound(paddr_t addr) {
     char temp_buf[LOG_BUFSIZE];
     RingBuffer_get(log_buff,temp_buf,LOG_BUFSIZE);
     log_write("%s\n", temp_buf);
-    extern FILE *log_fp;
-    fflush(log_fp); 
   }
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
