5874776c34b3a5c131c47e0de241faff74913642
tracer-ysyx <tracer@ysyx.org>
2024-11-07 15:50:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:01 up 6 days,  4:31,  2 users,  load average: 0.03, 0.06, 0.01

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -6,27 +6,28 @@
 // #include <stdlib.h>
 
 int elf_check_file(Elf32_Ehdr *header){
-    return memcmp(header->e_ident, ELFMAG, SELFMAG) == 0;
+   
+    return memcmp(header->e_ident, ELFMAG, 3) == 0;
 }
 
 void init_ftrace(char* elf_file){
     FILE *fp = fopen(elf_file, "rb");
     if (fp == NULL) {
-    fprintf(stderr, "Unable to open '%s': %s\n", elf_file, strerror(errno));
-    return;
+        fprintf(stderr, "Unable to open '%s': %s\n", elf_file, strerror(errno));
+        return;
     }
 
     Elf32_Ehdr ehdr;
     if (fread(&ehdr, sizeof(ehdr), 1, fp) != 1) {
-    fprintf(stderr, "fread: %s\n", strerror(errno));
-    fclose(fp);
-    return;
+        fprintf(stderr, "fread: %s\n", strerror(errno));
+        fclose(fp);
+        return;
     }
 
     if (!elf_check_file(&ehdr)) {
-    fprintf(stderr, "'%s' is not an ELF file\n", elf_file);
-    fclose(fp);
-    return;
+        fprintf(stderr, "'%s' is not an ELF file\n", elf_file);
+        fclose(fp);
+        return;
     }
     /* Parse the rest of the ELF file here. */
 
