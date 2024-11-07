b6e9312150b13f927322cce2987122bad97ef71b
tracer-ysyx <tracer@ysyx.org>
2024-11-07 13:03:50 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:03:50 up 6 days,  1:44,  1 user,  load average: 0.10, 0.11, 0.04

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -65,7 +65,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE
-    log_write("R0x%x\tLength:%d\n",addr,len);
+    log_write("R0x%16x\tLength:%d\n",addr,len);
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
