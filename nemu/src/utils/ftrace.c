2aff9cb3ffb928cdc7a7a7955d7f6092bc9079fc
tracer-ysyx <tracer@ysyx.org>
2024-11-07 21:23:14 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:14 up 6 days, 10:04,  3 users,  load average: 0.19, 0.15, 0.11

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -114,8 +114,8 @@ ftrace_meta* read_func_meta(Elf32_Ehdr* ehdr,FILE *fp){
     func_meta *func_entries = calloc(sizeof(func_meta),n_symbol);
     
     
-    // string_table str_tb = strtb_entries[0];
-    printf("%s",content+1);
+    string_table str_tb = strtb_entries[0];
+    printf("%s",*str_tb.contents+1);
     /* read symbol name from string table, address from symbol table */
     int fm_index = 0;
     for(int i=0;i<n_symbol;i++){
