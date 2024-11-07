09f99cf19d1a333270fdbbee31390e7bebc54e25
tracer-ysyx <tracer@ysyx.org>
2024-11-08 01:59:15 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:59:15 up 6 days, 14:40,  3 users,  load average: 0.04, 0.03, 0.05

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -13,7 +13,7 @@
 # See the Mulan PSL v2 for more details.
 #**************************************************************************************/
 
-ifeq ($(CONFIG_FTRACE)$(CONFIG_IQUEUE),)
+ifeq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
 SRCS-BLACKLIST-y += src/utils/disasm.c
 else
 LIBCAPSTONE = tools/capstone/repo/libcapstone.so.5
