fbe9a9f834dda96d32f8fcd76f94258e85607367
tracer-ysyx <tracer@ysyx.org>
2024-11-07 16:04:24 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:04:24 up 6 days,  4:45,  2 users,  load average: 0.19, 0.09, 0.07

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -6,8 +6,6 @@
 // #include <stdlib.h>
 
 int elf_check_file(Elf32_Ehdr *header){
-    char temp[4];
-    memcpy(temp,header->e_ident,4);
     int result = memcmp(ELFMAG,header->e_ident, 4);
     return result;
 }
