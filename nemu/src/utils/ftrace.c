f7870fcfef34661d64cb8d0d6e7d334a5115cca2
tracer-ysyx <tracer@ysyx.org>
2024-11-08 10:50:59 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:59 up 6 days, 23:32,  2 users,  load average: 0.20, 0.15, 0.11

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -176,21 +176,34 @@ int is_ret(char* inst){
     return strcmp(inst,"ret")==0 ;
 }
 
-void ftrace_message(uint32_t pc, uint32_t dnpc,char* inst){
-    char* spaces = malloc(space_len+1);
-    memset(spaces,' ',space_len); 
-    spaces[space_len] = '\0';
-    if (is_jal(inst)){
-        log_write("0x%x: %s %s@0x%x\n",pc,spaces,inst,dnpc);
-        space_len++;
-    }else if (is_jrtype(inst)){
-        log_write("0x%x: %s %s@0x%x\n",pc,spaces,inst,dnpc);
-        space_len++;
-    }else if (is_ret(inst)){
-        log_write("0x%x: %s %s@0x%x\n",pc,spaces,inst,dnpc);
-        space_len = (space_len>0)?space_len-1:1;
+char* func_name( uint32_t addr ){
+    for (int i=0;i<ftrace_meta->size;i++){
+        func_meta entry = ftrace_meta->fm_entries[i];
+        if (entry.addr == addr)
+            return entry.name;
     }
-    free(spaces);
+    return NULL;
+}
+
+void ftrace_message(uint32_t pc, uint32_t dnpc,char* inst){
+    char* name = "";
+    if ((name = func_name(dnpc))!=NULL){
+        char* spaces = malloc(space_len+1);
+        memset(spaces,' ',space_len); 
+        spaces[space_len] = '\0';
+        if (is_jal(inst)){
+            log_write("0x%x: %s %s [%s@0x%x]\n",pc,spaces,inst,name,dnpc);
+            space_len++;
+        }else if (is_jrtype(inst)){
+            log_write("0x%x: %s %s[%s@0x%x]\n",pc,spaces,inst,name,dnpc);
+            space_len++;
+        }else if (is_ret(inst)){
+            log_write("0x%x: %s %s[%s@0x%x]\n",pc,spaces,inst,name,dnpc);
+            space_len = (space_len>0)?space_len-1:1;
+        }
+        free(spaces);
+    }
+    
     return;
 }
 
