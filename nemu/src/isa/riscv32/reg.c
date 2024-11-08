4d71515dedaf627ef7b94902b63c72d6868cb067
tracer-ysyx <tracer@ysyx.org>
2024-11-09 02:03:51 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:03:51 up 7 days, 14:44,  1 user,  load average: 0.03, 0.20, 0.12

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -28,6 +28,7 @@ void isa_reg_display() {
     for (int i= 0;i<32;i++){
         word_t val = gpr(i);
         printf("%4s%16x%16d\n",regs[i],val,val);
+        printf("  pc%16x%16d\n",cpu.pc,cpu.pc);
     }
 }
 
