cf5940894f3509a5608dcaf97e7a5f3ac952a7e2
tracer-ysyx <tracer@ysyx.org>
2024-11-09 23:32:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:32:01 up 8 days, 12:13,  2 users,  load average: 0.00, 0.03, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,15 +1,20 @@
 #include <am.h>
 #include <nemu.h>
 
-unsigned long start=0; 
-unsigned long now=0; 
+long long rtc[2];
+static long long  start=0; 
+static long long  now=0; 
 
 void __am_timer_init() {
-  ioe_read(RTC_ADDR,&start);
+  ioe_read(RTC_ADDR,&rtc[0]);
+  ioe_read(RTC_ADDR+4,&rtc[1]);
+  start = (rtc[1]<<32)+rtc[0];
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  ioe_read(RTC_ADDR,&now);
+  ioe_read(RTC_ADDR,&rtc[0]);
+  ioe_read(RTC_ADDR+4,&rtc[1]);
+  now = (rtc[1]<<32)+rtc[0];
   uptime->us = now-start;
 }
 
