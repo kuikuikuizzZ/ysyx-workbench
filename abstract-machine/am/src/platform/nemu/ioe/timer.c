328611c839213712773e55a2d76377b5d002b63b
kuikuikuizzZ <kimmmy@gmail.com>
2024-11-09 23:51:11 +0800

    feat(abstract-machine): support ioe timer

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,21 @@
 #include <am.h>
 #include <nemu.h>
 
+long long rtc[2];
+static long long  start=0; 
+static long long  now=0; 
+
 void __am_timer_init() {
+  rtc[0] = inl(RTC_ADDR);
+  rtc[1] = inl(RTC_ADDR+4);
+  start = (rtc[1]<<32)+rtc[0];
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  rtc[0] = inl(RTC_ADDR);
+  rtc[1] = inl(RTC_ADDR+4);
+  now = (rtc[1]<<32)+rtc[0];
+  uptime->us = now-start;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
