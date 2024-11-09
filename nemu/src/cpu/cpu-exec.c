4e776154c0cf6d6cffaa0ccd6e8adee71da6b394
tracer-ysyx <tracer@ysyx.org>
2024-11-09 18:56:14 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:56:14 up 8 days,  7:37,  1 user,  load average: 0.00, 0.06, 0.07

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -64,9 +64,10 @@ static void exec_once(Decode *s, vaddr_t pc) {
   isa_exec_once(s);
   cpu.pc = s->dnpc;
   // 
-  IFDEF(CONFIG_FTRACE, char inst_name[CS_MNEMONIC_SIZE]);
 
 #ifdef CONFIG_ITRACE
+  // 32 match inst name in capstone define
+  char inst_name[32];
   char *p = s->logbuf;
   if (nemu_state.state == NEMU_RUNNING)
     p += snprintf(p, LOG_BUFSIZE,"    " FMT_WORD ":", s->pc);
