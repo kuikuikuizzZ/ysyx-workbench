a0800073e0c348ce7f5dc6f12ab90fe4b8de48b6
tracer-ysyx <tracer@ysyx.org>
2024-11-11 01:05:08 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:05:08 up 9 days, 13:46,  1 user,  load average: 0.23, 0.26, 0.19

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -136,7 +136,7 @@ void init_monitor(int argc, char *argv[]) {
   init_difftest(diff_so_file, img_size, difftest_port);
 
   /* Initialize ftrace */
-  init_ftrace(elf_file);
+   IFDEF(CONFIG_FTRACE,init_ftrace(elf_file));
 
   /* Initialize the simple debugger. */
   init_sdb();
