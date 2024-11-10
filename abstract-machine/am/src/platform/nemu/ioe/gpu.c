bf084a249314e6f499ddb7281bd03fbfb2db63d7
tracer-ysyx <tracer@ysyx.org>
2024-11-10 15:43:30 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:30 up 9 days,  4:24,  1 user,  load average: 0.25, 0.10, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,7 +22,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  memcpy(fb+ctl->x*w+ctl->y,ctl->pixels,ctl->w*ctl->h);
+  memcpy(fb+ctl->x+ctl->y,ctl->pixels,ctl->w*ctl->h);
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
