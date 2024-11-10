1983cb5409499dfd59c96a48771990b14f3eb18f
tracer-ysyx <tracer@ysyx.org>
2024-11-10 16:55:43 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:43 up 9 days,  5:36,  1 user,  load average: 0.20, 0.21, 0.20

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -74,8 +74,8 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  char *s1 = (char*)out;
-  const char *s2 = (char*)in;
+  uint32_t *s1 = (uint32_t*)out;
+  const uint32_t *s2 = (uint32_t*)in;
   int i=0;
   while (i<n && (*s1++=*s2++))
   {
