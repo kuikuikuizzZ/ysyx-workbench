9afd141336103e00ca1aa73737e7bf3f585b2b3c
tracer-ysyx <tracer@ysyx.org>
2024-11-10 14:47:11 +0800

    > compile NEMU ysyx_22040000 张三 Linux uenui-MS-7E07 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:11 up 9 days,  3:28,  1 user,  load average: 0.09, 0.12, 0.07

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -50,24 +50,19 @@ void device_update() {
       case SDL_QUIT:
         nemu_state.state = NEMU_QUIT;
         break;
-// #ifdef CONFIG_HAS_KEYBOARD
-      
-
+#ifdef CONFIG_HAS_KEYBOARD
       // If a key was pressed
       case SDL_KEYDOWN:
       case SDL_KEYUP: {
         uint8_t k = event.key.keysym.scancode;
         bool is_keydown = (event.key.type == SDL_KEYDOWN);
         send_key(k, is_keydown);
-       
         break;
       }
-// #endif
+#endif
       default: break;
     }
-   printf("in event\n");
   }
-  // printf("sdl key %d, down %d\n",k,is_keydown);
 #endif
 }
 
