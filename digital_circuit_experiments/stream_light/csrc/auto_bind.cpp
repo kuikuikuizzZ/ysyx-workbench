c0bbdb3974448e8717b9e78cd98e184fa8a32fb1
kuikuikuizzZ <kimmmy@gmail.com>
2024-11-17 14:23:04 +0800

    rm(*): rm digital experiments

--- digital_circuit_experiments/stream_light/csrc/auto_bind.cpp
+++
@@ -1,6 +0,0 @@
-#include <nvboard.h>
-#include "Vstream_light.h"
-
-void nvboard_bind_all_pins(Vstream_light* top) {
-	nvboard_bind_pin( &top->led, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
-}
