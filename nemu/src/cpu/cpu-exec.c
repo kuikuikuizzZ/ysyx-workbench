f9390a3f865f8b7584264d22c4a21594e89e4201
tracer-ysyx <tracer@ysyx.org>
2024-11-21 17:59:41 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:59:41 up 7 days,  1:25,  1 user,  load average: 0.28, 0.21, 0.22

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -92,12 +92,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + LOG_BUFSIZE - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen,inst_name);
   int len = strlen(s->logbuf);
-  memset(s->logbuf+len,' ',ITRACE_SIZE-len-2);
+  memset(s->logbuf+len,' ',ITRACE_SIZE-len);
   s->logbuf[ITRACE_SIZE-1] = '\n';
-  if(RingBuffer_available(log_buff)<(len+1)){
-    Assert(ITRACE_SIZE>(len+1),"length of itrace excceed\n");
+  if(RingBuffer_available(log_buff)<ITRACE_SIZE){
     RingBuffer_commit_read(log_buff,ITRACE_SIZE);
   }
+  Assert(ITRACE_SIZE>(len+1),"length of itrace excceed\n");
   RingBuffer_put(log_buff,s->logbuf,ITRACE_SIZE);
   memset(s->logbuf,0,ITRACE_SIZE);
 #endif
