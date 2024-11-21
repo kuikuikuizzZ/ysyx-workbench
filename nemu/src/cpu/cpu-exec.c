6776de9ce6f8df2dc12657d8bbd39ad1f796c7bb
tracer-ysyx <tracer@ysyx.org>
2024-11-21 17:44:30 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:30 up 7 days,  1:10,  1 user,  load average: 0.65, 0.39, 0.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -93,13 +93,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen,inst_name);
   int len = strlen(s->logbuf);
   s->logbuf[len] = '\n';
-  printf("before put\n");
   if(RingBuffer_available(log_buff)<(len+1)){
     Assert(ITRACE_SIZE>(len+1),"length of itrace excceed\n");
     RingBuffer_commit_read(log_buff,ITRACE_SIZE);
-  } 
+  }
   RingBuffer_put(log_buff,s->logbuf,ITRACE_SIZE);
-  printf("after put\n");
   memset(s->logbuf,0,ITRACE_SIZE);
 #endif
 #ifdef CONFIG_FTRACE
