5f74c02599d454966941f4c73bf2097ca38c6458
tracer-ysyx <tracer@ysyx.org>
2024-11-21 18:08:24 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:08:24 up 7 days,  1:34,  1 user,  load average: 0.21, 0.26, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -93,8 +93,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen,inst_name);
   int len = strlen(s->logbuf);
   memset(s->logbuf+len,' ',ITRACE_SIZE-len);
-  s->logbuf[ITRACE_SIZE-2] = '\n';
-  s->logbuf[ITRACE_SIZE-1] = 0;
+  s->logbuf[ITRACE_SIZE-1] = '\n';
   if(RingBuffer_available(log_buff)<ITRACE_SIZE){
     RingBuffer_commit_read(log_buff,ITRACE_SIZE);
   }
