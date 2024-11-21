377e51c4d707f96e57758ab60271d36f2ec1f455
tracer-ysyx <tracer@ysyx.org>
2024-11-21 17:49:44 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:44 up 7 days,  1:15,  1 user,  load average: 0.07, 0.22, 0.23

--- nemu/src/utils/ringbuffer.c
+++ nemu/src/utils/ringbuffer.c
@@ -11,7 +11,7 @@ RingBuffer *RingBuffer_create(int power_of_two){
     RingBuffer *buffer = calloc(1,sizeof(RingBuffer));
     buffer->mask = power_of_two-1;
     buffer->buffer = calloc(buffer->mask+1,1);
-    memset(buffer->buffer,0,buffer->mask+1);
+    memset(buffer->buffer,' ',buffer->mask+1);
     buffer->start = buffer->end = 0;
     return buffer;
 }
