b8c6964e2a87ef64480503d5a64318af77ca94cf
tracer-ysyx <tracer@ysyx.org>
2024-11-22 16:12:31 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:31 up 7 days, 23:38,  1 user,  load average: 0.39, 0.28, 0.16

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -32,7 +32,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
       }
     } else{
       for(size_t i = 0; i < n; i++){
-        paddr_write(addr,1,*(buf_in_byte+i));
+        paddr_write(addr+i,1,*(buf_in_byte+i));
       }
     }
     return;
