c9530aa90f30f9cb8afd1437c6a2ce3bbe276d28
tracer-ysyx <tracer@ysyx.org>
2024-11-24 20:13:07 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:07 up 10 days,  3:38,  1 user,  load average: 0.42, 0.20, 0.17

--- npc/csrc/device/device.c
+++ npc/csrc/device/device.c
@@ -16,12 +16,9 @@
 #include <common.h>
 #include <utils.h>
 #include <device/alarm.h>
-#include <npc.h>
-#ifndef CONFIG_TARGET_AM
-#include <SDL2/SDL.h>
-#endif
-
-extern NPCState npc_state;
+// #ifndef CONFIG_TARGET_AM
+// // #include <SDL2/SDL.h>
+// #endif
 
 void init_map();
 void init_timer();
@@ -44,37 +41,37 @@ void device_update() {
   }
   last = now;
 
-  // IFDEF(CONFIG_HAS_VGA, vga_update_screen());
-  vga_update_screen();
-#ifndef CONFIG_TARGET_AM
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        npc_state.state = NPC_QUIT;
-        break;
-#ifdef CONFIG_HAS_KEYBOARD
-      // If a key was pressed
-      case SDL_KEYDOWN:
-      case SDL_KEYUP: {
-        uint8_t k = event.key.keysym.scancode;
-        bool is_keydown = (event.key.type == SDL_KEYDOWN);
-        send_key(k, is_keydown);
-        break;
-      }
-#endif
-      default: break;
-    }
-  }
-#endif
+  IFDEF(CONFIG_HAS_VGA, vga_update_screen());
+
+// #ifndef CONFIG_TARGET_AM
+//   SDL_Event event;
+//   while (SDL_PollEvent(&event)) {
+//     switch (event.type) {
+//       case SDL_QUIT:
+//         npc.state = NEMU_QUIT;
+//         break;
+// #ifdef CONFIG_HAS_KEYBOARD
+//       // If a key was pressed
+//       case SDL_KEYDOWN:
+//       case SDL_KEYUP: {
+//         uint8_t k = event.key.keysym.scancode;
+//         bool is_keydown = (event.key.type == SDL_KEYDOWN);
+//         send_key(k, is_keydown);
+//         break;
+//       }
+// #endif
+    //   default: break;
+    // }
+//   }
+// #endif
 }
 
-void sdl_clear_event_queue() {
-#ifndef CONFIG_TARGET_AM
-  SDL_Event event;
-  while (SDL_PollEvent(&event));
-#endif
-}
+// void sdl_clear_event_queue() {
+// #ifndef CONFIG_TARGET_AM
+//   SDL_Event event;
+//   while (SDL_PollEvent(&event));
+// #endif
+// }
 
 void init_device() {
   printf("init device \n");
@@ -83,7 +80,6 @@ void init_device() {
 
   init_serial();
   init_timer();
-  init_vga();
   // IFDEF(CONFIG_HAS_TIMER, init_timer());
   // IFDEF(CONFIG_HAS_VGA, init_vga());
   // IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
