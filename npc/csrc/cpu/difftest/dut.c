1977235a78292a0ba18ba04880706f5fd440210a
tracer-ysyx <tracer@ysyx.org>
2024-11-24 10:19:17 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:19:17 up 9 days, 17:45,  1 user,  load average: 0.01, 0.03, 0.12

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -116,25 +116,25 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
 
-  if (skip_dut_nr_inst > 0) {
-    ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-    if (ref_r.pc == npc) {
-      skip_dut_nr_inst = 0;
-      checkregs(&ref_r, npc);
-      return;
-    }
-    skip_dut_nr_inst --;
-    if (skip_dut_nr_inst == 0)
-      panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, pc);
-    return;
-  }
+  // if (skip_dut_nr_inst > 0) {
+  //   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+  //   if (ref_r.pc == npc) {
+  //     skip_dut_nr_inst = 0;
+  //     checkregs(&ref_r, npc);
+  //     return;
+  //   }
+  //   skip_dut_nr_inst --;
+  //   if (skip_dut_nr_inst == 0)
+  //     panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, pc);
+  //   return;
+  // }
 
-  if (is_skip_ref) {
-    // to skip the checking of an instruction, just copy the reg state to reference design
-    ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-    is_skip_ref = false;
-    return;
-  }
+  // if (is_skip_ref) {
+  //   // to skip the checking of an instruction, just copy the reg state to reference design
+  //   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+  //   is_skip_ref = false;
+  //   return;
+  // }
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
