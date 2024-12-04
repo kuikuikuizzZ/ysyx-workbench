f7a3667e8ad2a7716b793ce7e1131158f4becab6
tracer-ysyx <tracer@ysyx.org>
2024-12-04 18:16:31 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:31 up 20 days,  1:42,  1 user,  load average: 0.47, 0.33, 0.28

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -3,11 +3,20 @@
 #include <klib.h>
 #include <riscv.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-static int w = 0,h=0;
+
+static inline uint8_t R(uint32_t p) { return p >> 16; }
+static inline uint8_t G(uint32_t p) { return p >> 8; }
+static inline uint8_t B(uint32_t p) { return p; }
+
+struct pixel {
+  uint8_t b, g, r;
+} __attribute__ ((packed));
+
+static int display_w = 0,display_h=0;
 static uint32_t *fb = NULL;
 void __am_gpu_init() {
-  w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
-  h = inw(VGACTL_ADDR);  // TODO: get the correct height
+  display_w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
+  display_h = inw(VGACTL_ADDR);  // TODO: get the correct height
   fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (int i = 0; i < w * h; i ++) fb[i] = 0;
   outl(SYNC_ADDR, 1);
@@ -19,15 +28,29 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .width = inw(VGACTL_ADDR+2), .height = inw(VGACTL_ADDR),
     .vmemsz = inl(FB_ADDR),
   };
-  w = cfg->width;
-  h = cfg->height;
+  display_w = cfg->width;
+  display_h = cfg->height;
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *draw) {
   uint32_t* p = ctl->pixels;
   // for(int i=0;i<ctl->h;i++){
   //   memcpy(fb+ctl->x+(ctl->y+i)*w,p+ctl->w*i,ctl->w*sizeof(uint32_t));
   // }
+
+  int x = draw->x, y = draw->y, w = draw->w, h = draw->h;
+  int W = display_w, H = display_h;
+  uint32_t *pixels = draw->pixels;
+  int len = (x + w >= W) ? W - x : w;
+  for (int j = 0; j < h; j ++, pixels += w) {
+    if (y + j < H) {
+      struct pixel *px = &fb[x + (j + y) * W];
+      for (int i = 0; i < len; i ++, px ++) {
+        uint32_t p = pixels[i];
+        *px = (struct pixel) { .r = R(p), .g = G(p), .b = B(p) };
+      }
+    }
+  }
 
   for(int i=0;i<ctl->h;i++){
     for(int j=0;j<ctl->w;j++){
