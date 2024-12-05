e646db1b8d0152af89497ef5460387e9e9f01415
tracer-ysyx <tracer@ysyx.org>
2024-12-06 01:26:01 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:26:01 up 21 days,  8:51,  1 user,  load average: 0.15, 0.11, 0.16

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -65,10 +65,7 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
   if (!hbrk) hbrk = (void *)ROUNDUP(heap.start, 8);
-// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-    // panic("Not implemented!\n");
-// #endif
-  // return malloc(size);  
+
   size  = (size_t)ROUNDUP(size, 8);
   char *old = hbrk;
   hbrk += size;
@@ -77,6 +74,10 @@ void *malloc(size_t size) {
     *p = 0;
   }
   return old;
+  // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+    // panic("Not implemented!\n");
+  // #endif
+  // return malloc(size);  
 
 }
 
