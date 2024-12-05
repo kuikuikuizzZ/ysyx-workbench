ba3bb776c8f5a792b147a0b56a3086d70b0cfa40
kuikuikuizzZ <kimmmy@gmail.com>
2024-12-05 15:04:23 +0800

    feat(abstract-machine): support malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -3,6 +3,7 @@
 #include <klib-macros.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+static char *hbrk = NULL;
 static unsigned long int next = 1;
 
 int rand(void) {
@@ -62,8 +63,16 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  if (!hbrk) hbrk = (void *)ROUNDUP(heap.start, 8);
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  size  = (size_t)ROUNDUP(size, 8);
+  char *old = hbrk;
+  hbrk += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+    *p = 0;
+  }
+  return old;
 #endif
   return NULL;
 }
