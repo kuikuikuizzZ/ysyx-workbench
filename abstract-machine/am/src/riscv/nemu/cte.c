d98fb683658fc02e53f27a7f16abab995677307a
tracer-ysyx <tracer@ysyx.org>
2024-12-07 00:21:23 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:21:23 up 22 days,  7:47,  1 user,  load average: 0.25, 0.20, 0.19

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ Context* __am_irq_handle(Context *c) {
       case -1: ev.event = EVENT_YIELD;break;
       default: ev.event = EVENT_ERROR; break;
     }
-    printf("context: mcause %x, mepc %x",c->mcause,c->mepc);
+    printf("context: mcause %x, mepc %x\n",c->mcause,c->mepc);
     c = user_handler(ev, c);
     assert(c != NULL);
   }
@@ -42,7 +42,6 @@ void yield() {
 #else
   asm volatile("li a7, -1; ecall");
 #endif
-  asm volatile("jalr ra, -1; ecall");
   asm volatile("lw ra,4(sp);addi sp,sp, 4");
 }
 
