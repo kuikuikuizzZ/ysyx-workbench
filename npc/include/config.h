e3ac62ac1b4e028df3f7df53336804b40a494e67
kuikuikuizzZ <kimmmy@gmail.com>
2024-12-06 15:32:22 +0800

    feat(*): adapted config

--- npc/include/config.h
+++ npc/include/config.h
@@ -1,17 +1,20 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define CONFIG_ITRACE
-#define CONFIG_SDB
+// #define CONFIG_ITRACE
+// #define CONFIG_SDB
 // #define CONFIG_DIFFTEST
 #define CONFIG_RVE          // use riscv32e
 // #define CONFIG_WATCH_TOP
 
 #define CONFIG_DEVICE
+
 #define CONFIG_SERIAL_MMIO 0xa00003f8
 #define CONFIG_HAS_SERIAL
 #define CONFIG_RTC_MMIO 0xa0000048
 #define CONFIG_HAS_TIMER
+#define CONFIG_I8042_DATA_MMIO 0xa0000060
+#define CONFIG_HAS_KEYBOARD
 
 #define CONFIG_TIMER_CLOCK_GETTIME
 
@@ -22,4 +25,7 @@
 
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_MSIZE 0x8000000
+
+#define __GUEST_ISA__ riscv32e
+
 #endif
