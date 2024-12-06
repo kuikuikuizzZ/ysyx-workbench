d3cb43145abf1cd581707e0a444a745c5b616e71
tracer-ysyx <tracer@ysyx.org>
2024-12-06 22:32:20 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:32:20 up 22 days,  5:58,  1 user,  load average: 0.31, 0.26, 0.16

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -27,7 +27,7 @@ enum {
   TYPE_I, TYPE_R,TYPE_J, TYPE_U, TYPE_S, TYPE_B, TYPE_CSR,
   TYPE_N, // none
 };
-
+#define show_csr() (printf("csr %d\n",*csr_no))
 #define src1R() do { *src1 = R(rs1);  }   while (0)
 #define src2R() do { *src2 = R(rs2);  }   while (0)
 #define srcCSR()do { *src_csr = CSR(*csr_no);}  while (0)
@@ -49,7 +49,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_J:                   immJ(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-    case TYPE_CSR:        src1R();srcCSR(); break;
+    case TYPE_CSR:        src1R();srcCSR();show_csr(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
