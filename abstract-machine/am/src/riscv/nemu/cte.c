b78020b54c59910e54109162c18906fe2d7b296f
tracer-ysyx <tracer@ysyx.org>
2024-12-07 00:02:39 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:02:39 up 22 days,  7:28,  1 user,  load average: 0.71, 0.32, 0.20

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -37,11 +37,11 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 void yield() {
   asm volatile("addi sp,sp, -4;sw ra,0(sp)");
 #ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
+  // asm volatile("li a5, -1; ecall");
 #else
   asm volatile("li a7, -1; ecall");
 #endif
-  asm volatile("lw ra,0(sp);addi sp,sp, 4;");
+  // asm volatile("lw ra,0(sp);addi sp,sp, 4;");
 }
 
 bool ienabled() {
--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -73,7 +73,7 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, R(1)=isa_raise_intr( R(17),s->pc)); 
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret  ,  R, s->dnpc=csr(MEPC)+4);   // TODO: mstatus should be set
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret  ,  R, s->dnpc=csr(MEPC)+4; printf("ra %x\n",gpr(1)); isa_csr_display());   // TODO: mstatus should be set
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
 
