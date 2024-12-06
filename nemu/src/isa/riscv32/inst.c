45da4432345fd82670721fc533344eca6b98a2bd
kuikuikuizzZ <kimmmy@gmail.com>
2024-12-07 00:32:58 +0800

    feat(npc): support ecall trap

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -18,28 +18,31 @@
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
 
+void isa_csr_display();
+
 #define R(i) gpr(i)
+#define CSR(i) csr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_R,TYPE_J, TYPE_U, TYPE_S, TYPE_B,
+  TYPE_I, TYPE_R,TYPE_J, TYPE_U, TYPE_S, TYPE_B, TYPE_CSR,
   TYPE_N, // none
 };
-
-#define src1R() do { *src1 = R(rs1); } while (0)
-#define src2R() do { *src2 = R(rs2); } while (0)
+#define src1R() do { *src1 = R(rs1);  }   while (0)
+#define src2R() do { *src2 = R(rs2);  }   while (0)
+#define srcCSR()do { *src_csr = CSR(*csr_no);}  while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i,30,21)<<1) | (BITS(i,20,20)<<11) | (BITS(i,19,12)<<12); } while(0)
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7)<<11)| (BITS(i, 30, 25)<<5)| (BITS(i, 11, 8)<<1); } while(0)
-
-static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
+static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, word_t *src_csr,int *csr_no, int type) {
   uint32_t i = s->isa.inst;
-  int rs1 = BITS(i, 19, 15);
-  int rs2 = BITS(i, 24, 20);
-  *rd     = BITS(i, 11, 7);
+  int rs1   = BITS(i, 19, 15);
+  int rs2   = BITS(i, 24, 20);
+  *rd       = BITS(i, 11, 7);
+  *csr_no   = BITS(i, 31, 20);
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
     case TYPE_R: src1R(); src2R();         break; 
@@ -47,6 +50,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_J:                   immJ(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_CSR:        src1R();srcCSR();break;
+    // 
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
@@ -58,15 +63,17 @@ static int decode_exec(Decode *s) {
 
 #define INSTPAT_INST(s) ((s)->isa.inst)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  int rd = 0; \
-  word_t src1 = 0, src2 = 0, imm = 0; \
-  decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+  int rd = 0, csr_no=0; \
+  word_t src1 = 0, src2 = 0, imm = 0,src_csr=0; \
+  decode_operand(s, &rd, &src1, &src2, &imm,&src_csr,&csr_no, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
 
   INSTPAT_START();
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc=isa_raise_intr( R(17),s->pc)); 
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret  ,  R, s->dnpc=csr(MEPC) );   // TODO: mstatus should be set
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
 
@@ -129,6 +136,12 @@ static int decode_exec(Decode *s) {
 
   // J Type
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal     , J, R(rd)=s->pc+4; s->dnpc =s->pc+imm);
+
+
+  // csr Type
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw   , CSR, R(rd)=src_csr; CSR(csr_no)=src1);
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs   , CSR, R(rd)=src_csr; CSR(csr_no)=src_csr|src1);
+
 
   // NO MATCH Type
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -17,11 +17,21 @@
 #include "local-include/reg.h"
 #include <string.h>
 
+
+#define  USED_CSR_NUM 5
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+static CSR_INFO csr_info[USED_CSR_NUM]= {
+  {.idx=0x180,.name="STAP"},
+  {.idx=0x300,.name="MSTATUS"},
+  {.idx=0x305,.name="MTVEC"},
+  {.idx=0x341,.name="MEPC"},
+  {.idx=0x342,.name="MCAUSE"},
 };
 
 void isa_reg_display() {
@@ -32,16 +42,24 @@ void isa_reg_display() {
     printf("%4s:%.8x\n","pc",cpu.pc);
 }
 
+
 word_t isa_reg_str2val(const char *s, bool *success) {
   word_t val;
   if(strstr(s,"$pc")){
     return cpu.pc;
   }
-  for (int i= 0;i<32;i++){
+  for (int i= 0;i<MUXDEF(CONFIG_RVE, 16, 32);i++){
       if (strstr(s,regs[i])){
         val = gpr(i);
         *success = true;
       } 
   }
   return val;
+}
+
+void isa_csr_display() {
+    for(int i=0;i<USED_CSR_NUM;i++){
+        printf("%4s:%.8x\n",csr_info[i].name,csr(csr_info[i].idx));
+    }
+    printf("%4s:%.8x\n","pc",cpu.pc);
 }
