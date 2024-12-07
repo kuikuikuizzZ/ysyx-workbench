ba527efc09c6de958454f1e97b529a5b5e2963c2
tracer-ysyx <tracer@ysyx.org>
2024-12-07 13:18:25 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:18:25 up 22 days, 20:44,  1 user,  load average: 0.28, 0.26, 0.17

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -34,9 +34,8 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = malloc(sizeof(Context));
   c->mstatus = 0x1800;   
   c-> mepc = (uint32_t)entry;           // mepc is set to entry 
-  c->gpr[10] =(uint32_t)&arg;           // a0 = &arg
+  c->gpr[10] =(uint32_t)arg;           // a0 = &arg
   c->gpr[1] = (uint32_t)kstack.end;     // sp = kstack top should kstack.end?
-  printf("kstack.start %d,kstack.end %d,size %d \n",(uint32_t)kstack.start,(uint32_t)kstack.end,(uint32_t)kstack.end-(uint32_t)kstack.start);
   return c;
 }
 
