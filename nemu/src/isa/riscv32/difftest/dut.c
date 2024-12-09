b21b962aa9879bcd84426fd4a50f829cc1849d9d
tracer-ysyx <tracer@ysyx.org>
2024-12-09 16:17:12 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:12 up 24 days, 23:43,  1 user,  load average: 0.26, 0.16, 0.15

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,21 +18,21 @@
 #include "../local-include/reg.h"
 
 const int gpr_size = MUXDEF(CONFIG_RVE, 16, 32);
-// extern CSR_INFO csr_info[USED_CSR_NUM];
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if (ref_r->pc != cpu.pc ){
     return false;
   }
   for (int i=0;i<gpr_size;i++){
     if(ref_r->gpr[i]!=cpu.gpr[check_reg_idx(i)]) {
-      printf("x%d,ref %x, dut %x\n",i,ref_r->gpr[i],cpu.gpr[check_reg_idx(i)]);
+      printf("x%d: ref %x, dut %x\n",i,ref_r->gpr[i],cpu.gpr[check_reg_idx(i)]);
       return false;
     }
   } 
   for (int i=0;i<USED_CSR_NUM;i++){
     CSR_INFO used_csr = csr_info[i];
     if(ref_r->csr[used_csr.idx]!=cpu.csr[used_csr.idx]){
-      printf("x%d,ref %x, dut %x\n",i,ref_r->csr[used_csr.idx],cpu.csr[used_csr.idx]);
+      printf("%s,ref %x, dut %x\n",used_csr.name,ref_r->csr[used_csr.idx],cpu.csr[used_csr.idx]);
       return false;
     }
   }
