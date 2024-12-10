adfc09c58a4d6b58691748c2d3da1f82c41a5f24
tracer-ysyx <tracer@ysyx.org>
2024-12-10 17:09:25 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:25 up 26 days, 35 min,  1 user,  load average: 0.29, 0.23, 0.19

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -58,12 +58,7 @@ void itoa_dec(int n , char s[]){
   reverse(s);
 }
 
-void itoa_hex(int n, char s[]){
-  if (n ==INT32_MIN ){
-    char *res = "0x100000000";
-    memcpy(s,res,12);
-    return;
-  }
+void itoa_hex(unsigned n, char s[]){
   int i=0; 
   do{
     s[i++] = (n%16>=10)?n%16+'a':n%16+'0';
@@ -75,7 +70,7 @@ void itoa_hex(int n, char s[]){
 void klib_itoa(int n , char s[], int type){
   switch (type){
   case ITOA_DEC: itoa_dec(n,s); break;
-  case ITOA_HEX: itoa_hex(n,s); break;
+  case ITOA_HEX: itoa_hex((unsigned)n,s); break;
   default:break;
   }
   return;
