2ec7c0bd8c27b6d63f40a05107b3b1b8ac4e3640
tracer-ysyx <tracer@ysyx.org>
2024-12-10 18:53:18 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:18 up 26 days,  2:19,  1 user,  load average: 0.65, 0.18, 0.11

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -90,6 +90,7 @@ void *malloc(size_t size) {
   for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
     *p = 0;
   }
+  printf("in malloc\n");
   return old;
   // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
     // panic("Not implemented!\n");
