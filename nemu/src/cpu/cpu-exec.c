0f934dc62425076224b3865b773711c16298bdef
tracer-ysyx <tracer@ysyx.org>
2025-06-05 13:26:40 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:40 up  2:09,  3 users,  load average: 0.24, 0.07, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,9 +41,9 @@ extern bool wps_diff();
 extern RingBuffer *log_buff;
 
 void itrace_once(Decode*s) {
-  printf("itrace_once: pc = " FMT_WORD ", snpc = " FMT_WORD "\n", s->pc, s->snpc);
   #ifdef CONFIG_ITRACE
   // 32 match inst name in capstone define
+  printf("In itrace_once\n"); 
   char inst_name[32];
   char *p = s->logbuf;
   p +=  (nemu_state.state != NEMU_ABORT) ? 
