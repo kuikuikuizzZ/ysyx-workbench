fb7d7ca90e0214d78a1b3d468d389c1342010d6c
tracer-ysyx <tracer@ysyx.org>
2025-06-06 16:40:58 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:40:58 up  3:47,  3 users,  load average: 0.04, 0.08, 0.03

--- nemu/include/generated/autoconf.h
+++ nemu/include/generated/autoconf.h
@@ -28,6 +28,7 @@
 #define CONFIG_CC "gcc"
 #define CONFIG_DIFFTEST_REF_PATH "tools/spike-diff"
 #define CONFIG_DTRACE_COND "true"
+#define CONFIG_CC_DEBUG 1
 #define CONFIG_TRACE_START 0
 #define CONFIG_CC_GCC 1
 #define CONFIG_TRACE 1
