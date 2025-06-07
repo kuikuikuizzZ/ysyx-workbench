eb0d2ed8150a6297b39f5213418c8d85aadc3d63
tracer-ysyx <tracer@ysyx.org>
2025-06-07 22:30:34 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:34 up  8:52,  3 users,  load average: 0.20, 0.24, 0.18

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -47,6 +47,8 @@ static void audio_play(void *userdata, uint8_t *stream, int len) {
     memset(stream + nread, 0, len - nread);
   }
   memset(sbuf, 0, audio_base[reg_sbuf_size]);
+  audio_base[reg_count]=0;
+  last = 0;
 }
 
 void init_audio_ctrl(u_int32_t* audio_base) {
