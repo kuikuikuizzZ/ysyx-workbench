f264d16f80e439e1614e7b3f8b8626f9bf92ecaa
tracer-ysyx <tracer@ysyx.org>
2025-06-07 23:44:09 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:44:09 up 10:05,  3 users,  load average: 0.45, 0.67, 0.45

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -39,8 +39,8 @@ static int front = 0, tail = 0;
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   switch (offset) {
   case AUDIO_COUNT_ADDR:
-    if (is_write) tail = *(audio_base + AUDIO_COUNT_ADDR );
-    else *(audio_base + (AUDIO_COUNT_ADDR) ) = tail;
+    if (is_write) tail = *(audio_base + AUDIO_COUNT_ADDR / 4);
+    else *(audio_base + (AUDIO_COUNT_ADDR) / 4) = tail;
     assert(tail <= CONFIG_SB_SIZE);
     break;
   default: printf("%d\n", offset);
