cc204d940271e742d26562817cc081dcce368c9d
tracer-ysyx <tracer@ysyx.org>
2025-06-08 00:01:00 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:01:00 up 10:22,  3 users,  load average: 0.24, 0.26, 0.35

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -37,6 +37,7 @@ static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 static int front = 0, tail = 0;
 static SDL_AudioSpec s = {};
+void work(uint32_t x);
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   switch (offset) {
@@ -57,6 +58,8 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
     else *(audio_base + (AUDIO_COUNT_ADDR) / 4) = tail;
     assert(tail <= CONFIG_SB_SIZE);
     break;
+  case AUDIO_INIT_ADDR:
+    assert(is_write); work(*(audio_base + (AUDIO_INIT_ADDR) / 4)); break;
   default: printf("%d\n", offset);
   }
 }
@@ -71,20 +74,18 @@ static void audio_play(void *userdata, uint8_t *stream, int len) {
   return;
 }
 
-void init_audio_ctrl(u_int32_t* audio_base) {
-  assert(audio_base != NULL);
 
-  s.freq = audio_base[reg_freq];
-  s.format = AUDIO_S16SYS;
-  s.channels = audio_base[reg_channels];
-  s.samples = audio_base[reg_samples];
-  s.callback = audio_play;
-  s.userdata = NULL;
-  int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
-  if (ret == 0) {
-    SDL_OpenAudio(&s, NULL);
-    SDL_PauseAudio(0);
+void work(uint32_t x) {
+  if (!x) return;
+  *(audio_base + (AUDIO_INIT_ADDR) / 4) = 0;
+  if (x) {
+    int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+    if (!ret) {
+      SDL_OpenAudio(&s, NULL);
+      SDL_PauseAudio(0);
+    }
   }
+  return;
 }
 void init_audio() {
   printf("Initializing audio device...\n");
@@ -98,6 +99,10 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
-  init_audio_ctrl(audio_base);
   audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  s.format = AUDIO_S16SYS;
+  s.userdata = NULL;
+  s.callback = audio_play;
+  SDL_InitSubSystem(SDL_INIT_AUDIO);
+
 }
