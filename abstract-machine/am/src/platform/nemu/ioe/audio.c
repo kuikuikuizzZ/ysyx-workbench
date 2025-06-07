3a32f535e08d4ca14ce6146a0a0cab1b8a6afd3e
kuikuikuizzZ <kimmmywu@gmail.com>
2025-06-07 19:24:49 +0800

    feat(audio): init audio

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -9,18 +9,30 @@
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
 void __am_audio_init() {
+
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR,ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_COUNT_ADDR, 0);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  int nwrite = 0;
+  while(nwrite < len) {
+    outl(AUDIO_SBUF_ADDR+nwrite, (uint32_t)(ctl->buf.start + nwrite));
+    nwrite += 4; // each write is 4 bytes (uint32_t)
+    outl(AUDIO_COUNT_ADDR, nwrite);
+  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -31,8 +31,39 @@ static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+
 }
 
+static void audio_play(void *userdata, uint8_t *stream, int len) {
+  int nread = len;
+  if (audio_base[reg_count] < len) nread = audio_base[reg_count] ;
+  void* res = memcpy(stream,sbuf, nread);
+  if (res == NULL) {
+    return;
+  }
+
+  audio_base[reg_count] -= nread;
+  if (len > nread) {
+    memset(stream + nread, 0, len - nread);
+  }
+}
+
+void init_audio_ctrl(u_int32_t* audio_base) {
+  assert(audio_base != NULL);
+
+  SDL_AudioSpec s = {};
+  s.freq = audio_base[reg_freq];
+  s.format = AUDIO_S16SYS;
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+  s.callback = audio_play;
+  s.userdata = NULL;
+  int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+  if (ret == 0) {
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+  }
+}
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
@@ -44,4 +75,7 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  init_audio_ctrl(audio_base);
+  audio_base[reg_init] = sbuf != NULL ? 1 : 0; 
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
 }
