c100cea1cb72f94afcdf2eef2a6fc5ed1983ac8a
tracer-ysyx <tracer@ysyx.org>
2025-06-07 23:11:09 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:09 up  9:32,  3 users,  load average: 0.16, 0.27, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -45,6 +45,6 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
     outl(AUDIO_SBUF_ADDR+pos+nwrite, (uint32_t)(ctl->buf.start + nwrite));
     nwrite += 1; // each write is 4 bytes (uint32_t)
   }
-  outl(AUDIO_COUNT_ADDR, len);
+  outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR)+len);
   pos+= nwrite;
 }
