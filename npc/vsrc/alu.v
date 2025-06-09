b6a92d42adf3e0c0891bc8bdbee639fb0acffa29
kuikuikuizzZ <kimmmywu@gmail.com>
2025-06-09 23:33:59 +0800

    fix(npc): sll mod src2

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -1,4 +1,4 @@
-module ysyx_24100012_alu #(DATA_WIDTH=32,N_SEL=4)(
+module ysyx_24100012_alu #(DATA_WIDTH=32,N_SEL=4,SHIFT_WIDTH=5)(
     input clk,
     input rst,
     input [1:0] csrType,
@@ -13,12 +13,12 @@ module ysyx_24100012_alu #(DATA_WIDTH=32,N_SEL=4)(
         alu_sel,{
             4'b0000, in_a+in_b,                         //add
             4'b1000, in_a-in_b,                         //sub
-            4'b0001, in_a<<in_b,                        //slt
+            4'b0001, in_a<<in_b[SHIFT_WIDTH-1:0],                        //sll
             4'b0010, ($signed(in_a)<$signed(in_b))? 32'h1 :32'h0,   //slt
             4'b0011, (in_a<in_b)? 32'h1 :32'h0,         //sltu
             4'b0100, in_a^in_b,                         //xor
-            4'b0101, in_a>>in_b,                        //srl
-            4'b1101, $signed(in_a)>>>in_b,                       //sra
+            4'b0101, in_a>>in_b[SHIFT_WIDTH-1:0],                        //srl
+            4'b1101, $signed(in_a)>>>in_b[SHIFT_WIDTH-1:0],                       //sra
             4'b0110, in_a | in_b,                       //or
             4'b0111, in_a & in_b                        //and
         });
@@ -40,7 +40,7 @@ module ysyx_24100012_alu #(DATA_WIDTH=32,N_SEL=4)(
             2'b11, nopriv_out
         });
     // always@(*)
-        // $display("alu a: %x, b: %x, code %b", in_a,in_b,alu_sel);
+    //     $display("alu a: %x, b: %x,csrType %b code %b,noprivout: %x", in_a,in_b,csrType,alu_sel,nopriv_out);
 
 endmodule
 
