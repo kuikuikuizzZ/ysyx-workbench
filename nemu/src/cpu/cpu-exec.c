4620fd9d306c59bf90b9834ad903d39c4e84e07e
tracer-ysyx <tracer@ysyx.org>
2025-06-11 00:26:20 +0800

    > compile NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:26:20 up 10:32,  3 users,  load average: 0.31, 0.36, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -110,6 +110,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
+  IFDEF(CONFIG_ETRACE, s->is_exception = 0);
   isa_exec_once(s);
   cpu.pc = s->dnpc;
 }
