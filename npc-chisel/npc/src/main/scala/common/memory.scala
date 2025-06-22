b4c1059abca495de20054ba376c65bba4401da6b
tracer-ysyx <tracer@ysyx.org>
2025-06-23 03:55:26 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:55:26 up 13:13,  4 users,  load average: 0.92, 1.03, 0.92

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -114,12 +114,12 @@ class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 2
 
    val req_typi = io.core_ports(DPORT).req.bits.typ
    // val resp_datai = async_data.io.dataInstr(DPORT).data
-   io.core_ports(DPORT).resp.bits.data := MuxCase(resp_datai,Array(
-      (req_typi === MT_B) -> Cat(Fill(24,resp_datai(7)),resp_datai(7,0)),
-      (req_typi === MT_H) -> Cat(Fill(16,resp_datai(15)),resp_datai(15,0)),
-      (req_typi === MT_BU) -> Cat(Fill(24,0.U),resp_datai(7,0)),
-      (req_typi === MT_HU) -> Cat(Fill(16,0.U),resp_datai(15,0))
-   ))
+   // io.core_ports(DPORT).resp.bits.data := MuxCase(resp_datai,Array(
+   //    (req_typi === MT_B) -> Cat(Fill(24,resp_datai(7)),resp_datai(7,0)),
+   //    (req_typi === MT_H) -> Cat(Fill(16,resp_datai(15)),resp_datai(15,0)),
+   //    (req_typi === MT_BU) -> Cat(Fill(24,0.U),resp_datai(7,0)),
+   //    (req_typi === MT_HU) -> Cat(Fill(16,0.U),resp_datai(15,0))
+   // ))
    async_data.io.dw.en := false.B
    when (io.core_ports(DPORT).req.valid && (io.core_ports(DPORT).req.bits.fcn === M_XWR))
    {
