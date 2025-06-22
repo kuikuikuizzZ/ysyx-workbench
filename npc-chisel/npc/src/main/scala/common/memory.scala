66e58687ce9e22e1094dbf9b46882cb1dac50669
tracer-ysyx <tracer@ysyx.org>
2025-06-23 02:15:38 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:15:38 up 11:33,  4 users,  load average: 7.27, 4.76, 3.25

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -84,6 +84,7 @@ class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 2
    val num_bytes_per_line = 8
    val num_lines = num_bytes / num_bytes_per_line
    // val async_data = Module(new YSYX2400012Mem())
+   val async_data =  SyncReadMem(1024, Vec(4, UInt(width.W)))
    for (i <- 0 until num_core_ports)
    {
       io.core_ports(i).resp.valid := io.core_ports(i).req.valid
