03dc30e929d0608bd51bbac2ad78dfba06e13392
tracer-ysyx <tracer@ysyx.org>
2025-06-23 03:30:14 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:30:14 up 12:47,  4 users,  load average: 0.67, 0.65, 0.66

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -81,10 +81,9 @@ class YSYX2400012Mem(val addrWidth: Int) extends Module  {
       val dataInstr = Vec(2, new Rport(addrWidth,32))
       val dw = new  Wport(addrWidth,32)
    }) 
-
+   io := DontCare
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
-   io.dataInstr(0).addr := 0.U
-   io.dataInstr(1).addr := 0.U
+   
    io.dataInstr(0).data := 0.U
    io.dataInstr(1).data := 0.U
 }
