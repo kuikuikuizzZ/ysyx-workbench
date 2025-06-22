23187fa1af978d2a8ff74850603f25fa4cecca81
tracer-ysyx <tracer@ysyx.org>
2025-06-23 02:56:05 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:56:05 up 12:13,  4 users,  load average: 0.22, 0.29, 1.15

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -65,66 +65,15 @@ class MemResp(val data_width: Int) extends Bundle
    val data = Output(UInt(data_width.W))
 }
 
-class YSYX2400012Mem extends BlackBox with HasBlackBoxInline {
+class YSYX2400012Mem extends BlackBox with HasBlackBoxPath {
    val io = IO(new Bundle{
-      val dataInstr = Vec(2, new Rport(32,32))
+      // val dataInstr = Vec(2, new Rport(32,32))
       val dw = new  Wport(32,32)
       val clk = Input(Clock())
    }) 
-   setInline("ysyx2400012Mem.v",
-      """
-import "DPI-C" function void pmem_read(input int outaddr,input int length, output int dout);
-import "DPI-C" function void pmem_write(input int inaddr,input int length, input int din);
 
-module YSYX2400012Mem #(
-    ADDR_WIDTH = 32,
-    DATA_WIDTH = 32,
-    ORIGIN_ADDR=32'h80000000,
-    MEM_SIZE=32'h08000000
-) (
-    // // 全局时钟（根据Chisel的MemIo需补充）
-    input clk,
-    
-    // 指令读端口（Vec(2, Rport)）
-    input  [ADDR_WIDTH-1:0] dataInstr_0_addr,  // 端口0地址
-    input  [ADDR_WIDTH-1:0] dataInstr_1_addr,  // 端口1地址
-
-
-    // 数据写端口（dw: Wport）
-    input                  dw_en,           // 写使能 (原MemWEn)
-    input  [ADDR_WIDTH-1:0] dw_addr,         // 写地址
-    input  [DATA_WIDTH-1:0] dw_data,         // 写数据
-    input  [DATA_WIDTH-1:0] dw_len,        // 字节掩码 (原Length整合至mask)
-    output reg [DATA_WIDTH-1:0] dataInstr_0_data,   // 端口0数据
-    output reg [DATA_WIDTH-1:0] dataInstr_1_data   // 端口1数据
-);
-
-    // 1. 重构读写逻辑分离
-    //-----------------------------
-    // 写逻辑：使用dw_en触发pmem_write
-    always @(posedge clk) begin
-        if (dw_en) begin
-            pmem_write(dw_addr, dw_len, dw_data); // mask替代Length
-        end
-    end
-
-    // 读逻辑：双端口独立读取
-    reg [DATA_WIDTH-1:0] read_buf [0:1]; // 双缓冲避免组合环路
-
-    always @(*) begin
-        // 端口0读取
-        pmem_read(dataInstr_0_addr, 4, read_buf[0]); // 固定32位=4字节
-        assign dataInstr_0_data = read_buf[0];
-        
-        // 端口1读取
-        pmem_read(dataInstr_1_addr, 4, read_buf[1]); 
-        assign dataInstr_1_data = read_buf[1];
-    end
-
-endmodule""".stripMargin)
-   
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
-   // addPath("/home/uenui/code/github.com/OSCPU/ysyx-workbench/npc-chisel/npc/src/main/resources/YSYX2400012Mem.v")
+   addPath("/home/uenui/code/github.com/OSCPU/ysyx-workbench/npc-chisel/npc/src/main/resources/YSYX2400012Mem.v")
 }
 
 class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 21))(implicit val conf: YSYX24100012Config) extends Module
