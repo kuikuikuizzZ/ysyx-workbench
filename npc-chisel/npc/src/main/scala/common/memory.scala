b3efe95da472582e89a8fec4a8d1c9915bb242fa
tracer-ysyx <tracer@ysyx.org>
2025-06-23 02:18:04 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:18:04 up 11:35,  4 users,  load average: 7.80, 6.08, 3.99

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -65,15 +65,14 @@ class MemResp(val data_width: Int) extends Bundle
    val data = Output(UInt(data_width.W))
 }
 
-// class YSYX2400012Mem extends BlackBox with HasBlackBoxPath {
-class YSYX2400012Mem extends Module{
+class YSYX2400012Mem extends BlackBox with HasBlackBoxPath {
    val io = IO(new Bundle{
       val dataInstr = Vec(2, new Rport(32,32))
       val dw = new  Wport(32,32)
    }) 
-   // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
-   // addPath("/home/uenui/code/github.com/OSCPU/ysyx-workbench/npc-chisel/npc/src/main/scala/common/YSYX2400012Mem.v")
 
+   // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
+   addPath("/home/uenui/code/github.com/OSCPU/ysyx-workbench/npc-chisel/npc/src/main/scala/common/YSYX2400012Mem.v")
 }
 
 class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 21))(implicit val conf: YSYX24100012Config) extends Module
