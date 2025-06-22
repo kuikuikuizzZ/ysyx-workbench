8dbc66996b2f5f651615da4f356325a577ab462e
tracer-ysyx <tracer@ysyx.org>
2025-06-23 03:02:06 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:02:06 up 12:19,  4 users,  load average: 0.65, 0.60, 1.00

--- npc-chisel/npc/src/main/scala/Elaborate.scala
+++ npc-chisel/npc/src/main/scala/Elaborate.scala
@@ -1,6 +1,6 @@
 object Elaborate extends App {
   val firtoolOptions = Array(
-    "--mlir-print-ir-after-all",
+    "--mlir-print-ir-after-all 2",
     "--lowering-options=" + List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
