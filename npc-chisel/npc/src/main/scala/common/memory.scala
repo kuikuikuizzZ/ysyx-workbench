2a8601239abd4ceef83ad3d5a42d17d3bb4b2802
tracer-ysyx <tracer@ysyx.org>
2025-06-23 03:31:25 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:31:25 up 12:49,  4 users,  load average: 0.79, 0.69, 0.68

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -76,7 +76,7 @@ class MemResp(val data_width: Int) extends Bundle
 //    addPath("/home/uenui/code/github.com/OSCPU/ysyx-workbench/npc-chisel/npc/src/main/resources/YSYX2400012Mem.v")
 // }
 
-class YSYX2400012Mem(val addrWidth: Int) extends Module  {
+class Mymem(val addrWidth: Int) extends Module  {
    val io = IO(new Bundle{
       val dataInstr = Vec(2, new Rport(addrWidth,32))
       val dw = new  Wport(addrWidth,32)
@@ -84,7 +84,7 @@ class YSYX2400012Mem(val addrWidth: Int) extends Module  {
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
    
    io.dataInstr(0).data := 0.U
-   // io.dataInstr(1).data := 0.U
+   io.dataInstr(1).data := 0.U
 }
 
 class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 21))(implicit val conf: YSYX24100012Config) extends Module
@@ -95,7 +95,7 @@ class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 2
    })
    val num_bytes_per_line = 8
    val num_lines = num_bytes / num_bytes_per_line
-   val async_data = Module(new YSYX2400012Mem(32))
+   val async_data = Module(new Mymem(32))
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
    for (i <- 0 until num_core_ports)
    {
