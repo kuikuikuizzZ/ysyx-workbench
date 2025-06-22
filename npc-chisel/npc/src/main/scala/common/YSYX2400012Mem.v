8536ff7bd42875d308a8d7114e153ae12c6dbdb0
tracer-ysyx <tracer@ysyx.org>
2025-06-23 02:07:08 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:07:08 up 11:24,  4 users,  load average: 1.75, 2.31, 2.20

--- npc-chisel/npc/src/main/scala/common/YSYX2400012Mem.v
+++ npc-chisel/npc/src/main/scala/common/YSYX2400012Mem.v
@@ -11,24 +11,24 @@ module YSYX2400012Mem #(
     // input clk,
     
     // 指令读端口（Vec(2, Rport)）
-    input  [ADDR_WIDTH-1:0] dataInstr_0_addr,  // 端口0地址
-    output [DATA_WIDTH-1:0] dataInstr_0_data,   // 端口0数据
-    input  [ADDR_WIDTH-1:0] dataInstr_1_addr,  // 端口1地址
-    output [DATA_WIDTH-1:0] dataInstr_1_data,   // 端口1数据
+    input  [ADDR_WIDTH-1:0] io_dataInstr_0_addr,  // 端口0地址
+    output [DATA_WIDTH-1:0] io_dataInstr_0_data,   // 端口0数据
+    input  [ADDR_WIDTH-1:0] io_dataInstr_1_addr,  // 端口1地址
+    output [DATA_WIDTH-1:0] io_dataInstr_1_data,   // 端口1数据
 
     // 数据写端口（dw: Wport）
-    input                  dw_en,           // 写使能 (原MemWEn)
-    input  [ADDR_WIDTH-1:0] dw_addr,         // 写地址
-    input  [DATA_WIDTH-1:0] dw_data,         // 写数据
-    input  [DATA_WIDTH/8-1:0] dw_len        // 字节掩码 (原Length整合至mask)
+    input                  io_dw_en,           // 写使能 (原MemWEn)
+    input  [ADDR_WIDTH-1:0] io_dw_addr,         // 写地址
+    input  [DATA_WIDTH-1:0] io_dw_data,         // 写数据
+    input  [DATA_WIDTH/8-1:0] io_dw_len        // 字节掩码 (原Length整合至mask)
 );
 
 // 1. 重构读写逻辑分离
 //-----------------------------
 // 写逻辑：使用dw_en触发pmem_write
 always @(posedge clk) begin
-    if (dw_en) begin
-        pmem_write(dw_addr, dw_len, dw_data); // mask替代Length
+    if (io_dw_en) begin
+        pmem_write(io_dw_addr, io_dw_len, io_dw_data); // mask替代Length
     end
 end
 
@@ -37,12 +37,12 @@ reg [DATA_WIDTH-1:0] read_buf [0:1]; // 双缓冲避免组合环路
 
 always @(*) begin
     // 端口0读取
-    pmem_read(dataInstr_0_addr, 4, read_buf[0]); // 固定32位=4字节
-    assign dataInstr_0_data = read_buf[0];
+    pmem_read(io_dataInstr_0_addr, 4, read_buf[0]); // 固定32位=4字节
+    assign io_dataInstr_0_data = read_buf[0];
     
     // 端口1读取
-    pmem_read(dataInstr_1_addr, 4, read_buf[1]); 
-    assign dataInstr_1_data = read_buf[1];
+    pmem_read(io_dataInstr_1_addr, 4, read_buf[1]);
+    assign io_dataInstr_1_data = read_buf[1];
 end
 
 endmodule
