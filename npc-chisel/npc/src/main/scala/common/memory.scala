367ec2c7589e930d823766663722d354eb3d3fea
tracer-ysyx <tracer@ysyx.org>
2025-06-23 02:23:03 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:23:03 up 11:40,  4 users,  load average: 8.34, 7.53, 5.20

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -69,7 +69,7 @@ class YSYX2400012Mem extends BlackBox with HasBlackBoxPath {
    val io = IO(new Bundle{
       val dataInstr = Vec(2, new Rport(32,32))
       val dw = new  Wport(32,32)
-       val clk = Input(Clock())
+      val clk = Input(Clock())
    }) 
 
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
@@ -85,6 +85,7 @@ class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 2
    val num_bytes_per_line = 8
    val num_lines = num_bytes / num_bytes_per_line
    val async_data = Module(new YSYX2400012Mem())
+   async_data.io.clk := clock
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
    for (i <- 0 until num_core_ports)
    {
