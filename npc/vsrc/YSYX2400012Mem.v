fc6812dde66b90c912457f81296577439c6c29ef
tracer-ysyx <tracer@ysyx.org>
2025-06-23 02:48:12 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:48:12 up 12:05,  4 users,  load average: 0.12, 0.44, 1.77

--- npc/vsrc/YSYX2400012Mem.v
+++ npc/vsrc/YSYX2400012Mem.v
@@ -1,5 +1,5 @@
-import "DPI-C" function void pmem_read(input int outaddr,input int length, output int dout);
-import "DPI-C" function void pmem_write(input int inaddr,input int length, input int din);
+// import "DPI-C" function void pmem_read(input int outaddr,input int length, output int dout);
+// import "DPI-C" function void pmem_write(input int inaddr,input int length, input int din);
 
 module YSYX2400012Mem #(
     ADDR_WIDTH = 32,
