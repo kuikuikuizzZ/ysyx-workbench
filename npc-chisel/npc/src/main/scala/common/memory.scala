478c6eede41b9ea92954156fd42b6ac6f92da408
tracer-ysyx <tracer@ysyx.org>
2025-06-23 15:50:51 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:51 up 48 min,  2 users,  load average: 0.37, 0.22, 0.12

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -78,7 +78,7 @@ class MemResp(val data_width: Int) extends Bundle
 
 class Mymem(val addrWidth: Int) extends Module  {
    val io = IO(new Bundle{
-      val dataInstr = Vec(2,  Rport(addrWidth,32))
+      val dataInstr = new Rport(addrWidth,32)
       val dw = new  Wport(addrWidth,32)
    }) 
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
@@ -102,40 +102,43 @@ class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 2
    val num_lines = num_bytes / num_bytes_per_line
    val async_data = Module(new Mymem(32))
    // val async_data =  SyncReadMem(1024, Vec(4, UInt(32.W)))
-   for (i <- 0 until num_core_ports)
-   {
-      io.core_ports(i).resp.valid := io.core_ports(i).req.valid
-      io.core_ports(i).req.ready := true.B // for now, no back pressure 
-      async_data.io.dataInstr(i).addr := io.core_ports(i).req.bits.addr
-   }
+   // for (i <- 0 until num_core_ports)
+   // {
+   //    io.core_ports(i).resp.valid := io.core_ports(i).req.valid
+   //    io.core_ports(i).req.ready := true.B // for now, no back pressure 
+   //    async_data.io.dataInstr(i).addr := io.core_ports(i).req.bits.addr
+   // }
+   async_data.io.dataInstr.addr := io.core_ports(0).req.bits.addr
 
    /////////// DPORT 
-   val req_addri = io.core_ports(DPORT).req.bits.addr
+   // val req_addri = io.core_ports(DPORT).req.bits.addr
 
-   val req_typi = io.core_ports(DPORT).req.bits.typ
-   val resp_datai = async_data.io.dataInstr(DPORT).data
-   io.core_ports(DPORT).resp.bits.data := MuxCase(resp_datai,Array(
-      (req_typi === MT_B) -> Cat(Fill(24,resp_datai(7)),resp_datai(7,0)),
-      (req_typi === MT_H) -> Cat(Fill(16,resp_datai(15)),resp_datai(15,0)),
-      (req_typi === MT_BU) -> Cat(Fill(24,0.U),resp_datai(7,0)),
-      (req_typi === MT_HU) -> Cat(Fill(16,0.U),resp_datai(15,0))
-   ))
-   async_data.io.dw.en := false.B
-   when (io.core_ports(DPORT).req.valid && (io.core_ports(DPORT).req.bits.fcn === M_XWR))
-   {
-      async_data.io.dw.en := true.B
-      // 这里是配合下面 mask 设计的，目的让数据靠左对齐，0000 0001 -> 0001 0000
-      // async_data.io.dw.data := io.core_ports(DPORT).req.bits.data << (req_addri(1,0) << 3)
-      async_data.io.dw.data := io.core_ports(DPORT).req.bits.data 
-      async_data.io.dw.addr := Cat(req_addri(31,2),0.asUInt(2.W))
-      async_data.io.dw.len := Mux(req_typi === MT_B,1.U,
-                              Mux(req_typi === MT_H,2.U,4.U))
-   }
+   // val req_typi = io.core_ports(DPORT).req.bits.typ
+   // val resp_datai = async_data.io.dataInstr(DPORT).data
+   // io.core_ports(DPORT).resp.bits.data := MuxCase(resp_datai,Array(
+   //    (req_typi === MT_B) -> Cat(Fill(24,resp_datai(7)),resp_datai(7,0)),
+   //    (req_typi === MT_H) -> Cat(Fill(16,resp_datai(15)),resp_datai(15,0)),
+   //    (req_typi === MT_BU) -> Cat(Fill(24,0.U),resp_datai(7,0)),
+   //    (req_typi === MT_HU) -> Cat(Fill(16,0.U),resp_datai(15,0))
+   // ))
+   // async_data.io.dw.en := false.B
+   // when (io.core_ports(DPORT).req.valid && (io.core_ports(DPORT).req.bits.fcn === M_XWR))
+   // {
+   //    async_data.io.dw.en := true.B
+   //    // 这里是配合下面 mask 设计的，目的让数据靠左对齐，0000 0001 -> 0001 0000
+   //    // async_data.io.dw.data := io.core_ports(DPORT).req.bits.data << (req_addri(1,0) << 3)
+   //    async_data.io.dw.data := io.core_ports(DPORT).req.bits.data 
+   //    async_data.io.dw.addr := Cat(req_addri(31,2),0.asUInt(2.W))
+   //    async_data.io.dw.len := Mux(req_typi === MT_B,1.U,
+   //                            Mux(req_typi === MT_H,2.U,4.U))
+   // }
    /////////////////
 
    ///////////// IPORT
    if (num_core_ports == 2){
-      io.core_ports(IPORT).resp.bits.data := async_data.io.dataInstr(IPORT).data
+      // io.core_ports(IPORT).resp.bits.data := async_data.io.dataInstr(IPORT).data
+      io.core_ports(IPORT).resp.bits.data := async_data.io.dataInstr.data
+
    }
    ////////////
  
