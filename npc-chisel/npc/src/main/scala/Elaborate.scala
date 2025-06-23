8d129fcc09039552f199eee36b7a61e227bf5732
tracer-ysyx <tracer@ysyx.org>
2025-06-23 17:19:10 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:10 up  2:16,  3 users,  load average: 0.98, 0.48, 0.40

--- npc-chisel/npc/src/main/scala/Elaborate.scala
+++ npc-chisel/npc/src/main/scala/Elaborate.scala
@@ -12,5 +12,7 @@ object Elaborate extends App {
     ).reduce(_ + "," + _),
   )
   circt.stage.ChiselStage.emitSystemVerilogFile(
-    new Top(),args,firtoolOptions)
+    new Top(),    
+    Array("--split-verilog"),
+    firtoolOptions)
 }
