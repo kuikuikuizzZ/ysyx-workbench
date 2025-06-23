4c1dcc47c90ecbc681e7dbb1a695d002ab75520d
tracer-ysyx <tracer@ysyx.org>
2025-06-23 19:00:02 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:00:02 up  3:57,  3 users,  load average: 2.17, 1.09, 0.81

--- npc-chisel/npc/src/main/scala/dpath.scala
+++ npc-chisel/npc/src/main/scala/dpath.scala
@@ -38,7 +38,6 @@ class YSYX24100012Dpath(implicit conf: YSYX24100012Config) extends Module
    val jump_reg_target  = Wire(UInt(32.W))
    // val exception_target = Wire(UInt(32.W))
    val xcpt             = Wire(Bool())
-   val inst             = Wire(UInt(conf.xprlen.W))
 
    // PC Register
    pc_next := MuxCase(pc_plus4, Seq(
@@ -62,7 +61,7 @@ class YSYX24100012Dpath(implicit conf: YSYX24100012Config) extends Module
    io.imem.req.bits.addr := pc_reg
    io.imem.req.valid := true.B 
    // val inst = Mux(io.imem.resp.valid, io.imem.resp.bits.data, BUBBLE) 
-   inst := Mux(io.imem.resp.valid, io.imem.resp.bits.data, BUBBLE) 
+   val inst = Mux(io.imem.resp.valid, io.imem.resp.bits.data, BUBBLE) 
 
    // Decode
    val rs1_addr = inst(RS1_MSB, RS1_LSB)
