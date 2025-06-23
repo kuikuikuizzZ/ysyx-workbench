ba86b86badb6a9e237c24c7dd72b6983c0245010
tracer-ysyx <tracer@ysyx.org>
2025-06-23 20:08:24 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:24 up  5:06,  3 users,  load average: 1.14, 0.94, 0.84

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -120,7 +120,7 @@ class AsyncScratchPadMemory(val num_core_ports: Int,val num_bytes: Int = (1 << 2
       (req_typi === MT_BU) -> Cat(Fill(24,0.U),resp_datai(7,0)),
       (req_typi === MT_HU) -> Cat(Fill(16,0.U),resp_datai(15,0))
    ))
-   // async_data.io.dw := DontCare
+   async_data.io.dw := DontCare
    async_data.io.dw.en := false.B
    when (io.core_ports(DPORT).req.valid && (io.core_ports(DPORT).req.bits.fcn === M_XWR))
    {
