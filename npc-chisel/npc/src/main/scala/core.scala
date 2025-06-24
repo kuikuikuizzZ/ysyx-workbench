060de55b38cfba05555cc782a1d2b174c21eddef
tracer-ysyx <tracer@ysyx.org>
2025-06-25 02:51:39 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:51:39 up 16:00,  3 users,  load average: 2.07, 2.03, 2.01

--- npc-chisel/npc/src/main/scala/core.scala
+++ npc-chisel/npc/src/main/scala/core.scala
@@ -29,7 +29,7 @@ class Core(implicit val conf: YSYX24100012Config) extends Module
   io.dmem.req.valid := c.io.dmem.req.valid
   io.dmem.req.bits.typ := c.io.dmem.req.bits.typ
   io.dmem.req.bits.fcn := c.io.dmem.req.bits.fcn
-  io.halt = d.io.ebreak 
+  io.halt := d.io.ebreak 
 }
 
 
