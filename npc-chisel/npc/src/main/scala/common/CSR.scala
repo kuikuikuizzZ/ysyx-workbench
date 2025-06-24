47a96adf77f7a65786e0fde9cd188c0f74f8eb75
tracer-ysyx <tracer@ysyx.org>
2025-06-25 02:20:21 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:20:21 up 15:29,  3 users,  load average: 1.55, 1.62, 1.45

--- npc-chisel/npc/src/main/scala/common/CSR.scala
+++ npc-chisel/npc/src/main/scala/common/CSR.scala
@@ -204,7 +204,7 @@ class CSRFile(implicit val conf: YSYX24100012Config) extends Module
   //   }
   // }
 
-  val decoded_addr = read_mapping map { case (k, v) => k -> (io.decode.csr === k) }
+  val decoded_addr = read_mapping map { case (k.asUInt, v) => k -> (io.decode.csr === k) }
 
   val priv_sufficient = reg_mstatus.prv >= io.decode.csr(9,8)
   val read_only = io.decode.csr(11,10).andR
@@ -218,7 +218,7 @@ class CSRFile(implicit val conf: YSYX24100012Config) extends Module
   val insn_ret = system_insn && opcode(2) && priv_sufficient
   val insn_wfi = system_insn && opcode(5) && priv_sufficient
 
-  private def decodeAny(m: collection.mutable.LinkedHashMap[Int,_:Bits]): Bool = m.map { case(k: Int, _: Bits) => io.decode.csr === k }.reduce(_||_)
+  private def decodeAny(m: collection.mutable.LinkedHashMap[Int:Bits]): Bool = m.map { case(k: Int, _: Bits) => io.decode.csr === k }.reduce(_||_)
   io.decode.read_illegal := reg_mstatus.prv < io.decode.csr(9,8) || !decodeAny(read_mapping) ||
     (io.decode.csr.inRange(CSR.firstCtr, CSR.firstCtr + CSR.nCtr) || io.decode.csr.inRange(CSR.firstCtrH, CSR.firstCtrH + CSR.nCtr)) ||
     !reg_debug
