bf93e0af83c0fe0bc9c9e80f82f4022f0abc491e
tracer-ysyx <tracer@ysyx.org>
2025-06-25 15:48:13 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:13 up  3:51,  2 users,  load average: 0.15, 0.32, 0.29

--- npc-chisel/npc/src/main/resources/YSYX2400012Mem.v
+++ npc-chisel/npc/src/main/resources/YSYX2400012Mem.v
@@ -39,15 +39,14 @@ module YSYX2400012Mem #(
             dataInstr_0_data = 32'b0; // 重置端口0数据
             dataInstr_1_data = 32'b0; // 重置端口1数据
         end else begin
-            dataInstr_0_data = 32'b0; // 默认值，防止综合警告
-            dataInstr_1_data = 32'b0; // 默认值，防止综合警告
+            pmem_read(dataInstr_0_addr, 4, dataInstr_0_data); // 固定32位=4字节
+            // assign dataInstr_0_data = read_buf[0];
+            // 端口1读取
+            pmem_read(dataInstr_1_addr, 4, dataInstr_1_data);
+            // assign dataInstr_1_data = read_buf[1];
         end
         // 端口0读取
-        pmem_read(dataInstr_0_addr, 4, dataInstr_0_data); // 固定32位=4字节
-        // assign dataInstr_0_data = read_buf[0];
-        // 端口1读取
-        pmem_read(dataInstr_1_addr, 4, dataInstr_1_data);
-        // assign dataInstr_1_data = read_buf[1];
+
     end
 
 endmodule
