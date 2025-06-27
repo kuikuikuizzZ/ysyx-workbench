5447824f8313f570098f253e52d710a7c5999b6f
tracer-ysyx <tracer@ysyx.org>
2025-06-27 20:20:47 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:20:47 up  4:19,  5 users,  load average: 2.51, 1.39, 0.98

--- npc-chisel/npc/src/main/scala/core.scala
+++ npc-chisel/npc/src/main/scala/core.scala
@@ -21,9 +21,10 @@ class Core(implicit val conf: YSYX24100012Config) extends Module
   val inst_fetch = Module(new YSYX24100012InstFetch())
   c.io.ctl  <> d.io.ctl
   c.io.dat  <> d.io.dat
-  d.io.inst := inst_fetch.io.inst
   d.io.pc_io <> inst_fetch.io.pc_io
+  d.io.inst := inst_fetch.io.inst
   inst_fetch.io.targets <> d.io.targets
+  inst_fetch.io.pc_sel :=  c.io.ctl.pc_sel
   
   io.imem <> c.io.imem
   io.imem <> inst_fetch.io.imem
