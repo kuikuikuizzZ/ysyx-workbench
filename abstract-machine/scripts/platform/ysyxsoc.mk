1aa99a44073517a82f9edd897f6a9432715abbfc
wuwenhui.wuwh <wuwenhui.wuwh@bytedance.com>
2025-07-19 17:57:56 +0800

    feat(am): add ysyxsoc

---
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -0,0 +1,45 @@
+AM_SRCS := riscv/ysyxsoc/start.S \
+           riscv/ysyxsoc/trm.c \
+           riscv/ysyxsoc/ioe.c \
+           riscv/ysyxsoc/timer.c \
+           riscv/ysyxsoc/gpu.c  \
+           riscv/ysyxsoc/audio.c  \
+           riscv/ysyxsoc/input.c \
+           riscv/ysyxsoc/cte.c \
+           riscv/ysyxsoc/trap.S \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+CFLAGS    += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
+CFLAGS    += -I$(AM_HOME)/am/src/riscv
+
+LDSCRIPTS += $(AM_HOME)/scripts/linker_ysyx_soc.ld
+LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+YSYXSOCFLAGS += -l $(shell dirname $(IMAGE).elf)/ysyxsoc-log.txt
+# YSYXSOCFLAGS += -e $(IMAGE).elf
+# YSYXSOCFLAGS += -b
+
+
+MAINARGS_MAX_LEN = 64
+MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
+CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=\""$(MAINARGS_PLACEHOLDER)"\"
+
+insert-arg: image
+	@python3 $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
+
+image: image-dep
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: insert-arg
+	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(YSYXSOCFLAGS)" IMG=$(IMAGE).bin
+#$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(YSYXSOCFLAGS)" IMG=$(IMAGE).bin
+
+gdb: insert-arg
+	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) gdb IMG=$(IMAGE).bin
+#$(MAKE) -C $(NPC_HOME) ISA=$(ISA) gdb  ARGS="$(YSYXSOCFLAGS)" IMG=$(IMAGE).bin
+
+.PHONY: insert-arg
