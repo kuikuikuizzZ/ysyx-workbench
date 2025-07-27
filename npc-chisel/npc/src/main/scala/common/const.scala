a7a76cd390f98ed7e42ed7b885646e9747a1fcf0
wuwenhui.wuwh <wuwenhui.wuwh@bytedance.com>
2025-07-27 21:03:55 +0800

    feat(npc-chisel): update flash base

--- npc-chisel/npc/src/main/scala/common/const.scala
+++ npc-chisel/npc/src/main/scala/common/const.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 trait PrivilegedConstants
 {
    val MTVEC = 0x100
-   val START_ADDR = "h2000_0000".U
+   val START_ADDR = "h3000_0000".U
    // val START_ADDR = "h8000_0000".U
 
    val MSTATUS = 0x1800.U
