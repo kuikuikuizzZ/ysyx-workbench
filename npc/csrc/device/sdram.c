d9bf3b6e121ad614ae02efc175b66e056cfedcb1
wuwenhui.wuwh <wuwenhui.wuwh@bytedance.com>
2025-08-05 16:39:36 +0800

    feat(npc): add sdram word extend

--- npc/csrc/device/sdram.c
+++ npc/csrc/device/sdram.c
@@ -3,7 +3,9 @@
 #include <device/device.h>
 #include <memory/memory.h>
 #define SRAM_BIT_LENGTH 2
-static uint16_t npc_sdram[CONFIG_SDRAM_BA_BITS][CONFIG_SDRAM_ROW_BITS][CONFIG_SDRAM_COL_BITS][SRAM_BIT_LENGTH] = {0};
+#define SRAM_BIT_WORD_EXTEND 2
+
+static uint16_t npc_sdram[CONFIG_SDRAM_ROW_BITS][SRAM_BIT_WORD_EXTEND][CONFIG_SDRAM_BA_BITS][CONFIG_SDRAM_COL_BITS][SRAM_BIT_LENGTH] = {0};
 
 void init_sdram() {
     return;
@@ -15,20 +17,20 @@ const uint16_t masks[4] = {0xffff,0xff00,0x00ff,0x0000};
 extern "C" {
 #endif
 #ifdef CONFIG_HAS_SDRAM
-    void sdram_read(int32_t bit, int32_t row,int32_t col,int32_t ba,int32_t dqm, uint32_t *data){
-        uint16_t ret = npc_sdram[ba][row][col][bit];
+    void sdram_read(int32_t word,int32_t bit, int32_t row,int32_t col,int32_t ba,int32_t dqm, uint32_t *data){
+        uint16_t ret = npc_sdram[row][word][ba][col][bit];
         uint16_t mask = masks[dqm];
         *data = (ret & mask);
-        // printf("read sdram [ba][row][col] %x %x %x data %x dqm %d \n", ba,row,col,*data,dqm );
+        printf("read  [row][word][ba][col] %x %x %x %x data %x dqm %d mask %08x \n", row,word,ba,col, npc_sdram[row][word][ba][col][bit],dqm,mask );
     }
 
-    void sdram_write(int32_t bit, int32_t row,int32_t col,int32_t ba,int32_t dqm, uint32_t data){
+    void sdram_write(int32_t word,int32_t bit, int32_t row,int32_t col,int32_t ba,int32_t dqm, uint32_t data){
         // printf("sdram_write: address %x x length %d, data %x \n", addr,length,data);
-        uint16_t ret = npc_sdram[ba][row][col][bit];
+        uint16_t ret = npc_sdram[row][word][ba][col][bit];
         uint16_t mask = masks[dqm];
-        npc_sdram[ba][row][col][bit] = (ret & ~mask) | (data & mask);
+        npc_sdram[row][word][ba][col][bit] = (ret & ~mask) | (data & mask);
         // uint32_t new_data =  host_read(guest_to_sdram(addr), 4);
-        // printf("write [ba][row][col] %x %x %x data %x dqm %d mask %08x \n", ba,row,col,npc_sdram[ba][row][col][bit],dqm,mask );
+        printf("write [row][word][ba][col] %x %x %x %x data %x dqm %d mask %08x \n", row,word,ba,col, npc_sdram[row][word][ba][col][bit],dqm,mask );
 
     }
 #endif
