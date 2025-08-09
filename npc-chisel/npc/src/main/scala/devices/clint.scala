0aec09cb507832fce56ba4ba508e0463db6d3bd0
tracer-ysyx <tracer@ysyx.org>
2025-08-09 16:47:31 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:31 up 1 day, 23:55,  4 users,  load average: 0.72, 0.98, 0.99

--- npc-chisel/npc/src/main/scala/devices/clint.scala
+++ npc-chisel/npc/src/main/scala/devices/clint.scala
@@ -15,7 +15,7 @@ object  CLINTS{
 
 class ysyx_24100012_AXI4CLINT(implicit val conf: ysyx_24100012_Config) extends Module { 
     val io = IO(new Bundle() { 
-        val axi_io = Flipped(new AXI4LiteIo())
+        val axi_io = (new AXI4LiteIo())
         val reset = Input(Bool())
         val clock = Input(Clock())
     })
