91eae5150304bfa56a6c09941da453ff2d3bab37
tracer-ysyx <tracer@ysyx.org>
2025-08-11 11:40:44 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:40:44 up 1 day,  2:42,  4 users,  load average: 7.96, 7.90, 8.02

--- npc-chisel/npc/src/main/scala/common/axi.scala
+++ npc-chisel/npc/src/main/scala/common/axi.scala
@@ -122,7 +122,7 @@ class ysyx_24100012_AXI4LiteMaster (implicit val conf: ysyx_24100012_Config) ext
     awfire  :=  Mux(wstate === ws_wait_bvalid,  false.B, (io.axi_io.aw.valid && io.axi_io.aw.ready) || awfire)
     wfire   :=  Mux(wstate === ws_wait_bvalid,  false.B, (io.axi_io.w.valid && io.axi_io.w.ready) || wfire)
     bfire   :=  Mux(wstate === ws_idle,         false.B,        (io.axi_io.b.valid && io.axi_io.b.ready) || bfire)
-    arfire  :=  Mux(rstate === rs_wait_rlast,   false.B,  (io.axi_io.ar.valid && io.axi_io.ar.ready) || arfire)
+    arfire  :=  Mux(rstate === rs_wait_rlast || io.axi_io.r.last,   false.B,  (io.axi_io.ar.valid && io.axi_io.ar.ready) || arfire)
     rfire   :=  Mux(rstate === rs_idle,         false.B,  (io.axi_io.r.valid && io.axi_io.r.ready) || rfire)
 
 
