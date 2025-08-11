ff06f44d01ce0f4ee4679af77b1cbc794a47496a
kuikuikuizzZ <kimmmywu@gmail.com>
2025-08-11 09:40:13 +0800

    feat(am): add keyboard

--- abstract-machine/am/src/platform/ysyxsoc/ioe/input.c
+++ abstract-machine/am/src/platform/ysyxsoc/ioe/input.c
@@ -1,17 +1,93 @@
 #include <am.h>
 #include <ysyxsoc.h>
 #include <riscv.h>
+#include <klib.h>
 
-#define KEYDOWN_MASK 0x8000
+// #define KEYDOWN_MASK 0x8000
+// #define KEYDOWN_MASK 0x000000ff
+#define BREAK_CODE 0xF0
+// void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+//   uint32_t key=inl(KBD_ADDR);
+
+//   if (key==AM_KEY_NONE){
+//     kbd->keycode = AM_KEY_NONE;
+//     kbd->keydown = 0;
+//   } else{
+//     kbd->keydown = (key&KEYDOWN_MASK)==KEYDOWN_MASK;
+//     kbd->keycode = (kbd->keydown)?key-KEYDOWN_MASK:key;
+//   }
+// }
+char scancode_to_ascii(uint8_t scan_byte);
+
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t key=inl(KBD_ADDR);
+    char code = inl(KEYBOARD_BASE);
+    char key = 0;
+    if (code == BREAK_CODE) {
+        kbd->keydown = 0;
+        code = inl(KEYBOARD_BASE);
+    } else{
+        kbd->keydown = 1;
+    }     
+    key = scancode_to_ascii(code);
+    if (key == 0) {
+        kbd->keycode = 0;
+        // putch('-');
+    }
+    kbd->keycode = key;
+}
 
-  if (key==AM_KEY_NONE){
-    kbd->keycode = AM_KEY_NONE;
-    kbd->keydown = 0;
-  } else{
-    kbd->keydown = (key&KEYDOWN_MASK)==KEYDOWN_MASK;
-    kbd->keycode = (kbd->keydown)?key-KEYDOWN_MASK:key;
-  }
+
+static const char BASE_KEYMAP[128] = {
+    [0x00] = 0,      [0x01] = 0,      [0x02] = 0,      [0x03] = 0,
+    [0x04] = 0,      [0x05] = 0,      [0x06] = 0,      [0x07] = 0,
+    [0x08] = 0,      [0x09] = 0,      [0x0A] = 0,      [0x0B] = 0,
+    [0x0C] = 0,      [0x0D] = '\t',   [0x0E] = '`',    [0x0F] = 0,
+    [0x10] = 0,      [0x11] = 0,      [0x12] = 0,      [0x13] = 0,
+    [0x14] = 0,      [0x15] = 'q',    [0x16] = '1',    [0x17] = 0,
+    [0x18] = 0,      [0x19] = 0,      [0x1A] = 'z',    [0x1B] = 's',
+    [0x1C] = 'a',    [0x1D] = 'w',    [0x1E] = '2',    [0x1F] = 0,
+    [0x20] = 0,      [0x21] = 'c',    [0x22] = 'x',    [0x23] = 'd',
+    [0x24] = 'e',    [0x25] = '4',    [0x26] = '3',    [0x27] = 0,
+    [0x28] = 0,      [0x29] = ' ',    [0x2A] = 'v',    [0x2B] = 'f',
+    [0x2C] = 't',    [0x2D] = 'r',    [0x2E] = '5',    [0x2F] = 0,
+    [0x30] = 0,      [0x31] = 'n',    [0x32] = 'b',    [0x33] = 'h',
+    [0x34] = 'g',    [0x35] = 'y',    [0x36] = '6',    [0x37] = 0,
+    [0x38] = 0,      [0x39] = 0,      [0x3A] = 'm',    [0x3B] = 'j',
+    [0x3C] = 'u',    [0x3D] = '7',    [0x3E] = '8',    [0x3F] = 0,
+    [0x40] = 0,      [0x41] = ',',    [0x42] = 'k',    [0x43] = 'i',
+    [0x44] = 'o',    [0x45] = '0',    [0x46] = '9',    [0x47] = 0,
+    [0x48] = 0,      [0x49] = '.',    [0x4A] = '/',    [0x4B] = 'l',
+    [0x4C] = ';',    [0x4D] = 'p',    [0x4E] = '-',    [0x4F] = 0,
+    [0x50] = 0,      [0x51] = 0,      [0x52] = '\'',   [0x53] = 0,
+    [0x54] = '[',    [0x55] = '=',    [0x56] = 0,      [0x57] = 0,
+    [0x58] = 0,      [0x59] = 0,      [0x5A] = '\n',   [0x5B] = ']',
+    [0x5C] = 0,      [0x5D] = '\\',   [0x5E] = 0,      [0x5F] = 0,
+    [0x60] = 0,      [0x61] = 0,      [0x62] = 0,      [0x63] = 0,
+    [0x64] = 0,      [0x65] = 0,      [0x66] = '\b',   [0x67] = 0,
+    [0x68] = 0,      [0x69] = '1',    [0x6A] = 0,      [0x6B] = '4',
+    [0x6C] = '7',    [0x6D] = 0,      [0x6E] = 0,      [0x6F] = 0,
+    [0x70] = '0',    [0x71] = '.',    [0x72] = '2',    [0x73] = '5',
+    [0x74] = '6',    [0x75] = '8',    [0x76] = 0x1B,   [0x77] = 0,
+    [0x78] = 0,      [0x79] = '+',    [0x7A] = '3',    [0x7B] = '-',
+    [0x7C] = '*',    [0x7D] = '9',    [0x7E] = 0,      [0x7F] = 0
+};
+
+
+char scancode_to_ascii(uint8_t scancode) {
+    // 0xF0是断码标志
+    if (scancode == 0xF0) return 0xF0;
+    
+    // E0扩展序列标志 (单独字节不产生ASCII)
+    if (scancode == 0xE0 || scancode == 0xE1) return 0;
+    
+    // 0x76是ESC键的特殊扫描码
+    if (scancode == 0x76) return 0x1B;
+    
+    // 从映射表中获取ASCII值
+    if (scancode < sizeof(BASE_KEYMAP)) {
+        return BASE_KEYMAP[scancode];
+    }
+    
+    return 0; // 未知扫描码返回0
 }
--- abstract-machine/am/src/platform/ysyxsoc/ioe/ioe.c
+++ abstract-machine/am/src/platform/ysyxsoc/ioe/ioe.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
-
+#include <klib.h>
 void __am_timer_init();
 void __am_gpu_init();
 void __am_audio_init();
@@ -17,39 +17,46 @@ void __am_audio_status(AM_AUDIO_STATUS_T *);
 void __am_audio_play(AM_AUDIO_PLAY_T *);
 void __am_uart_rx(AM_UART_RX_T *recv);
 void __am_uart_tx(AM_UART_TX_T *send);
-static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
-static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
-static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 
-typedef void (*handler_t)(void *buf);
-static void *lut[128] = {
-  [AM_TIMER_CONFIG] = __am_timer_config,
-  [AM_TIMER_RTC   ] = __am_timer_rtc,
-  [AM_TIMER_UPTIME] = __am_timer_uptime,
-  [AM_INPUT_CONFIG] = __am_input_config,
-  [AM_INPUT_KEYBRD] = __am_input_keybrd,
-  [AM_UART_CONFIG]  = __am_uart_config,
-  [AM_GPU_CONFIG  ] = __am_gpu_config,
-  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
-  [AM_GPU_STATUS  ] = __am_gpu_status,
-  [AM_AUDIO_CONFIG] = __am_audio_config,
-  [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
-  [AM_AUDIO_STATUS] = __am_audio_status,
-  [AM_AUDIO_PLAY  ] = __am_audio_play,
-  [AM_UART_TX     ] = __am_uart_tx,
-  [AM_UART_RX     ] = __am_uart_rx,
-};
+static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = false; }
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
+static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
+
+typedef void (*handler_t)(void *buf) ; 
+
+static handler_t lut[128];
 
 static void fail(void *buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
+  lut[AM_TIMER_CONFIG] = (handler_t)__am_timer_config,
+  lut[AM_TIMER_RTC   ] = (handler_t)__am_timer_rtc,
+  lut[AM_TIMER_UPTIME] = (handler_t)__am_timer_uptime,
+  lut[AM_INPUT_CONFIG] = (handler_t)__am_input_config,
+  lut[AM_INPUT_KEYBRD] = (handler_t)__am_input_keybrd,
+  lut[AM_UART_CONFIG]  = (handler_t)__am_uart_config,
+  lut[AM_GPU_CONFIG  ] = (handler_t)__am_gpu_config,
+  lut[AM_GPU_FBDRAW  ] = (handler_t)__am_gpu_fbdraw,
+  lut[AM_GPU_STATUS  ] = (handler_t)__am_gpu_status,
+  lut[AM_AUDIO_CONFIG] = (handler_t)__am_audio_config,
+  lut[AM_AUDIO_CTRL  ] = (handler_t)__am_audio_ctrl,
+  lut[AM_AUDIO_STATUS] = (handler_t)__am_audio_status,
+  lut[AM_AUDIO_PLAY  ] = (handler_t)__am_audio_play,
+  lut[AM_UART_TX     ] = (handler_t)__am_uart_tx,
+  lut[AM_UART_RX     ] = (handler_t)__am_uart_rx;
+  
   for (int i = 0; i < LENGTH(lut); i++)
-    if (!lut[i]) lut[i] = fail;
+  if (!lut[i]) lut[i] = fail;
+
   __am_timer_init();
   // __am_gpu_init();
   // __am_audio_init();
+  printf("%d %d\n",AM_TIMER_CONFIG,AM_TIMER_UPTIME);
   return true;
 }
 
-void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) { 
+  void (*handler_t)(void *) = lut[reg];
+  handler_t(buf);
+}
 void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
