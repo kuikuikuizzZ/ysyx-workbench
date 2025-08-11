e5eb2c11c79d92ae1222008ce82e1722d13990c4
tracer-ysyx <tracer@ysyx.org>
2025-08-11 14:35:57 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:35:57 up 1 day,  5:38,  4 users,  load average: 8.94, 8.22, 8.05

--- npc-chisel/npc/src/main/scala/instFetch.scala
+++ npc-chisel/npc/src/main/scala/instFetch.scala
@@ -71,13 +71,12 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
   val inst_reg = RegEnable(io.port.resp.bits.data,BUBBLE,io.port.resp.valid)
   val inst = Mux(io.port.resp.valid,io.port.resp.bits.data,inst_reg)
 
-  io.inst := inst_reg
+  io.inst := inst
   // val pc_reg_reg = RegNext(pc_reg)
   io.pc_io.pc_plus4 := (pc_reg + 4.asUInt(conf.xprlen.W)) 
   io.pc_io.pc := pc_reg       
 
   // val valid = RegInit(false.B)
   val valid = RegNext(io.port.resp.valid,false.B)
-  val valid_reg = RegNext(valid,false.B)
   io.valid := valid     
 }
