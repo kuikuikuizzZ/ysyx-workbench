3f0e5398c126448d20e0464bc9f4e7d44224f329
tracer-ysyx <tracer@ysyx.org>
2025-08-12 10:50:17 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:17 up  1:23,  3 users,  load average: 1.39, 0.49, 0.23

--- npc/svsrc/debug_port.v
+++ npc/svsrc/debug_port.v
@@ -1,14 +1,38 @@
 
      import "DPI-C" function void dpi_port(input int halt, input int pc, input int inst);
+     import "DPI-C" function void lsu_port( input enable, input int fcn,input int addr, input int data);
+
      module debug_port(
         input clock,
         input reset,
         input halt, 
         input [31:0] pc,
-        input [31:0] inst);
+        input [31:0] inst,
+        input [31:0] lsu_port_addr,
+        input [31:0] lsu_port_rdata,
+        input [31:0] lsu_port_wdata,
+        input lsu_port_mem_en,
+        input lsu_port_fcn,
+        input lsu_port_valid
+        );
+
         wire [31:0] expand_halt = {31'b0,halt};
         always @(posedge clock) begin
             dpi_port(expand_halt, pc, inst);
+        end
+
+        wire [31:0] expand_fcn = {31'b0,lsu_port_fcn};
+        wire [31:0] expand_mem_en = {31'b0,lsu_port_mem_en};
+        wire [31:0] expand_valid = {31'b0,lsu_port_valid};}
+    }
+        always @(posedge clock) begin
+            if (lsu_port_mem_en && lsu_port_fcn == 1'b1) begin
+                lsu_port(expand_mem_en,expand_fcn, lsu_port_addr, lsu_port_wdata);
+            end else if (lsu_port_valid && lsu_port_fcn == 1'b0) begin
+                lsu_port(expand_valid,expand_fcn, lsu_port_addr, lsu_port_rdata);
+            end else begin
+                lsu_port(32'd0,32'd0,32'd0,32'd0);
+            end
         end
      endmodule
      
