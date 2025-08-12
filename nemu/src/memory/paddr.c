f97e674282d6bd99ed1ba8e037c9e0b037f80c23
tracer-ysyx <tracer@ysyx.org>
2025-08-12 20:29:17 +0800

    > run NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:17 up 11:02,  3 users,  load average: 0.15, 0.06, 0.04

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -179,9 +179,9 @@ word_t paddr_read(paddr_t addr, int len) {
     likely(in_flash_pmem(addr)) ||
     likely(in_psram_pmem(addr))   ||
     likely(in_sdram_pmem(addr))) return pmem_read(addr, len);
-  // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   else if (likely(in_uart_pmem(addr)))  return 0x20;
   else if (likely(in_clint_pmem(addr))) return 0;
+    IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr); 
   return 0;
 }
