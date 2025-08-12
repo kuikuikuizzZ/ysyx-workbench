70194bdac96eeb292d3db8b6b17c85b43115e0da
tracer-ysyx <tracer@ysyx.org>
2025-08-12 21:13:51 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:51 up 11:47,  3 users,  load average: 0.19, 0.15, 0.18

--- npc-chisel/npc/src/main/scala/common/axi.scala
+++ npc-chisel/npc/src/main/scala/common/axi.scala
@@ -153,7 +153,7 @@ class ysyx_24100012_AXI4LiteMaster (implicit val conf: ysyx_24100012_Config) ext
         is (rs_wait_arready){ rstate := Mux(arfire, rs_wait_rlast, rs_wait_arready)}
         is (rs_wait_rlast){ 
             // rlast is high when rvalid is high
-            rstate := Mux(io.axi_io.r.last || || (rstate === rs_wait_rlast) && (rfire), rs_idle, rs_wait_rlast)
+            rstate := Mux(io.axi_io.r.last || (rstate === rs_wait_rlast) && (rfire), rs_idle, rs_wait_rlast)
             when (io.axi_io.r.valid){ io.resp.bits.data  := io.axi_io.r.data}
         }
     }
