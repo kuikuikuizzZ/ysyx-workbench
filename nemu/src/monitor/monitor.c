d9dc7a6da8fae816dd537c841b501ee899c6caca
tracer-ysyx <tracer@ysyx.org>
2025-08-13 17:56:26 +0800

    > run NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:26 up 1 day,  8:30,  3 users,  load average: 0.00, 0.04, 0.03

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -134,6 +134,7 @@ void init_monitor(int argc, char *argv[]) {
 
   IFDEF(CONFIG_DIFFTEST,printf("diff_so_file = %s\n",diff_so_file));
   /* Initialize differential testing. */
+  diff_so_file = "tools/spike-diff/build/riscv32-spike-so";
   init_difftest(diff_so_file, img_size, difftest_port);
 
   /* Initialize ftrace */
