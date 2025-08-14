dc429e01f4d65e012fe8d86b1ae94868842ff77a
tracer-ysyx <tracer@ysyx.org>
2025-08-14 21:35:14 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:14 up 2 days, 12:08,  3 users,  load average: 1.68, 1.47, 1.24

--- npc-chisel/npc/src/main/scala/Docoder.scala
+++ npc-chisel/npc/src/main/scala/Docoder.scala
@@ -33,14 +33,14 @@ class CtlToWBIo(implicit val conf: ysyx_24100012_Config) extends Bundle()
 }
 
 class CtrlDebugPort (implicit val conf: ysyx_24100012_Config) extends Bundle(){
-   val csrCount    = Output(conf.perfCountBits)
-   val storeCount  = Output(conf.perfCountBits)
-   val loadCount   = Output(conf.perfCountBits)
-   val itypeCount  = Output(conf.perfCountBits)
-   val rtypeCount  = Output(conf.perfCountBits)
-   val jtypeCount  = Output(conf.perfCountBits)
-   val utypeCount  = Output(conf.perfCountBits)
-   val otherCount  = Output(conf.perfCountBits)
+   val csrCount    = Output(UInt(conf.perfCountBits))
+   val storeCount  = Output(UInt(conf.perfCountBits))
+   val loadCount   = Output(UInt(conf.perfCountBits))
+   val itypeCount  = Output(UInt(conf.perfCountBits))
+   val rtypeCount  = Output(UInt(conf.perfCountBits))
+   val jtypeCount  = Output(UInt(conf.perfCountBits))
+   val utypeCount  = Output(UInt(conf.perfCountBits))
+   val otherCount  = Output(UInt(conf.perfCountBits))
 }
 
 class CpathIo(implicit val conf: ysyx_24100012_Config) extends Bundle()
