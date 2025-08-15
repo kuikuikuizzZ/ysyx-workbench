1346e1050c65b5b62ddc67e029e44b30e3254148
tracer-ysyx <tracer@ysyx.org>
2025-08-15 16:33:42 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:42 up 3 days,  7:07,  3 users,  load average: 0.19, 0.23, 0.19

--- npc-chisel/npc/src/main/scala/core.scala
+++ npc-chisel/npc/src/main/scala/core.scala
@@ -97,8 +97,9 @@ class ysyx_24100012 extends Module
   debug.io.inst := inst_fetch.io.inst
   debug.io.lsu_port := lsu.io.debug
   
-  perfEvent.io.clock := clock
-  perfEvent.io.reset := reset
+  perfEvent.io.clock    := clock
+  perfEvent.io.reset    := reset
+  perfEvent.io.finish   := c.io.finish
   perfEvent.io.ifu_port := inst_fetch.io.debug
   perfEvent.io.ctl_port := c.io.debug
   perfEvent.io.lsu_port := lsu.io.debug
