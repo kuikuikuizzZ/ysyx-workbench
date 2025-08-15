2583ce802f5870c0e88c5c6a8b2d7ab020778999
tracer-ysyx <tracer@ysyx.org>
2025-08-15 16:34:15 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:15 up 3 days,  7:07,  3 users,  load average: 0.26, 0.24, 0.19

--- npc-chisel/npc/src/main/scala/debug.scala
+++ npc-chisel/npc/src/main/scala/debug.scala
@@ -63,8 +63,9 @@ class DebugPort() (implicit val conf: ysyx_24100012_Config)extends BlackBox with
 
 class PerfEventPort() (implicit val conf: ysyx_24100012_Config)extends BlackBox with HasBlackBoxInline{ 
      val io = IO(new Bundle {
-        val clock = Input(Clock())
-        val reset = Input(Bool()) 
+        val clock       = Input(Clock())
+        val reset       = Input(Bool()) 
+        val finish      = Input(Bool())
         val lsu_port    = Flipped(new LSUDebugPort()) 
         val ifu_port    = Flipped(new IFUDebugPort())
         val wbu_port    = Flipped(new WBUDebugPort())
