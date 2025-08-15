42656807cd86429650501c991871a7817671f354
tracer-ysyx <tracer@ysyx.org>
2025-08-15 16:31:24 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:31:24 up 3 days,  7:05,  3 users,  load average: 0.11, 0.19, 0.17

---
+++ npc/svsrc/ysyx_24100012_SyncMemHalf.v
@@ -0,0 +1,56 @@
+
+module ysyx_24100012_SyncMemHalf #(
+    ADDR_WIDTH = 32,
+    DATA_WIDTH = 32,
+    ORIGIN_ADDR=32'h80000000,
+    MEM_SIZE=32'h08000000
+) (
+    // // // 全局时钟（根据Chisel的MemIo需补充）
+    // input clk,
+    input clock,
+    input reset,
+    // 指令读端口（Vec(2, Rport)）
+    input  [ADDR_WIDTH-1:0] dataInstr_0_addr,  // 端口0地址
+    input  [ADDR_WIDTH-1:0] dataInstr_1_addr,  // 端口1地址
+    input  dataInstr_0_en,  // 端口0使能
+    input  dataInstr_1_en,  // 端口1使能
+
+    // 数据写端口（dw: Wport）
+    input                  dw_en,           // 写使能 (原MemWEn)
+    input  [ADDR_WIDTH-1:0] dw_addr,         // 写地址
+    input  [DATA_WIDTH-1:0] dw_data,         // 写数据
+    input  [DATA_WIDTH-1:0] dw_len,        // 字节掩码 (原Length整合至mask)
+    output  reg [DATA_WIDTH-1:0] dataInstr_0_data,   // 端口0数据
+    output  reg [DATA_WIDTH-1:0] dataInstr_1_data   // 端口1数据
+);
+
+    always @(posedge clock) begin
+        if (reset) begin
+            dataInstr_1_data = 32'b0; // 重置端口1数据
+        end else begin
+            // 端口1读取
+            pmem_read(dataInstr_1_addr, 4, dataInstr_1_data);
+            // assign dataInstr_1_data = read_buf[1];
+        end
+
+    end
+
+    always @(*) begin
+        // 端口0写入
+        if (dw_en) begin
+            pmem_write(dw_addr, dw_len, dw_data); // mask替代Length
+        end 
+
+    end
+
+    always @(*) begin
+        // 端口0读取
+        if (dataInstr_0_en) begin
+            pmem_read(dataInstr_0_addr, 4, dataInstr_0_data);
+        end else begin
+            dataInstr_0_data = 32'b0; // 重置端口0数据
+        end
+    end
+
+
+endmodule
