2513d0aa2fb7b89642283c71a731ead642c7c97e
tracer-ysyx <tracer@ysyx.org>
2025-08-16 10:51:07 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:51:07 up 4 days,  1:24,  4 users,  load average: 0.35, 0.31, 0.17

--- npc-chisel/npc/src/main/scala/core.scala
+++ npc-chisel/npc/src/main/scala/core.scala
@@ -87,23 +87,23 @@ class ysyx_24100012 extends Module
   io.slave.b.resp := 0.U
   io.slave.b.id := 0.U
 
-  /////// debug port
-  // val debug = Module(new ysyx_24100012_DebugPort())
-  // val perfEvent = Module(new ysyx_24100012_PerfEventPort())
-  // debug.io.clock := clock
-  // debug.io.reset := reset
-  // debug.io.halt := halt
-  // debug.io.pc := inst_fetch.io.pc_io.pc
-  // debug.io.inst := inst_fetch.io.inst
-  // debug.io.lsu_port := lsu.io.debug
+  ///// debug port
+  val debug = Module(new ysyx_24100012_DebugPort())
+  val perfEvent = Module(new ysyx_24100012_PerfEventPort())
+  debug.io.clock := clock
+  debug.io.reset := reset
+  debug.io.halt := halt
+  debug.io.pc := inst_fetch.io.pc_io.pc
+  debug.io.inst := inst_fetch.io.inst
+  debug.io.lsu_port := lsu.io.debug
   
-  // perfEvent.io.clock    := clock
-  // perfEvent.io.reset    := reset
-  // perfEvent.io.finish   := c.io.finish
-  // perfEvent.io.ifu_port := inst_fetch.io.debug
-  // perfEvent.io.ctl_port := c.io.debug
-  // perfEvent.io.lsu_port := lsu.io.debug
-  // perfEvent.io.wbu_port := wbu.io.debug
+  perfEvent.io.clock    := clock
+  perfEvent.io.reset    := reset
+  perfEvent.io.finish   := c.io.finish
+  perfEvent.io.ifu_port := inst_fetch.io.debug
+  perfEvent.io.ctl_port := c.io.debug
+  perfEvent.io.lsu_port := lsu.io.debug
+  perfEvent.io.wbu_port := wbu.io.debug
 }
 
 
