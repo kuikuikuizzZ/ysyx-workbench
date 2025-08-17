8d29f77f1314c348cd712807ba089f2af2233045
tracer-ysyx <tracer@ysyx.org>
2025-08-17 17:38:37 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:37 up 5 days,  8:12,  4 users,  load average: 1.04, 1.11, 1.17

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -33,23 +33,29 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
     val mem = SyncReadMem(conf.ICacheSize,UInt(conf.ICacheBlockBits.W))
     val ren = RegInit(false.B)
     val reg_req_valid = RegNext(io.req_valid,false.B)
-    val cache_data = mem.read(io.pc(5,2),io.req_valid || ren)
-    val cache_valid = cache_data(58)
-    val hit = cache_valid && (io.pc(31,6) === cache_data(57,32))
     
+    val in_sdram = io.pc >= SDRAM_BASE && io.pc < SDRAM_BASE + SDRAM_SIZE
+    val in_flash = io.pc >= FLASH_BASE && io.pc < FLASH_BASE + FLASH_SIZE
+    val in_psram = io.pc >= PSRAM_BASE && io.pc < PSRAM_BASE + PSRAM_SIZE
+    val in_mem = in_sdram || in_flash || in_psram
+
+    val cache_data = mem.read(io.pc(5,2),(io.req_valid || ren)&&in_mem)
+    val cache_valid = cache_data(58) && in_mem
+    val hit = cache_valid && (io.pc(31,6) === cache_data(57,32))
+
     io.port.req.valid := !hit && reg_req_valid
     io.port.req.bits.addr := io.pc
     io.port.req.bits.fcn := M_XRD
     io.port.req.bits.typ := MT_WU
     
-    when (io.port.resp.valid){
+    when (io.port.resp.valid && in_mem){
         mem.write(io.pc(5,2),Cat(1.U,io.pc(31,6),io.port.resp.bits.data))
         ren := true.B
     } .otherwise {
         ren := false.B
     }
     
-    io.inst := Mux(hit,cache_data(31,0),BUBBLE)
+    io.inst := Mux(hit,cache_data(31,0),Mux(io.port.resp.valid,io.port.resp.bits.data,BUBBLE))
     io.valid := Mux(hit,true.B,false.B)
 
 
