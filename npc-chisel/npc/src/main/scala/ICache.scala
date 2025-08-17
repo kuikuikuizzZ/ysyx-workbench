4a168237cfc8c6826855f97bd6603ce43048670c
tracer-ysyx <tracer@ysyx.org>
2025-08-17 15:54:56 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:56 up 5 days,  6:28,  4 users,  load average: 1.11, 1.18, 1.21

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -1,43 +1,41 @@
 
-// package npc
+package npc
 
-// import chisel3._
-// import chisel3.util._
-// import npc.common._
-// import npc.Constants._
+import chisel3._
+import chisel3.util._
+import npc.common._
+import npc.Constants._
 
-// class ICacheIO(implicit val conf: ysyx_24100012_Config) extends Bundle {
-//   val port      = new MemPortIo(conf.xlen)
-//   val pc        = Input(UInt(conf.xprlen.W))
-//   val req_valid = Input(Bool())
-//   val inst      = Output(UInt(conf.xlen.W))
-//   val valid     = Output(Bool())
-// }
+class ICacheIO(implicit val conf: ysyx_24100012_Config) extends Bundle {
+  val clock     = Input(Clock())
+  val reset     = Input(Bool())
+  val port      = new MemPortIo(conf.xlen)
+  val pc        = Input(UInt(conf.xprlen.W))
+  val req_valid = Input(Bool())
+  val inst      = Output(UInt(conf.xlen.W))
+  val valid     = Output(Bool())
+}
 
-// class ICache(implicit val conf: ysyx_24100012_Config) extends Module { 
-//     val io = IO(new ICacheIO)
-//     // tag bits = 32-4-2 = 26 (16 = 2^4,4 = 2^2 bytes)
-//     // valid bits = 1, tag bits = 26, 32 (4bytes) 
-//     // 1+ 26 +32 = 59
-//     val mem = SyncReadMem(conf.ICacheSize,UInt(conf.ICacheBlockBits.W))
+class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Module { 
+    val io = IO(new ICacheIO)
+    // tag bits = 32-4-2 = 26 (16 = 2^4,4 = 2^2 bytes)
+    // valid bits = 1, tag bits = 26, 32 (4bytes) 
+    // 1+ 26 +32 = 59
+    val mem = SyncReadMem(conf.ICacheSize,UInt(conf.ICacheBlockBits.W))
 
-//     val cache_data = mem.read(io.pc(5,2),io.req_valid)
-//     val cache_valid = cache_data(58)
-//     val hit = cache_valid && (io.pc(31,6) === cache_data(57,32))
-//     when(cache_inst(58) && ){
-//       io.inst := cache_data(31:0)
-//       io.valid := true.B
-//     } .(){
-
-//     }
-//     io.port.req.valid := !hit && !io.reset
-//     io.port.req.bits.addr := pc_reg
-//     io.port.req.bits.fcn := M_XRD
-//     io.port.req.bits.typ := MT_WU
-//     when 
+    val cache_data = mem.read(io.pc(5,2),io.req_valid)
+    val cache_valid = cache_data(58)
+    val hit = cache_valid && (io.pc(31,6) === cache_data(57,32))
+    
+    io.port.req.valid := !hit && io.req_valid
+    io.port.req.bits.addr := pc_reg
+    io.port.req.bits.fcn := M_XRD
+    io.port.req.bits.typ := MT_WU
+    io.port.resp.bits.data
+    mem.write(io.pc(5,2),Cat(1.U,(31,6),io.inst),io.port.resp.valid)
 
 
-//     io.inst := Mux(io.hit,cache_data(31:0),0.U)
-//     io.valid := Mux(io.hit,true.B,false.B)
+    io.inst := Mux(io.hit,cache_data(31:0),0.U)
+    io.valid := Mux(io.hit,true.B,false.B)
 
-// }
+}
