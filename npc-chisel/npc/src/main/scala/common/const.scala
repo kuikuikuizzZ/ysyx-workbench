bb23176807f373548dc108ac54da1a86425ba44b
tracer-ysyx <tracer@ysyx.org>
2025-08-17 21:28:01 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:01 up 5 days, 12:01,  4 users,  load average: 0.17, 0.41, 0.30

--- npc-chisel/npc/src/main/scala/common/const.scala
+++ npc-chisel/npc/src/main/scala/common/const.scala
@@ -7,7 +7,7 @@ trait PrivilegedConstants
    val MTVEC = 0x100
    val START_ADDR = (0x30000000.U(32.W))
    // val START_ADDR = "h8000_0000".U
-   val SDRAM_BASE = (0xa0000000.U(32.W))
+   // val SDRAM_BASE = (0xa0000000.U(32.W))
    val SDRAM_SIZE = (0x10000000.U(32.W))
    val FLASH_BASE = (0x30000000.U(32.W))
    val FLASH_SIZE = (0x01000000.U(32.W))
