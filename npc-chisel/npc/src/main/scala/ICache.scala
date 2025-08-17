75d93375c46b1bd159bcbf09bd1ec00a6992e596
tracer-ysyx <tracer@ysyx.org>
2025-08-17 21:23:29 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:29 up 5 days, 11:57,  4 users,  load average: 0.80, 0.47, 0.27

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -56,8 +56,8 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
         ren := false.B
     }
     
-    io.inst := Mux(hit,cache_data(31,0),BUBBLE)
-    io.valid := Mux(hit,true.B,io.port.resp.valid)
+    io.inst := Mux(hit,cache_data(31,0),Mux(io.port.resp.valid,io.port.resp.bits.data,BUBBLE))
+    io.valid := Mux(hit,true.B,false.B)
 
 
     /////// DEBUG PORT
