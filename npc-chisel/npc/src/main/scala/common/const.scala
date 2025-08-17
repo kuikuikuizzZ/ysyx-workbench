76d1a5d63c9231f120e03c9d8c03f827c3fc0ec3
tracer-ysyx <tracer@ysyx.org>
2025-08-17 21:31:05 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:05 up 5 days, 12:04,  4 users,  load average: 0.94, 0.63, 0.40

--- npc-chisel/npc/src/main/scala/common/const.scala
+++ npc-chisel/npc/src/main/scala/common/const.scala
@@ -7,13 +7,13 @@ trait PrivilegedConstants
    val MTVEC = 0x100
    val START_ADDR = (0x30000000.U(32.W))
    // val START_ADDR = "h8000_0000".U
-   val SDRAM_BASE = (0xa0000000).U(32.W)
-   val SDRAM_SIZE = (0x10000000).U(32.W)
-   val FLASH_BASE = (0x30000000).U(32.W)
-   val FLASH_SIZE = (0x01000000).U(32.W)
-   val PSRAM_BASE = (0x80000000).U(32.W)
-   val PSRAM_SIZE = (0x01000000).U(32.W)
-   val CLINT_BASE = (0x02000000).U(32.W)
+   val SDRAM_BASE = (0xa0000000.U)(32.W)
+   val SDRAM_SIZE = (0x10000000.U)(32.W)
+   val FLASH_BASE = (0x30000000.U)(32.W)
+   val FLASH_SIZE = (0x01000000.U)(32.W)
+   val PSRAM_BASE = (0x80000000.U)(32.W)
+   val PSRAM_SIZE = (0x01000000.U)(32.W)
+   val CLINT_BASE = (0x02000000.U)(32.W)
    val CLINT_SIZE = 0xC000.U
 
    val MSTATUS = 0x1800.U
