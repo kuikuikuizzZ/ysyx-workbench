afd377e8c6ec3cc7c5a2f4c2f562e7ddf3e6a519
tracer-ysyx <tracer@ysyx.org>
2025-08-17 21:16:18 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:18 up 5 days, 11:49,  4 users,  load average: 0.10, 0.13, 0.12

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -34,21 +34,21 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
     val ren = RegInit(false.B)
     val reg_req_valid = RegNext(io.req_valid,false.B)
     
-    val in_sdram = io.pc >= SDRAM_BASE && io.pc < SDRAM_BASE + SDRAM_SIZE
-    val in_flash = io.pc >= FLASH_BASE && io.pc < FLASH_BASE + FLASH_SIZE
-    val in_psram = io.pc >= PSRAM_BASE && io.pc < PSRAM_BASE + PSRAM_SIZE
+    val in_sdram = io.pc >= SDRAM_BASE && io.pc < (SDRAM_BASE + SDRAM_SIZE)
+    val in_flash = io.pc >= FLASH_BASE && io.pc < (FLASH_BASE + FLASH_SIZE)
+    val in_psram = io.pc >= PSRAM_BASE && io.pc < (PSRAM_BASE + PSRAM_SIZE)
     val in_mem = in_sdram || in_flash || in_psram
 
-    val cache_data = mem.read(io.pc(5,2),(io.req_valid || ren)&&in_mem)
+    val cache_data = mem.read(io.pc(5,2),(io.req_valid || ren) && in_mem)
     val cache_valid = cache_data(58) && in_mem
     val hit = cache_valid && (io.pc(31,6) === cache_data(57,32))
 
     io.port.req.valid := !hit && reg_req_valid
-    io.port.req.bits.addr := io.pc
-    io.port.req.bits.fcn := M_XRD
-    io.port.req.bits.typ := MT_WU
+    io.port.req.bits.addr   := io.pc
+    io.port.req.bits.fcn    := M_XRD
+    io.port.req.bits.typ    := MT_WU
     
-    when (io.port.resp.valid && in_mem){
+    when (in_mem && io.port.resp.valid){
         mem.write(io.pc(5,2),Cat(1.U,io.pc(31,6),io.port.resp.bits.data))
         ren := true.B
     } .otherwise {
--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -227,7 +227,7 @@ void perf_event_display(){
     printf("perf_file: %s\n",perf_file);
     if (perf_file) fp = fopen(perf_file,"w");
     fprintf(fp,"******* Performance counter *******\n");
-    fprintf(fp,"Cycles: \t %.20d\n", cycles);
+    fprintf(fp,"Cycles: \t %ld\n", cycles);
     top_perf_event_display(fp);
     fprintf(fp,"******* End Performance counter *******\n");
     if (perf_file) fclose(fp);
