f47ee521b608540cb6f4858b38caf00510bbc792
kuikuikuizzZ <kimmmywu@gmail.com>
2025-08-18 10:19:37 +0800

    feat(npc-chisel): support icache address mux

--- npc-chisel/npc/src/main/scala/common/const.scala
+++ npc-chisel/npc/src/main/scala/common/const.scala
@@ -7,10 +7,16 @@ trait PrivilegedConstants
    val MTVEC = 0x100
    val START_ADDR = (0x30000000.U(32.W))
    // val START_ADDR = "h8000_0000".U
+   val SDRAM_BASE = (0xa0000000L.U(32.W))
+   val SDRAM_SIZE = (0x10000000L.U(32.W))
+   val FLASH_BASE = (0x30000000L.U(32.W))
+   val FLASH_SIZE = (0x01000000L.U(32.W))
+   val PSRAM_BASE = (0x80000000L.U(32.W))
+   val PSRAM_SIZE = (0x01000000L.U(32.W))
 
    val MSTATUS = 0x1800.U
    
-   val CLINT_BASE = 0x02000000.U(32.W)
+   val CLINT_BASE = (0x02000000.U(32.W))
    val CLINT_SIZE = 0xC000.U
 
    val SZ_PRV = 2
