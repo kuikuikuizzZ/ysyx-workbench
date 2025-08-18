ac3367394b702e789273e8c6c9ce8356f21f6729
tracer-ysyx <tracer@ysyx.org>
2025-08-18 14:13:08 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:13:08 up 6 days,  4:46,  4 users,  load average: 2.15, 0.88, 0.38

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -35,13 +35,9 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
     val ren = RegInit(false.B)
     val reg_req_valid = RegNext(io.req_valid,false.B)
     
-    val in_sdram = io.pc >= SDRAM_BASE && io.pc < (SDRAM_BASE + SDRAM_SIZE)
-    val in_flash = io.pc >= FLASH_BASE && io.pc < (FLASH_BASE + FLASH_SIZE)
-    val in_psram = io.pc >= PSRAM_BASE && io.pc < (PSRAM_BASE + PSRAM_SIZE)
-    val in_mem = in_sdram || in_flash || in_psram
 
-    val cache_data = mem.read(io.pc(5,2),(io.req_valid || ren) && in_mem)
-    val cache_valid = cache_data(58) && in_mem
+    val cache_data = mem.read(io.pc(5,2),(io.req_valid || ren))
+    val cache_valid = cache_data(58)
     val hit = cache_valid && (io.pc(31,6) === cache_data(57,32))
 
     // in mem
