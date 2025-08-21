8b63921494b7614e6b0e53b0aaaf25e6050401e5
tracer-ysyx <tracer@ysyx.org>
2025-08-21 16:16:28 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:28 up 9 days,  6:50,  4 users,  load average: 2.37, 2.07, 1.93

--- npc-chisel/npc/src/main/scala/common/axi.scala
+++ npc-chisel/npc/src/main/scala/common/axi.scala
@@ -143,7 +143,7 @@ class ysyx_24100012_AXI4LiteMaster (implicit val conf: ysyx_24100012_Config) ext
     val araddr  = Mux(accept_read,io.req.raddr,RegEnable(io.req.raddr,  0.U ,  accept_read||io.axi_io.ar.ready))
     val arlen   = Mux(conf.ICacheEnableBurst,io.req.burstlen,0.U)
     val arburst = io.req.burst
-    val arsize  = 2.U
+    val arsize  = Mux(conf.ICacheEnableBurst,2.U,0.U)
 
     val awaddr  =   Mux(accept_write,io.req.waddr,RegEnable(io.req.waddr, accept_write||io.axi_io.aw.ready))
     val awvalid =   Mux(awfire|| wstate === ws_wait_bvalid,false.B, is_write)
