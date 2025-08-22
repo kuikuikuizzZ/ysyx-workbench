11dca42f8338d4d0b579a44b76512a77864e3aa4
tracer-ysyx <tracer@ysyx.org>
2025-08-22 20:29:33 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:33 up 10 days, 11:03,  4 users,  load average: 3.40, 2.93, 2.79

--- npc-chisel/npc/src/main/scala/common/configuration.scala
+++ npc-chisel/npc/src/main/scala/common/configuration.scala
@@ -16,7 +16,7 @@ case class ysyx_24100012_Config(
     val AXIBurstLenBits = 8
     val burstLength = 1.U   // burstLength = axlen - 1   
 
-    val ICacheSizeBits = 3
+    val ICacheSizeBits = 2
     val ICacheBlockBits = 1
-    val ICacheEnableBurst = false.B
+    val ICacheEnableBurst = true.B
 }
