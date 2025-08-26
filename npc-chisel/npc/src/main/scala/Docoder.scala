b1f73d85c69346bbb600b130be84e1e8f05b62f2
tracer-ysyx <tracer@ysyx.org>
2025-08-26 14:09:32 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:09:32 up 14 days,  4:43,  4 users,  load average: 1.87, 2.11, 1.97

--- npc-chisel/npc/src/main/scala/Docoder.scala
+++ npc-chisel/npc/src/main/scala/Docoder.scala
@@ -49,6 +49,8 @@ class CpathIo(implicit val conf: ysyx_24100012_Config) extends Bundle()
    val reg_in     =  Flipped(new RegFileOut())
    val ifu_out    =  Flipped(new InstFetchIn)
    val debug      =  new CtrlDebugPort
+   val clock        = Input(Clock())
+   val reset        = Input(Bool())
 }
 
 class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Module
