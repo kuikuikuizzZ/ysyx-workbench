a9e1f5b8824ff1ad0bb244e969090961de1ce899
tracer-ysyx <tracer@ysyx.org>
2025-08-27 09:55:38 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:55:38 up 15 days, 29 min,  4 users,  load average: 2.03, 2.01, 1.98

--- npc-chisel/npc/src/main/scala/Docoder.scala
+++ npc-chisel/npc/src/main/scala/Docoder.scala
@@ -330,7 +330,7 @@ class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Mod
       io.dec_exe.bits.ctrl_mem_val  := false.B
       io.dec_exe.bits.ctrl_mem_fcn  := M_X
       io.dec_exe.bits.ctrl_csr_cmd  := CSR.N
-      io.dec_exe.bits.ctrl_br_type  := BR_N
+      io.dec_exe.bits.br_type       := BR_N
    }
    .elsewhen(!stall && !full_stall)
    {
@@ -345,7 +345,7 @@ class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Mod
       io.dec_exe.bits.alu_fun       := cs_alu_fun
       io.dec_exe.bits.ctrl_wb_sel   := cs_wb_sel
 
-      when (dec_kill)
+      when (deckill)
       {
          io.dec_exe.valid         := false.B
          io.dec_exe.bits.inst          := BUBBLE
