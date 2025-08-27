cbf9a6078b25a0a086da7cfe6b313b19d553a928
tracer-ysyx <tracer@ysyx.org>
2025-08-27 09:54:54 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:54:54 up 15 days, 28 min,  4 users,  load average: 1.92, 2.00, 1.98

--- npc-chisel/npc/src/main/scala/Docoder.scala
+++ npc-chisel/npc/src/main/scala/Docoder.scala
@@ -345,7 +345,7 @@ class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Mod
       io.dec_exe.bits.alu_fun       := cs_alu_fun
       io.dec_exe.bits.ctrl_wb_sel   := cs_wb_sel
 
-      when (io.ctl.dec_kill)
+      when (dec_kill)
       {
          io.dec_exe.valid         := false.B
          io.dec_exe.bits.inst          := BUBBLE
@@ -354,7 +354,7 @@ class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Mod
          io.dec_exe.bits.ctrl_mem_val  := false.B
          io.dec_exe.bits.ctrl_mem_fcn  := M_X
          io.dec_exe.bits.ctrl_csr_cmd  := CSR.N
-         io.dec_exe.bits.ctrl_br_type  := BR_N
+         io.dec_exe.bits.br_type  := BR_N
       }
       .otherwise
       {
@@ -366,7 +366,7 @@ class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Mod
          io.dec_exe.bits.ctrl_mem_fcn  := cs_mem_fcn
          io.dec_exe.bits.ctrl_mem_typ  := cs_msk_sel
          io.dec_exe.bits.ctrl_csr_cmd  := cs_csr_cmd
-         io.dec_exe.bits.ctrl_br_type  := cs_br_type
+         io.dec_exe.bits.br_type  := cs_br_type
       }
    }
 
