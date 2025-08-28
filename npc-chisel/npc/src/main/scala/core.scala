d34e97773bc08dadc77abd8baed10829d32a87b0
tracer-ysyx <tracer@ysyx.org>
2025-08-28 21:43:23 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:23 up 16 days, 12:17,  4 users,  load average: 2.13, 1.99, 1.88

--- npc-chisel/npc/src/main/scala/core.scala
+++ npc-chisel/npc/src/main/scala/core.scala
@@ -71,7 +71,7 @@ class ysyx_24100012 extends Module
   wbu.io.ctl <> decoder.io.ctl_sign
   wbu.io.reg <> reg_file.io.wb
 
-  pipelineConnect(inst_fetch.io.ifu_pipe, decoder.io.ifu_pipe, decoder.io.dec_exe)
+  pipelineConnect(inst_fetch.io.ifu_dec, decoder.io.ifu_dec, decoder.io.dec_exe)
   pipelineConnect(decoder.io.dec_exe, exu.io.dec_exe, exu.io.exe_mem)
   pipelineConnect(exu.io.exe_mem, lsu.io.exe_mem, lsu.io.mem_wb)
   pipelineConnect(lsu.io.mem_wb, wbu.io.mem_wb, wbu.io.reg)
