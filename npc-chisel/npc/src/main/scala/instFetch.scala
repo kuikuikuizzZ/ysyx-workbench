25de4c173e217629d08a88884d106005f14edc8f
tracer-ysyx <tracer@ysyx.org>
2025-08-29 14:51:16 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:16 up 17 days,  5:24,  4 users,  load average: 1.94, 1.98, 1.92

--- npc-chisel/npc/src/main/scala/instFetch.scala
+++ npc-chisel/npc/src/main/scala/instFetch.scala
@@ -40,14 +40,10 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
 
   val pc_reg = RegInit(START_ADDR)
   val pc_valid = RegInit(true.B)
-  val reg_reset = RegNext(io.reset,true.B)
 
   when((!io.ctl.dec_stall && !io.ctl.full_stall) || io.ctl.pipeline_kill) {
       pc_reg := pc_next
       pc_valid := true.B
-  } .elsewhen (io.ctl.full_stall) {
-      pc_valid := pc_valid
-      pc_reg := pc_reg
   } .otherwise {
       pc_valid := false.B
   }
@@ -66,7 +62,7 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
   cache.io.reset := reset
   cache.io.port <> io.port
   cache.io.pc := pc_reg
-  cache.io.req_valid := !io.reset && pc_valid && (!io.ctl.full_stall || reg_reset)
+  cache.io.req_valid := !io.reset && pc_valid
   cache.io.debug <> io.debug.icache 
   
   // Pipeline Interface
