c85b11a63c1fe6d741f5d1d789b103e55f496a31
tracer-ysyx <tracer@ysyx.org>
2025-08-29 20:39:47 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:47 up 17 days, 11:13,  4 users,  load average: 2.15, 1.97, 1.94

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -159,6 +159,7 @@ class ysyx_24100012_AXI4LiteArbiter(numMasters: Int)(implicit val conf: ysyx_241
          // }
          when(io.ports(currentMaster).req.valid) {
             state := Mux(io.ports(currentMaster) === io.ports(IPORT),s_ifu_active,s_lsu_active)
+            burstlen_reg := Mux(io.ports(currentMaster) === io.ports(IPORT),Mux(req_burst =/= BURST_FIXED,req_burstlen,0.U)) 
             // req_valid      := io.ports(currentMaster).req.valid 
             // req_addri      := io.ports(currentMaster).req.bits.addr      
             // req_fcn        := io.ports(currentMaster).req.bits.fcn     
@@ -166,9 +167,11 @@ class ysyx_24100012_AXI4LiteArbiter(numMasters: Int)(implicit val conf: ysyx_241
             // req_data       := io.ports(currentMaster).req.bits.data
             // req_burst      := io.ports(currentMaster).req.bits.burst
             // req_burstlen   := io.ports(currentMaster).req.bits.burstlen
+
          }.elsewhen(io.ports(nextMaster).req.valid) {
             currentMaster := nextMaster
             state := Mux(io.ports(nextMaster) === io.ports(IPORT),s_ifu_active,s_lsu_active)
+            burstlen_reg := Mux(io.ports(nextMaster) === io.ports(IPORT),Mux(req_burst =/= BURST_FIXED,req_burstlen,0.U)) 
             // req_valid      := io.ports(nextMaster).req.valid 
             // req_addri      := io.ports(nextMaster).req.bits.addr      
             // req_fcn        := io.ports(nextMaster).req.bits.fcn     
