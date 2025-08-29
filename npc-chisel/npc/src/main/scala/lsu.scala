0ed061962f1790cb4fb01a68bd3604384c56680d
tracer-ysyx <tracer@ysyx.org>
2025-08-29 17:12:06 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:06 up 17 days,  7:45,  4 users,  load average: 2.27, 2.00, 2.01

--- npc-chisel/npc/src/main/scala/lsu.scala
+++ npc-chisel/npc/src/main/scala/lsu.scala
@@ -101,7 +101,6 @@ class ysyx_24100012_LSU(implicit val conf: ysyx_24100012_Config) extends Module 
         when (io.exe_mem.bits.ctrl_mem_fcn === M_XRD){
             io.clintIO.dr.en := true.B
             io.clintIO.dr.addr := addr
-            mem_data := io.clintIO.dr.data
         } .otherwise{
             io.clintIO.dr.en := false.B
         }
@@ -111,9 +110,9 @@ class ysyx_24100012_LSU(implicit val conf: ysyx_24100012_Config) extends Module 
         io.port.req.bits.typ := io.exe_mem.bits.ctrl_mem_typ
         io.port.req.bits.addr := addr
         io.port.req.bits.data := io.exe_mem.bits.rs2_data 
-        mem_data :=  io.port.resp.bits.data
     }
-
+    
+    mem_data :=  Mux(in_clint,io.clintIO.dr.data,io.port.resp.bits.data)
     io.to_ctl.resp_valid    := Mux(in_clint, io.clintIO.dr.ready, io.port.resp.valid)
     io.to_ctl.ctrl_mem_val  := mem_en
     // WB Mux
