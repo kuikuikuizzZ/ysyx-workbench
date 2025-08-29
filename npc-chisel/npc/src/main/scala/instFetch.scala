4ef9714be13cd94160d71ea3861f7af61ad64df3
tracer-ysyx <tracer@ysyx.org>
2025-08-29 16:12:16 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:16 up 17 days,  6:45,  4 users,  load average: 1.65, 1.66, 1.81

--- npc-chisel/npc/src/main/scala/instFetch.scala
+++ npc-chisel/npc/src/main/scala/instFetch.scala
@@ -6,38 +6,28 @@ import npc.common._
 import npc.Constants._
 
 class IFUDebugPort(implicit val conf: ysyx_24100012_Config)   extends Bundle() {
+  val valid           = Output(Bool())
   val instFetchCount  = Output(UInt(conf.perfCountBits.W))
-  val icache     = new ICacheDebugPort
+  val icache          = new ICacheDebugPort
+}
+
+class IFUPipeIO(implicit val conf: ysyx_24100012_Config) extends Bundle {
+  val pc        = Output(UInt(conf.xprlen.W))
+  val inst      = Output(UInt(conf.xprlen.W))
 }
 
 class InstFetchIo(implicit val conf: ysyx_24100012_Config) extends Bundle() {
-  val port          = new MemPortIo(conf.xlen)
-  val pipeline_kill = Input(Bool()) 
-  val in            = new InstFetchIn
-  val pc_io         = new PCOut()
-  val valid         = Output(Bool())
-  val inst          = Output(UInt(conf.xprlen.W))
-  val finish        = Input(Bool())
-  val halt          = Input(Bool())
-  val reset         = Input(Bool())
-  val clock         = Input(Clock())
-  val debug         = Output(new IFUDebugPort)
-
+  val clock             = Input(Clock())
+  val reset             = Input(Bool())
+  val ctl               = new CtrlSignalIO
+  val port              = new MemPortIo(conf.xlen)
+  val exu_in            = Flipped(new EXUToIFUOut)
+  val ifu_dec          = new DecoupledIO(new IFUPipeIO())
+  val exception_target  = Input(UInt(conf.xprlen.W))
+  val debug             = Output(new IFUDebugPort)
+  val icache_valid      = Output(Bool())
 }
 
-class InstFetchIn(implicit val conf: ysyx_24100012_Config) extends Bundle() {
-  val pc_sel            =   Input(UInt(PC_4.getWidth.W))
-  val br_target         =   Input(UInt(conf.xprlen.W))
-  val jmp_target        =   Input(UInt(conf.xprlen.W))
-  val jump_reg_target   =   Input(UInt(conf.xprlen.W))
-  val exception_target  =   Input(UInt(conf.xprlen.W))
-}
-
-
-class PCOut(implicit val conf: ysyx_24100012_Config) extends Bundle() {
-  val pc_plus4 = Output(UInt(conf.xprlen.W))
-  val pc =  Output(UInt(conf.xprlen.W))
-}
 
 class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Module {
   val io = IO(
@@ -48,65 +38,65 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
   // Instruction Fetch
   val pc_next = Wire(UInt(conf.xprlen.W))
 
-  // PC Register
-  pc_next := MuxCase(io.in.pc_sel, Seq(
-                    (io.in.pc_sel === PC_4)   -> io.pc_io.pc_plus4,
-                    (io.in.pc_sel === PC_BR)  -> io.in.br_target,
-                    (io.in.pc_sel === PC_J )  -> io.in.jmp_target,
-                    (io.in.pc_sel === PC_JR)  -> io.in.jump_reg_target,
-                    (io.in.pc_sel === PC_EXC) -> io.in.exception_target
-                    ))
-
   val pc_reg = RegInit(START_ADDR)
   val pc_valid = RegInit(true.B)
-  // val pc_ready = Wire(Bool())      
 
-  when(io.finish && !io.halt) {
+  when((!io.ctl.dec_stall && !io.ctl.full_stall) || io.ctl.pipeline_kill) {
       pc_reg := pc_next
       pc_valid := true.B
-  } 
-  .otherwise {
+  } .otherwise {
       pc_valid := false.B
-      pc_reg := pc_reg
-  } 
-  
-  // // Memory Requests
-  // io.port.req.valid := pc_valid && !io.reset
-  // io.port.req.bits.addr := pc_reg
-  // io.port.req.bits.fcn := M_XRD
-  // io.port.req.bits.typ := MT_WU
+  }
+  val pc_plus4 = (pc_reg + 4.asUInt(conf.xprlen.W))
 
-  // // Instruction Read
-  // val inst_reg = RegEnable(io.port.resp.bits.data,BUBBLE,io.port.resp.valid)
-  // val inst = Mux(io.port.resp.valid,io.port.resp.bits.data,inst_reg)
+  // PC Register
+  pc_next :=  Mux(io.ctl.pc_sel === PC_4,         pc_plus4,
+                 Mux(io.ctl.pc_sel === PC_BRJMP,  io.exu_in.exe_brjmp_target,
+                 Mux(io.ctl.pc_sel === PC_JALR,   io.exu_in.exe_jump_reg_target,
+                 /*Mux(io.ctl.pc_sel === PC_EXC*/ io.exception_target)))
 
-  // // val valid = RegInit(false.B)
-  // val valid = RegNext(io.port.resp.valid,false.B)
-  // // val valid_reg = RegNext(valid,false.B)
-  // io.valid := valid     
-
-  val cache = Module(new ysyx_24100012_ICache)
-  val inst_reg = RegEnable(cache.io.inst,BUBBLE,cache.io.valid)
+  val cache       = Module(new ysyx_24100012_ICache)
+  // val inst_reg    = RegEnable(cache.io.inst,BUBBLE,cache.io.valid)
+  // val valid       = RegNext(cache.io.valid,false.B)
   cache.io.clock := clock
   cache.io.reset := reset
   cache.io.port <> io.port
-  cache.io.req.bits := pc_reg
-  cache.io.req.valid := pc_valid && !io.reset
+  cache.io.pc := pc_reg
+  cache.io.req_valid := !io.reset && pc_valid
   cache.io.debug <> io.debug.icache 
-  // val valid = RegInit(false.B)
-  val valid = RegNext(cache.io.valid,false.B)
-  // val valid_reg = RegNext(valid,false.B)
   
-  
-  io.valid := valid 
-  io.inst := inst_reg
-  io.pc_io.pc_plus4 := (pc_reg + 4.asUInt(conf.xprlen.W)) 
-  io.pc_io.pc := pc_reg     
+  // Pipeline Interface
+  val if_inst = cache.io.inst
+  io.icache_valid := cache.io.valid 
+  when (io.ctl.pipeline_kill)
+  {
+    io.ifu_dec.valid := false.B
+    io.ifu_dec.bits.inst := BUBBLE
+  }
+  .elsewhen (!io.ctl.dec_stall && !io.ctl.full_stall)
+  {
+    when (io.ctl.if_kill)
+    {
+        io.ifu_dec.valid := false.B
+        io.ifu_dec.bits.inst := BUBBLE
+    }
+    .otherwise
+    {
+        io.ifu_dec.valid := cache.io.valid
+        io.ifu_dec.bits.inst := if_inst
+    }
+
+    io.ifu_dec.bits.pc := pc_reg
+  }
+
+
+
   ////////// debug
   val instFetchCount = RegInit(0.U(conf.perfCountBits.W))
-  when(io.valid) {
+  when(cache.io.valid) {
     instFetchCount := instFetchCount + 1.U
   }
+  io.debug.valid := cache.io.valid
   io.debug.instFetchCount := instFetchCount
   ////////// end of debug 
 }
--- npc-chisel/npc/src/main/scala/regFile.scala
+++ npc-chisel/npc/src/main/scala/regFile.scala
@@ -5,9 +5,14 @@ import npc.common.{ysyx_24100012_Config, MemPortIo}
 import npc.Constants._
 
 class RegFileIo(implicit val conf: ysyx_24100012_Config) extends Bundle {
-  val inst = Input(UInt(conf.xlen.W))
   val out = new RegFileOut()
-  val wb = Flipped(new WBToRegIo())
+  val wb = Flipped(new DecoupledIO(new WBToRegIo()))
+  val dec = new RegFilePipeIn()
+}
+
+class RegFilePipeIn(implicit val conf: ysyx_24100012_Config) extends Bundle {
+   val rs1_addr      = Input(UInt(5.W))
+   val rs2_addr      = Input(UInt(5.W))
 }
 
 class RegFileOut(implicit val conf: ysyx_24100012_Config) extends Bundle {
@@ -15,18 +20,21 @@ class RegFileOut(implicit val conf: ysyx_24100012_Config) extends Bundle {
   val rs2_data = Output(UInt(conf.xlen.W))
 }
 
+
+
+
 class ysyx_24100012_RegFile(implicit val conf: ysyx_24100012_Config) extends Module {
   val io = IO(new RegFileIo())
   io := DontCare
-  val rs1_addr = io.inst(RS1_MSB, RS1_LSB)
-  val rs2_addr = io.inst(RS2_MSB, RS2_LSB)
-  val wb_addr  = io.inst(RD_MSB, RD_LSB)
+  val rs1_addr = io.dec.rs1_addr
+  val rs2_addr = io.dec.rs2_addr
+  val wb_addr  = io.wb.bits.wbaddr
   
   // Register File
   val regfile = Mem(16, UInt(conf.xlen.W)).suggestName("ysyx_24100012_regfile_mem") 
 
-  when (io.wb.rf_wen && (wb_addr =/= 0.U)) {
-    regfile(wb_addr) := io.wb.data
+  when (io.wb.bits.rf_wen && (wb_addr =/= 0.U)) {
+    regfile(wb_addr) := io.wb.bits.data
   }
 
   io.out.rs1_data := Mux((rs1_addr =/= 0.U), regfile(rs1_addr), 0.asUInt(conf.xlen.W))
