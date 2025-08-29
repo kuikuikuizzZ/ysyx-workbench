509ed031358c225ec3091aa9c329edbeffc73689
tracer-ysyx <tracer@ysyx.org>
2025-08-29 21:39:22 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:39:22 up 17 days, 12:13,  4 users,  load average: 1.35, 1.71, 1.87

--- npc-chisel/npc/src/main/scala/instFetch.scala
+++ npc-chisel/npc/src/main/scala/instFetch.scala
@@ -67,6 +67,7 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
   
   // Pipeline Interface
   val if_inst = Mux(cache.io.valid,cache.io.inst,RegEnable(cache.io.inst,BUBBLE,cache.io.valid ))
+  val if_valid = Mux(cache.io.valid,cache.io.valid,RegEnable(cache.io.valid,cache.io.valid || pc_valid))
   io.icache_valid := cache.io.valid 
   when (io.ctl.pipeline_kill)
   {
