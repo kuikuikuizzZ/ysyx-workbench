e5a0d0a385d6eb5af28f95621bd48ca35ae2ebc6
tracer-ysyx <tracer@ysyx.org>
2025-08-29 21:56:16 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:16 up 17 days, 12:29,  4 users,  load average: 2.06, 2.10, 1.99

--- npc/csrc/cpu/top_chisel.c
+++ npc/csrc/cpu/top_chisel.c
@@ -139,11 +139,11 @@ uint32_t top_csr(int i) {
         printf("csr index %d out of range\n", i);
         return 0;
     }
-    uint32_t mstatus    = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu__DOT__csr_files__DOT__csr__DOT__reg_mstatus;
-    uint32_t mtvec      = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu__DOT__csr_files__DOT__csr__DOT__reg_mtvec;
-    uint32_t mepc       = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu__DOT__csr_files__DOT__csr__DOT__reg_mepc;
-    uint32_t mcause     = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu__DOT__csr_files__DOT__csr__DOT__reg_mcause;
-    uint32_t mtval      = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu__DOT__csr_files__DOT__csr__DOT__reg_mtval;
+    uint32_t mstatus    = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__csr__DOT__reg_mstatus;
+    uint32_t mtvec      = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__csr__DOT__reg_mtvec;
+    uint32_t mepc       = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__csr__DOT__reg_mepc;
+    uint32_t mcause     = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__csr__DOT__reg_mcause;
+    uint32_t mtval      = _rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__csr__DOT__reg_mtval;
     uint32_t csrs[5] = {0,mstatus,mtvec,mepc, mcause};  
     return csrs[i]; 
 }
@@ -152,14 +152,6 @@ uint32_t top_pc() {
     if (!_rootp) return 0;
     uint32_t pc ;
     IFDEF(CONFIG_SOC,pc=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__inst_fetch__DOT__pc_reg);
-    IFNDEF(CONFIG_SOC,pc=(uint32_t)_rootp->ysyxSoCFull__DOT__core__DOT__inst_fetch__DOT__pc_reg);
-    return pc;
-}
-
-uint32_t top_decode_pc() {
-    if (!_rootp) return 0;
-    uint32_t pc ;
-    IFDEF(CONFIG_SOC,pc=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__decoder_io_ifu_dec_bits_rpc);
     IFNDEF(CONFIG_SOC,pc=(uint32_t)_rootp->ysyxSoCFull__DOT__core__DOT__inst_fetch__DOT__pc_reg);
     return pc;
 }
@@ -182,14 +174,6 @@ uint32_t top_dnpc() {
     return pc;
 }
 
-uint32_t top_alu_out() {
-    if (!_rootp) return 0;
-    uint32_t alu_out;
-    IFDEF(CONFIG_SOC,alu_out=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__exu__DOT__casez_tmp);
-    return alu_out;
-
-}
-
 mem_access_t top_lsu_state(){
     return lsu_state;
 }
@@ -209,16 +193,16 @@ void delete_top() {
 }
 uint32_t top_op1() {
     if (!_rootp) return 0;
-    uint32_t op1=0;
-    IFDEF(CONFIG_SOC,op1=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__exu_io_dec_exe_bits_rop1_data);
+    uint32_t op1;
+    IFDEF(CONFIG_SOC,op1=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__casez_tmp_0);
     IFNDEF(CONFIG_SOC,op1=(uint32_t)_rootp->ysyxSoCFull__DOT__core__DOT__d__DOT__casez_tmp_0);
     return op1;
 }
 
 uint32_t top_op2() {
     if (!_rootp) return 0;
-    uint32_t op2 =0;
-    IFDEF(CONFIG_SOC,op2=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__exu_io_dec_exe_bits_rop2_data);
+    uint32_t op2;
+    IFDEF(CONFIG_SOC,op2=(uint32_t)_rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__d__DOT__casez_tmp_1);
     IFNDEF(CONFIG_SOC,op2=(uint32_t)_rootp->ysyxSoCFull__DOT__core__DOT__d__DOT__casez_tmp_1);
     return op2;
 }
@@ -235,16 +219,14 @@ void watch_top(){
     if (wt->inst==top_inst() && top_pc()==wt->pc && top_dnpc()==wt->dnpc) return;
     else {
         // if(top_pc()!=0x800013a0) return; // only watch when pc is 0x80000000
-        printf(" io_halt %d ,pc %.8x,dec_pc %.8x,dnpc %.8x, inst: %.8x, a0 %.8x alu1 %.8x, alu2 %.8x, alu_out %.8x, mem_en: %d,r/w %d addr %.8x, data %.8x \n",
+        printf(" io_halt %d ,pc %.8x,dnpc %.8x, inst: %.8x, a0 %.8x alu1 %.8x, alu2 %.8x, mem_en: %d,r/w %d addr %.8x, data %.8x \n",
             top_halt(),
             top_pc(),
-            top_decode_pc(),
             top_dnpc(),
             top_inst(),
             top_gpr(10),
             top_op1(),
             top_op2(),
-            top_alu_out(),
             lsu_state.enable,
             lsu_state.fcn,
             lsu_state.addr,
