61d267da7305d33811ffa9a3ee5486683680d76d
tracer-ysyx <tracer@ysyx.org>
2025-08-29 17:13:49 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:13:49 up 17 days,  7:47,  4 users,  load average: 1.96, 2.02, 2.02

--- npc-chisel/npc/src/main/scala/lsu.scala
+++ npc-chisel/npc/src/main/scala/lsu.scala
@@ -112,8 +112,10 @@ class ysyx_24100012_LSU(implicit val conf: ysyx_24100012_Config) extends Module 
         io.port.req.bits.data := io.exe_mem.bits.rs2_data 
     }
     
-    mem_data :=  Mux(in_clint,io.clintIO.dr.data,io.port.resp.bits.data)
-    io.to_ctl.resp_valid    := Mux(in_clint, io.clintIO.dr.ready, io.port.resp.valid)
+    // mem_data :=  Mux(in_clint,io.clintIO.dr.data,io.port.resp.bits.data)
+    // io.to_ctl.resp_valid    := Mux(in_clint, io.clintIO.dr.ready, io.port.resp.valid)
+    mem_data := io.port.resp.bits.data
+    io.to_ctl.resp_valid    := io.port.resp.valid
     io.to_ctl.ctrl_mem_val  := mem_en
     // WB Mux
     val wbdata = MuxCase(io.exe_mem.bits.alu_out, Array(
