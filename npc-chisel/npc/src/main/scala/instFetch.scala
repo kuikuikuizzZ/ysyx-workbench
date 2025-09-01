51c5e210ed6371956c6a4499388fea40480c2e49
tracer-ysyx <tracer@ysyx.org>
2025-09-01 16:33:10 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:10 up 20 days,  7:06,  4 users,  load average: 2.46, 2.00, 1.88

--- npc-chisel/npc/src/main/scala/instFetch.scala
+++ npc-chisel/npc/src/main/scala/instFetch.scala
@@ -41,7 +41,10 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
 
   val pc_reg = RegInit(START_ADDR)
   val pc_valid = RegInit(true.B)
-
+  val inst = Mux(io.ctl.if_kill, BUBBLE,cache.io.inst)
+  val if_inst = Mux(cache.io.valid,cache.io.inst,RegEnable(inst,BUBBLE,cache.io.valid || io.ifu_dec.ready || io.ctl.if_kill ))
+  val if_valid = Mux(cache.io.valid,cache.io.valid,RegEnable(cache.io.valid && !io.ifu_dec.ready,cache.io.valid || io.ifu_dec.ready || io.ctl.if_kill))
+  
   when(cache.io.valid && if_valid) {
       pc_reg := pc_next
       pc_valid := true.B
@@ -72,12 +75,7 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
   cache.io.port       <> io.port
   cache.io.debug      <> io.debug.icache 
   
-  // Pipeline Interface
-  val inst = Mux(io.ctl.if_kill, BUBBLE,cache.io.inst)
-  val if_inst = Mux(cache.io.valid,cache.io.inst,RegEnable(inst,BUBBLE,cache.io.valid || io.ifu_dec.ready || io.ctl.if_kill ))
-  // NOTE: when io.ifu_dec.ready, valid
-  val if_valid = Mux(cache.io.valid,cache.io.valid,RegEnable(cache.io.valid && !io.ifu_dec.ready,cache.io.valid || io.ifu_dec.ready || io.ctl.if_kill))
-  
+
   // NOTE: if_kill should clean inst, in ifu_dec reg
   io.ifu_dec.valid :=   Mux(io.ctl.if_kill, true.B, if_valid)
   io.ifu_dec.bits.inst :=  Mux(io.ctl.if_kill, BUBBLE,if_inst)
