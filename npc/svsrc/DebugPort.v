33ecfece446b1402fee0e8bfec9bbb87fe7ffa69
tracer-ysyx <tracer@ysyx.org>
2025-09-02 11:37:25 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:37:25 up 21 days,  2:11,  3 users,  load average: 1.69, 2.07, 2.01

--- npc/svsrc/DebugPort.v
+++ npc/svsrc/DebugPort.v
@@ -28,7 +28,7 @@
             dpi_port(expand_halt, pc, inst,mem_pc,wb_pc,wb_inst);
         end
 
-        always @(posedge clock) begin
+        always @(*) begin
             if (lsu_port_mem_en && lsu_port_fcn == 1'b1) begin
                 lsu_port(lsu_port_mem_en,lsu_port_fcn,expand_typ, lsu_port_addr, lsu_port_wdata);
             end else if (wb_valid && lsu_port_fcn == 1'b0) begin
