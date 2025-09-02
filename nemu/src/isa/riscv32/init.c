33475376e6b36ea5590338f4a3989febd010e500
tracer-ysyx <tracer@ysyx.org>
2025-09-02 17:20:44 +0800

    > run NEMU ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:44 up 21 days,  7:54,  3 users,  load average: 2.32, 2.06, 1.95

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -26,18 +26,35 @@
   //   0x00100073,  // ebreak (used as nemu_trap)
   //   0xdeadbeef,  // some data
   // };
-  static const uint32_t img []  = { 
-        0x00100513,      //addi a0 x0 1
-        0x00150513,      //addi a0 a0 1
-        0x00150513,      //addi a0 a0 1
-        0x70000117,      //auipc	sp,0xef001
-        0xffc10113,      //addi	  sp,sp,-4 # f001000 
-        0x00a12223,      //sw a0, 4(sp)
-        0x00000513,      //addi a0 x0 0
-        0x00412503,      //lw a0, 4(sp)
-        0x00150513,      //addi a0 a0 1
-        0xffc50513,      //addi a0 a0 -4
-        0x00100073,      //ebreak
+//   static const uint32_t img []  = { 
+//         0x00100513,      //addi a0 x0 1
+//         0x00150513,      //addi a0 a0 1
+//         0x00150513,      //addi a0 a0 1
+//         0x70000117,      //auipc	sp,0xef001
+//         0xffc10113,      //addi	  sp,sp,-4 # f001000 
+//         0x00a12223,      //sw a0, 4(sp)
+//         0x00000513,      //addi a0 x0 0
+//         0x00412503,      //lw a0, 4(sp)
+//         0x00150513,      //addi a0 a0 1
+//         0xffc50513,      //addi a0 a0 -4
+//         0x00100073,      //ebreak
+// };
+
+static const uint32_t img [] = {
+        0x00100513,    //addi a0 x0 1        30000000  
+        0x00150513,    //addi a0 a0 1        30000004  
+        0x00150513,    //addi a0 a0 1        30000008  
+        0xdf001117,    //auipc sp,0x000df001 3000000c  
+        0x00a12223,    //sw a0, 4(sp)        30000010  
+        0x00000513,    //addi a0 x0 0        30000014  
+        0x00350A63,    //beq a0, 0x3 0x14    30000018
+        0x00412503,    //lw a0, 4(sp)        3000001c 
+        0x004005EF,    //jal a1, 4           30000020
+        0xFF4585E7,    //jalr a1, a1 -12    30000024
+        0x004005EF,    //jal a1, 4           3000002c
+        0x004005EF,    //jal a1, 4           30000030
+        0xffc50513,    //addi a0 a0 -4       30000028 
+        0x00100073,    //ebreak              30000034 
 };
 static void restart() {
   /* Set the initial program counter. */
