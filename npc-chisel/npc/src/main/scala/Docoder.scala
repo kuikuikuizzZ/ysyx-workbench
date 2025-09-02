aba95e14e246eb5bf354bb3507ff4b9f0ac35720
tracer-ysyx <tracer@ysyx.org>
2025-09-02 10:56:54 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:56:54 up 21 days,  1:30,  3 users,  load average: 2.49, 2.23, 2.01

--- npc-chisel/npc/src/main/scala/Docoder.scala
+++ npc-chisel/npc/src/main/scala/Docoder.scala
@@ -286,8 +286,8 @@ class ysyx_24100012_Decoder(implicit val conf: ysyx_24100012_Config) extends Mod
       io.dec_exe.bits.ctrl_csr_cmd  := CSR.N
       io.dec_exe.bits.br_type       := BR_N
       io.dec_exe.bits.pc            := dec_reg_pc
-      // io.dec_exe.bits.pc_valid      := false.B
-
+      io.dec_exe.bits.pc_valid      := false.B
+      
    } .otherwise {
       io.dec_exe.bits.pc            := dec_reg_pc
       io.dec_exe.bits.rs1_addr      := dec_rs1_addr
