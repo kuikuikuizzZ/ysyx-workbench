8b20fdd7f58358a5a182f5d6ef568f16899c9039
kuikuikuizzZ <kimmmywu@gmail.com>
2025-09-04 15:36:11 +0800

    feat(nemu): adapt to pipeline

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -59,7 +59,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
     for (int i=0;i<NXPR;i++){
       cpu.gpr[i] = context->gpr[i];
     }
-    // cpu.pc = context->pc;
+    cpu.pc = (context->pc == 0x0)? cpu.pc : context->pc;
   }
 
   return;
