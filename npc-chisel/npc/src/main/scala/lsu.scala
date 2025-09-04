11e69e5be5cf0e216b07f02dd7422814f30e70b8
tracer-ysyx <tracer@ysyx.org>
2025-09-04 22:03:04 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:04 up 23 days, 12:36,  4 users,  load average: 8.14, 8.00, 7.97

--- npc-chisel/npc/src/main/scala/lsu.scala
+++ npc-chisel/npc/src/main/scala/lsu.scala
@@ -154,7 +154,7 @@ class ysyx_24100012_LSU(implicit val conf: ysyx_24100012_Config) extends Module 
 
     exception := Mux(mem_en && io.port.resp.bits.resp =/= 0.U , 
             Mux(io.exe_mem.bits.ctrl_mem_typ === M_XRD, EXC_LOAD_ACCESS_FAULT, 
-            Mux(io.exe_mem.bits.ctrl_mem_typ === M_XWR, EXC_STORE_ACCESS_FAULT), EXC_NORMAL ))
+            Mux(io.exe_mem.bits.ctrl_mem_typ === M_XWR, EXC_STORE_ACCESS_FAULT)), EXC_NORMAL )
     io.mem_wb.valid                 := (!mem_en || (mem_en && io.to_ctl.resp_valid))
     io.mem_wb.bits.data             := wbdata
     io.mem_wb.bits.wbaddr           := io.exe_mem.bits.wbaddr
