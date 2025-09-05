1171a264a75088f76059f8300d5742e6650d822f
tracer-ysyx <tracer@ysyx.org>
2025-09-05 11:11:54 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:11:54 up 24 days,  1:45,  3 users,  load average: 0.12, 0.25, 0.43

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -104,7 +104,6 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
                     state := sRequesting // 继续请求下一子块
                 }
                 when(io.port.resp.bits.resp =/= 0.U) {
-                    state := sIdle
                     io.exception  := EXC_INSTR_ACCESS_FAULT
                 }.otherwise {
                     io.exception  := EXC_NORMAL
--- npc-chisel/npc/src/main/scala/instFetch.scala
+++ npc-chisel/npc/src/main/scala/instFetch.scala
@@ -75,11 +75,11 @@ class ysyx_24100012_InstFetch(implicit conf: ysyx_24100012_Config) extends Modul
   
 
   // NOTE: if_kill should clean inst, in ifu_dec reg
-  io.ifu_dec.valid :=   Mux(should_kill || cache.io.exception =/= EXC_NORMAL , true.B, if_valid)
+  io.ifu_dec.valid :=   Mux(should_kill , true.B, if_valid)
   io.ifu_dec.bits.inst :=  Mux(should_kill || cache.io.exception =/= EXC_NORMAL, BUBBLE,if_inst)
   io.ifu_dec.bits.pc := pc_reg
   io.ifu_dec.bits.pc_valid := Mux(should_kill|| cache.io.exception =/= EXC_NORMAL, false.B, true.B)
-  io.ifu_dec.bits.exception := cache.io.exception
+  io.ifu_dec.bits.exception := cache.qio.exception
   io.icache_valid := cache.io.valid
   
 
