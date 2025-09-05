331443410eb491e09b4470280885c33d35c43101
tracer-ysyx <tracer@ysyx.org>
2025-09-05 10:33:24 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:33:24 up 24 days,  1:07,  3 users,  load average: 0.86, 0.45, 1.97

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -103,13 +103,14 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
                 }.otherwise {
                     state := sRequesting // 继续请求下一子块
                 }
-            }
-            when(io.port.resp.bits.resp =/= 0.U) {
+                when(io.port.resp.bits.resp =/= 0.U) {
                 state := sIdle
                 io.exception  := EXC_INSTR_ACCESS_FAULT
-            }.otherwise {
-                io.exception  := EXC_NORMAL
+                }.otherwise {
+                    io.exception  := EXC_NORMAL
+                }
             }
+            
         }
         is(sComplete) { 
             state   := sIdle
