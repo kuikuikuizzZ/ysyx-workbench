1ecd2f5df18fa3770b8551cb0410c998caa542ab
tracer-ysyx <tracer@ysyx.org>
2025-09-08 21:05:55 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:55 up 27 days, 11:39,  4 users,  load average: 9.42, 8.65, 6.82

--- npc-chisel/npc/src/main/scala/common/axi.scala
+++ npc-chisel/npc/src/main/scala/common/axi.scala
@@ -195,7 +195,7 @@ class ysyx_24100012_AXI4LiteMaster (implicit val conf: ysyx_24100012_Config) ext
     }
 
     // io.resp.valid := Mux(is_write ,(wstate === ws_wait_bvalid)&&(bfire) , (io.axi_io.r.valid) )
-    io.resp.valid := Mux(is_write && !is_read ,io.axi_io.b.valid , (io.axi_io.r.valid) )
+    io.resp.valid := Mux(is_write || !is_read ,io.axi_io.b.valid , (io.axi_io.r.valid) )
     io.resp.bits.resp :=  Mux(io.axi_io.r.valid, io.axi_io.r.resp ,
                           Mux(io.axi_io.b.valid , io.axi_io.b.resp, 0.U ))
 }
