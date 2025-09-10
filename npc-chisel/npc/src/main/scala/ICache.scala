126a401dd0f0856e362f0c4ea6b8e0da148dc255
tracer-ysyx <tracer@ysyx.org>
2025-09-10 21:15:16 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:15:16 up 1 day,  6:52,  3 users,  load average: 1.91, 1.83, 1.72

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -56,13 +56,13 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
     val tag = tags.read(io.pc(s_bits+b_bits+2-1,b_bits+2),(io.req_valid || ren))
     val hit = cache_valid && (io.pc(conf.xprlen-1,s_bits+b_bits+2) === tag)
     val cache_data = Wire(UInt(conf.xlen.W))
-    
+
     if (conf.ICacheBlockBits > 0){
         val group_index = io.pc(b_bits+2-1,2)
-        val cache_data = cache_block_vec(group_index)
         val cache_block_vec =  VecInit.tabulate(subBlocksPerLine) { i =>cache_block((i + 1) * conf.xlen - 1, i * conf.xlen) }
+        cache_data = cache_block_vec(group_index)
     } else {
-        val cache_data = cache_block
+        cache_data = cache_block
     }
 
     when (state === sRequesting){
