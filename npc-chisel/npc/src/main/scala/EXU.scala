6f05fe7c7b6c12bb89b4dbfc87c7b6f92f443462
tracer-ysyx <tracer@ysyx.org>
2025-09-10 18:43:39 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:39 up 1 day,  4:20,  4 users,  load average: 2.16, 2.11, 1.73

--- npc-chisel/npc/src/main/scala/EXU.scala
+++ npc-chisel/npc/src/main/scala/EXU.scala
@@ -86,9 +86,9 @@ class ysyx_24100012_EXU(implicit conf: ysyx_24100012_Config) extends Module
 
    // Branch/Jump Target Calculation
    val pc_plus4    = ( io.dec_exe.bits.pc + 4.U)(conf.xprlen-1,0)
-   val brjmp_offset                 = io.dec_exe.bits.op2_data
-   io.ifu_out.exe_brjmp_target      := io.dec_exe.bits.pc + brjmp_offset
-   io.ifu_out.exe_jump_reg_target   := adder_out
+   // val brjmp_offset                 = io.dec_exe.bits.op2_data
+   // io.ifu_out.exe_brjmp_target      := io.dec_exe.bits.pc + brjmp_offset
+   // io.ifu_out.exe_jump_reg_target   := adder_out
 
 
    when (io.ctl.pipeline_kill){
