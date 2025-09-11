b44862a4d7f0dbcbdd4d6580bd7c6d454313c348
tracer-ysyx <tracer@ysyx.org>
2025-09-11 10:21:02 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:21:02 up 1 day, 19:57,  4 users,  load average: 2.55, 2.30, 2.07

--- npc-chisel/npc/src/main/scala/Elaborate.scala
+++ npc-chisel/npc/src/main/scala/Elaborate.scala
@@ -3,7 +3,7 @@ import npc._
 
 object Elaborate extends App {
   val firtoolOptions = Array(
-    " --split-verilog",
+    "--split-verilog",
     "--lowering-options=" + List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
