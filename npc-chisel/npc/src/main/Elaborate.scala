2abbd646fdb2255e720d0141898945ebdff2a131
tracer-ysyx <tracer@ysyx.org>
2025-09-11 15:30:09 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:09 up 2 days,  1:07,  4 users,  load average: 1.90, 2.11, 2.01

--- npc-chisel/npc/src/main/Elaborate.scala
+++ npc-chisel/npc/src/main/Elaborate.scala
@@ -37,6 +37,7 @@ class ysyx_24100012 extends Module {
 
 object Elaborate extends App {
   val firtoolOptions = Array(
+    "--emit-verilog",
     "--lowering-options=" + List(
        "disallowLocalVariables",
       "disallowPackedArrays",
@@ -44,8 +45,8 @@ object Elaborate extends App {
     ).reduce(_ + "," + _),
   )
   emitVerilog(
-    new ysyx_24100012()
-    )
+    new ysyx_24100012(),
+    firtoolOptions)
 }
 
 
