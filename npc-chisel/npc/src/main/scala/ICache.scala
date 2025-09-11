7ed10f05655e53723b0826d981d6d79f82600a7e
tracer-ysyx <tracer@ysyx.org>
2025-09-11 09:41:02 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:41:02 up 1 day, 19:17,  4 users,  load average: 1.99, 1.79, 1.74

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -51,18 +51,14 @@ class ysyx_24100012_ICache(implicit val conf: ysyx_24100012_Config) extends Modu
     val tags = SyncReadMem(size,UInt(tag_bits.W)).suggestName("ysyx_24100012_icache_tags") 
     val valids = SyncReadMem(size,Bool()).suggestName("ysyx_24100012_icache_valids") 
 
+    val group_index = io.pc(b_bits+2-1,2)
     val cache_block = mem.read(io.pc(s_bits+b_bits+2-1,b_bits+2),(io.req_valid || ren))
+    val cache_block_vec =  VecInit.tabulate(subBlocksPerLine) { i =>cache_block((i + 1) * conf.xlen - 1, i * conf.xlen) }
+    val cache_data = cache_block_vec(group_index)
     val cache_valid = valids.read(io.pc(s_bits+b_bits+2-1,b_bits+2),(io.req_valid || ren))
     val tag = tags.read(io.pc(s_bits+b_bits+2-1,b_bits+2),(io.req_valid || ren))
     val hit = cache_valid && (io.pc(conf.xprlen-1,s_bits+b_bits+2) === tag)
-    val cache_data = Wire(UInt(conf.xlen.W))
-    val cache_block_vec =  VecInit.tabulate(subBlocksPerLine) { i =>cache_block((i + 1) * conf.xlen - 1, i * conf.xlen) }
-    if (conf.ICacheBlockBits > 0){
-        val group_index = io.pc(b_bits+2-1,2)
-        cache_data := cache_block_vec(group_index)
-    } else {
-        cache_data := cache_block
-    }
+        
 
     when (state === sRequesting){
         io.port.req             := DontCare
