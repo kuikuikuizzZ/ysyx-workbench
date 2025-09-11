83a69c327653e9c7440272cc0773e405b81b94e8
tracer-ysyx <tracer@ysyx.org>
2025-09-11 10:53:47 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:53:47 up 1 day, 20:30,  4 users,  load average: 5.37, 2.73, 2.15

--- npc-chisel/npc/src/main/scala/Core.scala
+++ npc-chisel/npc/src/main/scala/Core.scala
@@ -16,7 +16,7 @@ class CoreIo(implicit val conf: ysyx_24100012_Config) extends Bundle
   val slave = Flipped(new AXI4LiteIo())
 }
 
-class Core (implicit val conf: ysyx_24100012_Config)extends Module
+class Core(implicit val conf: ysyx_24100012_Config)extends Module
 {
   def pipelineConnect[T <: Data, T2 <: Data](prevOut: DecoupledIO[T],
     thisIn: DecoupledIO[T], thisOut: DecoupledIO[T2]) = {
