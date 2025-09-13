d7b51e8648dcf2b96020988cec43f6de1b878c9f
tracer-ysyx <tracer@ysyx.org>
2025-09-13 17:45:52 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:52 up 4 days,  3:22,  4 users,  load average: 8.43, 8.23, 8.12

--- npc-chisel/npc/src/main/scala/Core.scala
+++ npc-chisel/npc/src/main/scala/Core.scala
@@ -31,8 +31,8 @@ class Core(implicit val conf: Config)extends Module
       //   validReg := prevOut.valid
       // }
       // thisIn.valid := validReg
-      // thisIn.bits := RegEnable(prevOut.bits,0.U.asTypeOf(chiselTypeOf(prevOut.bits)),prevOut.valid && thisIn.ready )
-      thisIn.bits := RegEnable(prevOut.bits,prevOut.valid && thisIn.ready )
+      thisIn.bits := RegEnable(prevOut.bits,0.U.asTypeOf(chiselTypeOf(prevOut.bits)),prevOut.valid && thisIn.ready )
+      // thisIn.bits := RegEnable(prevOut.bits,prevOut.valid && thisIn.ready )
 
       thisIn.valid := (prevOut.valid && thisIn.ready)
   }
