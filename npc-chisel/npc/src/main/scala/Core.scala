e6950ea10f5a50ac58911e0dc332f0816325ae2b
tracer-ysyx <tracer@ysyx.org>
2025-09-13 22:12:24 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:24 up 4 days,  7:49,  5 users,  load average: 8.03, 7.99, 7.95

--- npc-chisel/npc/src/main/scala/Core.scala
+++ npc-chisel/npc/src/main/scala/Core.scala
@@ -22,19 +22,19 @@ class Core(implicit val conf: Config)extends Module
   def pipelineConnect[T <: Data, T2 <: Data](prevOut: DecoupledIO[T],
     thisIn: DecoupledIO[T], thisOut: DecoupledIO[T2]) = {
       prevOut.ready := thisIn.ready
-      // thisIn.bits := RegEnable(prevOut.bits,prevOut.valid && thisIn.ready )
       // thisIn.bits := RegNext(prevOut.bits)
-      thisIn.valid := (prevOut.valid && thisIn.ready)
-      val regBits = RegInit(0.U.asTypeOf(chiselTypeOf(prevOut.bits)))
-      when(prevOut.valid && thisIn.ready) {
-        regBits := prevOut.bits
-      }
-      thisIn.bits := regBits
+      // val regBits = RegInit(0.U.asTypeOf(chiselTypeOf(prevOut.bits)))
+      // when(prevOut.valid && thisIn.ready) {
+      //   regBits := prevOut.bits
+      // }
+      // thisIn.bits := regBits
       // val validReg = RegInit(false.B)
       // when(thisIn.ready) {
       //   validReg := prevOut.valid
       // }
       // thisIn.valid := validReg
+      thisIn.bits := RegEnable(prevOut.bits,0.U.asTypeOf(chiselTypeOf(prevOut.bits)),prevOut.valid && thisIn.ready )
+      thisIn.valid := (prevOut.valid && thisIn.ready)
   }
   val io = IO(new CoreIo())
 
