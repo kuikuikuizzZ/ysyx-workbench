2697c14d938622b87d08bee4ec25f27c9b56120e
tracer-ysyx <tracer@ysyx.org>
2025-09-15 16:14:32 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:32 up 6 days,  1:51,  4 users,  load average: 8.15, 8.29, 8.14

--- npc/svsrc/DPI.v
+++
@@ -1,4 +0,0 @@
-// import "DPI-C" function void pmem_read(input int outaddr,input int length, output int dout);
-// import "DPI-C" function void pmem_write(input int inaddr,input int length, input int din);
-
-
