c6f5a3b4685d9e869dc406e3013c2dc9647a9319
tracer-ysyx <tracer@ysyx.org>
2025-09-15 16:11:02 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:02 up 6 days,  1:47,  4 users,  load average: 8.21, 8.13, 8.05

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -51,14 +51,7 @@ class ICache(implicit val conf: Config) extends Module {
     val tags = SyncReadMem(size,UInt(tag_bits.W)).suggestName("icache_tags") 
     val valids = SyncReadMem(size,Bool()).suggestName("icache_valids") 
 
-    when(reset.asBool) {
-      // 默认初始化：将所有位置为0
-      for (i <- 0 until 4) {
-        mem.write(i.U, 0.U(cache_data_width.W))
-        tags.write(i.U, 0.U(tag_bits.W))
-        valids.write(i.U, false.B)
-      }
-    }
+    
 
     val group_index = io.pc(b_bits+2-1,2)
     val cache_block = mem.read(io.pc(s_bits+b_bits+2-1,b_bits+2),(io.req_valid || ren))
@@ -128,7 +121,14 @@ class ICache(implicit val conf: Config) extends Module {
     val fullCacheLine = cacheLineBuffer.asUInt
 
     // 写入缓存（仅当完成整行加载）
-    when(state === sComplete) {
+    when(reset.asBool) {
+      // 默认初始化：将所有位置为0
+      for (i <- 0 until size) {
+        mem.write(i.U, 0.U(cache_data_width.W))
+        tags.write(i.U, 0.U(tag_bits.W))
+        valids.write(i.U, false.B)
+      }
+    }.elsewhen(state === sComplete) {
         val index = io.pc(s_bits + b_bits + 2 - 1, b_bits+2)
         mem.write(index, fullCacheLine) // 写入数据
         tags.write(index, io.pc(conf.xprlen-1, s_bits + b_bits + 2)) // 写入Tag
