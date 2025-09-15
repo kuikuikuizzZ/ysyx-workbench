fcb22168ae40e2863617146cfc18eff59d6b27ad
tracer-ysyx <tracer@ysyx.org>
2025-09-15 17:21:32 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:32 up 6 days,  2:58,  4 users,  load average: 8.07, 8.30, 8.15

--- npc-chisel/npc/src/main/scala/common/memory.scala
+++ npc-chisel/npc/src/main/scala/common/memory.scala
@@ -201,8 +201,10 @@ class AXI4LiteRRArbiter(numMasters: Int)(implicit val conf: Config)  extends Mod
    io.ports(DPORT).resp.bits.resp := Mux(state === s_lsu_active,axi4lite_mem.io.resp.bits.resp,0.U)  
 
    for (i <- 0 until numMasters) {
-      io.ports(i).req.ready := state === s_idle
+      io.ports(IPORT).req.ready := state === s_idle 
+      io.ports(DPORT).req.ready := state === s_idle && !io.ports(IPORT).req.ready
    }
+
    axi4lite_mem.io.clock  := clock
    axi4lite_mem.io.reset := reset
    axi4lite_mem.io.axi_io <> io.axi_port
