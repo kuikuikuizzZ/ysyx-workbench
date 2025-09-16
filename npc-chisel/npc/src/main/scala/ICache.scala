23c44073aa78d47960227103a97f1754bcfb70e5
tracer-ysyx <tracer@ysyx.org>
2025-09-16 16:35:10 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:10 up 7 days,  2:12,  4 users,  load average: 7.85, 8.10, 8.24

--- npc-chisel/npc/src/main/scala/ICache.scala
+++ npc-chisel/npc/src/main/scala/ICache.scala
@@ -142,8 +142,8 @@ class ICache(implicit val conf: Config) extends Module {
     /////// DEBUG PORT
     val hit_cnt = RegInit(0.U(conf.perfCountBits.W))
     val miss_cnt = RegInit(0.U(conf.perfCountBits.W))
-    hit_cnt := Mux(hit && req_valid_reg,hit_cnt+1.U,hit_cnt)
-    miss_cnt := Mux(!hit && req_valid_reg,miss_cnt+1.U,miss_cnt)
+    hit_cnt := Mux(hit_reg && req_valid_reg,hit_cnt+1.U,hit_cnt)
+    miss_cnt := Mux(!hit_reg && req_valid_reg,miss_cnt+1.U,miss_cnt)
     io.debug.hit_cnt := hit_cnt
     io.debug.miss_cnt := miss_cnt
     ////// END DEBUG
