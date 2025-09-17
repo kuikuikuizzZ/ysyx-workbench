291ffcceed5caf491d3b26a8cbd5fc6daeba53c5
tracer-ysyx <tracer@ysyx.org>
2025-09-17 16:33:47 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:47 up 8 days,  2:10,  3 users,  load average: 8.07, 8.05, 8.01

--- npc/scripts/npc-chisel.mk
+++ npc/scripts/npc-chisel.mk
@@ -6,7 +6,7 @@ NAME = V$(TOP_NAME)
 
 # SV源文件
 ifdef CONFIG_SOC
-	SVSOURCES = $(wildcard $(NPC_HOME)/svsrc/*.v $(NPC_HOME)/svsrc/*.sv)
+	SVSOURCES = $(wildcard $(NPC_HOME)/build/*.v $(NPC_HOME)/build/*.sv)
 else
 	SVSOURCES = $(wildcard $(NPC_HOME)/svsrc_no_soc/*.v $(NPC_HOME)/svsrc_no_soc/*.sv)
 endif
