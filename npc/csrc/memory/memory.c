3f60ede0becf6909db91718be8864965db78f843
tracer-ysyx <tracer@ysyx.org>
2025-09-18 16:14:56 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:56 up 9 days,  1:51,  4 users,  load average: 8.07, 7.97, 8.00

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -51,7 +51,7 @@ extern "C" {
         if (in_pmem(raddr)) {
             // TODO: support mask read, 4 bytes read and npc take care of mask
             *rword = host_read(guest_to_host(raddr),4);
-            printf("pmem read: raddr = %x, data %.8x mask %.8x\n", raddr,*rword,mask);
+            // printf("pmem read: raddr = %x, data %.8x mask %.8x\n", raddr,*rword,mask);
             return;
         }
         return;
@@ -63,7 +63,7 @@ extern "C" {
             uint32_t rword = host_read(guest_to_host(waddr),4);
             rword = (rword & ~mask) | (wdata & mask);
             host_write(guest_to_host(waddr), 4,rword);
-            printf("pmem write: waddr = %x data %.8x , mask %.8x \n",waddr, wdata, mask);
+            // printf("pmem write: waddr = %x data %.8x , mask %.8x \n",waddr, wdata, mask);
         }
 
         return;
