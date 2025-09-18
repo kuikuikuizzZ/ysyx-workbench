603bd287d8f7bb9abde540b8d2fa1328fc1c23b8
tracer-ysyx <tracer@ysyx.org>
2025-09-18 20:16:11 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:11 up 9 days,  5:53,  4 users,  load average: 8.23, 7.95, 7.88

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -87,11 +87,12 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |        .mask(dw_mask),
    |        .mask_wide(dw_mask_wide)
    |    );
-   |    wire [31:0] dr_addr_aligned; 
+   |    wire [31:0] dr_addr_aligned,dr_data_raw; 
+   |    wire [1:0] dr_offset;
    |    assign dr_addr_aligned = {dr_addr[ADDR_WIDTH-1:2],2'b0} ;
    |    always @(posedge clock) begin
    |        if (dw_en) begin
-   |            pmem_mask_write(dw_addr, dw_mask_wide, dw_data);
+   |            pmem_mask_write(dr_addr_aligned, dw_mask_wide, dw_data);
    |            dw_ready = 1'b1;
    |        end else begin
    |            dw_ready = 1'b0;
@@ -101,7 +102,7 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |    always @(posedge clock) begin
    |        if (dr_en) begin
    |            // -1 -> 1111
-   |            pmem_mask_read(dr_addr, -1, dr_data);
+   |            pmem_mask_read(dr_addr, -1, dr_data_raw);
    |            dr_ready = 1'b1;
    |        end else begin
    |            dr_data = 32'b0;
@@ -110,7 +111,7 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |
    |    
    |    end
-   |
+   |    assign dr_data = dr_data_raw << (dr_offset * 8);
    |    assign dw_ready = 1'b1;
    |
    |endmodule
