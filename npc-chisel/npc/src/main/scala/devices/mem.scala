8223381af1860e707e599088d188ffb788cc8c58
tracer-ysyx <tracer@ysyx.org>
2025-09-18 11:30:48 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:30:48 up 8 days, 21:07,  4 users,  load average: 8.29, 7.77, 7.84

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -42,3 +42,102 @@ class AsyncMem(val addrWidth: Int) extends BlackBox with HasBlackBoxPath {
    addPath(path)
    println(s"AsyncMem path: ${path}")
 }
+
+
+class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with HasBlackBoxInline  {
+   val io = IO(new Bundle{
+      val dr = new AXIRport(conf.xprlen, conf.xlen)
+      val dw = new AXIWport(conf.xprlen, conf.xlen)
+      val clock = Input(Clock())
+      val reset = Input(Bool())
+   }) 
+
+   setInline("ysyx_24100012_AXI4LiteMem.v",
+   """module ysyx_24100012_AXI4LiteMem #(
+   |    ADDR_WIDTH = 32,
+   |    DATA_WIDTH = 32,
+   |    MASK_WIDTH = 4,
+   |    ORIGIN_ADDR=32'h80000000,
+   |    MEM_SIZE=32'h08000000
+   |) (
+   |    // // // 全局时钟（根据Chisel的MemIo需补充）
+   |    // input clk,
+   |    input clock,
+   |    input reset,
+   |
+   |    // 写端口（dw: Wport）
+   |    input                   dw_en,           // 写使能 (原MemWEn)
+   |    input  [ADDR_WIDTH-1:0] dw_addr,        // 写地址
+   |    input  [DATA_WIDTH-1:0] dw_data,        // 写数据
+   |    input  [MASK_WIDTH-1:0] dw_mask,         // 字节掩码 (原Length整合至mask)
+   |    // 读端口（dr: Rport)）
+   |    input                   dr_en,          // 端口使能
+   |    input  [ADDR_WIDTH-1:0] dr_addr,        // 端口0地址
+   |
+   |    output  reg [DATA_WIDTH-1:0]    dr_data,   // 端口数据
+   |    output  reg                     dr_ready,
+   |    output  reg                     dw_ready
+   |);
+   |
+   |import "DPI-C" function void pmem_mask_write(input int inaddr,input int mask, input int din);
+   |import "DPI-C" function void pmem_mask_read(input int outaddr,input int mask, output int dout);
+   |
+   |    wire [DATA_WIDTH-1:0] dw_mask_wide;
+   |    ysyx_24100012_mask_expander me (
+   |        .mask_narrow(dw_mask),
+   |        .mask_wide(dw_mask_wide)
+   |    );
+   |    always @(posedge clock) begin
+   |        if (dw_en) begin
+   |            pmem_mask_write(dw_addr, dw_mask_wide, dw_data); // mask替代Length
+   |            dw_ready = 1'b1;
+   |        end else begin
+   |            dw_ready = 1'b0;
+   |        end
+   |    end
+   |
+   |    always @(posedge clock) begin
+   |        if (dr_en) begin
+   |            // -1 -> 1111
+   |            pmem_mask_read(dr_addr, -1, dr_data);
+   |            dr_ready = 1'b1;
+   |        end else begin
+   |            dr_data = 32'b0;
+   |            dr_ready = 1'b0;
+   |        end
+   |
+   |        
+   |    end
+   |
+   |    assign dw_ready = 1'b1;
+   |
+   |endmodule
+    """.stripMargin)
+ }
+
+ class ysyx_24100012_mask_expander  extends BlackBox with HasBlackBoxInline  {
+   val io = IO(new Bundle {
+     val mask        = Input(UInt(4.W))
+     val mask_wide   = Output(UInt(32.W))
+   })
+    setInline("ysyx_24100012_mask_expander.v",
+    """module ysyx_24100012_mask_expander #(parameter DATA_WIDTH = 32) (
+   |    input [DATA_WIDTH/8-1:0] mask,
+   |    output [DATA_WIDTH-1:0] mask_wide
+   |);
+   |
+   |function [DATA_WIDTH-1:0] expand_mask;
+   |    input [DATA_WIDTH/8-1:0] mask_in;
+   |    integer i;
+   |    begin
+   |        for (i = 0; i < DATA_WIDTH/8; i = i+1) begin
+   |            expand_mask[i*8 +: 8] = {8{mask_in[i]}}; // 每个掩码位扩展为8比特
+   |        end
+   |    end
+   |endfunction
+   |
+   |assign mask_wide = expand_mask(mask); // 调用函数
+   |
+   |endmodule
+    """)
+ }
