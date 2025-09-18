c0dec2718c8ffd16b23874c941eab2cb09e0d808
tracer-ysyx <tracer@ysyx.org>
2025-09-18 20:45:59 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:59 up 9 days,  6:22,  4 users,  load average: 8.01, 8.15, 8.07

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -83,16 +83,15 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |import "DPI-C" function void pmem_mask_read(input int outaddr,input int mask, output int dout);
    |
    |    wire [DATA_WIDTH-1:0] dw_mask_wide;
-   |    wire [31:0] dr_addr_aligned,dw_addr_aligned; 
-   |    assign dr_addr_aligned = {dr_addr[ADDR_WIDTH-1:2],2'b0} ;
    |    ysyx_24100012_mask_expander me (
    |        .mask(dw_mask),
    |        .mask_wide(dw_mask_wide)
    |    );
-
+   |    wire [31:0] dr_addr_aligned; 
+   |    assign dr_addr_aligned = {dr_addr[ADDR_WIDTH-1:2],2'b0} ;
    |    always @(posedge clock) begin
    |        if (dw_en) begin
-   |            pmem_mask_write(dw_addr_aligned, dw_mask_wide, dw_data);
+   |            pmem_mask_write(dr_addr_aligned, dw_mask_wide, dw_data);
    |            dw_ready = 1'b1;
    |        end else begin
    |            dw_ready = 1'b0;
@@ -111,6 +110,7 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |
    |    
    |    end
+   |
    |    assign dw_ready = 1'b1;
    |
    |endmodule
--- npc/svsrc_no_soc/ysyxSoCFull.v
+++ npc/svsrc_no_soc/ysyxSoCFull.v
@@ -2906,7 +2906,6 @@ import "DPI-C" function void pmem_mask_read(input int outaddr,input int mask, ou
     wire [DATA_WIDTH-1:0] dw_mask_wide;
     wire [31:0] dr_addr_aligned,dw_addr_aligned; 
     assign dr_addr_aligned = {dr_addr[ADDR_WIDTH-1:2],2'b0} ;
-    assign dw_addr_aligned = {dw_addr[ADDR_WIDTH-1:2],2'b0} ;
     ysyx_24100012_mask_expander me (
         .mask(dw_mask),
         .mask_wide(dw_mask_wide)
@@ -2914,7 +2913,7 @@ import "DPI-C" function void pmem_mask_read(input int outaddr,input int mask, ou
 
     always @(posedge clock) begin
         if (dw_en) begin
-            pmem_mask_write(dr_addr_aligned, dw_mask_wide, dw_data);
+            pmem_mask_write(dw_addr_aligned, dw_mask_wide, dw_data);
             dw_ready = 1'b1;
         end else begin
             dw_ready = 1'b0;
