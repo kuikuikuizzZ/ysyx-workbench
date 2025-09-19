ffd0bdd530c0d42ef903bf3171d97a12cb131a4a
tracer-ysyx <tracer@ysyx.org>
2025-09-19 15:38:18 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:18 up 10 days,  1:15,  4 users,  load average: 8.12, 8.04, 8.00

--- npc-chisel/npc/src/main/scala/common/configuration.scala
+++ npc-chisel/npc/src/main/scala/common/configuration.scala
@@ -17,11 +17,11 @@ case class Config(
     val burstLength = 1.U   // burstLength = axlen - 1   
     val USE_FULL_BYPASSING = true
 
-    val START_ADDR = if (EnableSOC) 0x30000000L.U(32.W) else 0x80000000L.U(32.W)
     val ICacheSizeBits = sys.env.get("ICACHE_SIZE_BITS").map(_.toInt).getOrElse(2)
     val ICacheBlockBits = sys.env.get("ICACHE_BLOCK_BITS").map(_.toInt).getOrElse(1)
     val EnableBurst = sys.env.get("ICACHE_ENABLE_BURST").map(java.lang.Boolean.parseBoolean).getOrElse(true)
     val ICacheEnableBurst = if (EnableBurst) true.B else false.B
     val EnableSOC = sys.env.get("ENABLE_SOC").map(java.lang.Boolean.parseBoolean).getOrElse(true)
     val ENABLE_DEBUG =  sys.env.get("NPC_ENABLE_DEBUG").map(java.lang.Boolean.parseBoolean).getOrElse(false)
+    val START_ADDR = if (EnableSOC) 0x30000000L.U(32.W) else 0x80000000L.U(32.W)
 }
