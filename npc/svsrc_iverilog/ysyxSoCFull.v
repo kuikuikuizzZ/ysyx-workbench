c132743ed7c015e448d6af5361577163e8318a3e
tracer-ysyx <tracer@ysyx.org>
2025-09-20 12:53:14 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:53:14 up 10 days, 22:30,  4 users,  load average: 8.23, 8.14, 8.05

--- npc/svsrc_iverilog/ysyxSoCFull.v
+++ npc/svsrc_iverilog/ysyxSoCFull.v
@@ -2405,14 +2405,14 @@ module ysyx_24100012_AXI4LiteMem #(
       .mask(dw_mask),
       .mask_wide(dw_mask_wide)
   );
-  reg [7:0] mem [20000:0];
+  reg [7:0] mem [80000:0];
   initial begin
     reg [2047:0] path = 0;
     if (!$value$plusargs("image=%s", path)) begin
       path = "./iverilog_scripts/dummy-riscv32e-npc.hex";
     end
     $display("Reading image from %s", path);
-    $readmemh( "./iverilog_scripts/mem_zeros.hex", mem,0,20000);
+    $readmemh( "./iverilog_scripts/mem_zeros.hex", mem,0,80000);
     $readmemh( path, mem,0,2000);
   
   end
