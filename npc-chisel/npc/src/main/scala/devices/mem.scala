2cdedf00d80da547bf80743d0752c805db82beb9
tracer-ysyx <tracer@ysyx.org>
2025-09-21 12:31:18 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:31:18 up 11 days, 22:08,  4 users,  load average: 7.43, 7.81, 7.91

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -89,7 +89,7 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |    end
    |    $display("Reading image from %s", path);
    |    $readmemh( "./iverilog_scripts/mem_zeros.hex", mem,0,80000);
-   |    $readmemh( path, mem,0,2000);
+   |    $readmemh( path, mem,0,4000);
    |  
    |  end
    |    reg wen_reg;
--- npc/svsrc_iverilog/ysyxSoCFull.v
+++ npc/svsrc_iverilog/ysyxSoCFull.v
@@ -2332,7 +2332,7 @@ module ysyx_24100012_AXI4LiteMem #(
             mem[dw_addr_aligned] <= wdata_masked[7:0];
             wen_reg <= 1'b0;
             dw_ready = 1'b0;
-            $display("write %x to %x mask %x,read %x",(wdata_reg & wmask_wide_reg) | (rdata_reg & ~wmask_wide_reg),dw_addr_aligned,wmask_wide_reg,rdata_reg);
+            //$display("write %x to %x mask %x,read %x",(wdata_reg & wmask_wide_reg) | (rdata_reg & ~wmask_wide_reg),dw_addr_aligned,wmask_wide_reg,rdata_reg);
         end else begin
             dw_ready = 1'b0;
         end
@@ -2342,7 +2342,7 @@ module ysyx_24100012_AXI4LiteMem #(
         if (dr_en) begin
             dr_data = {mem[dr_addr_aligned+3],mem[dr_addr_aligned+2],mem[dr_addr_aligned+1],mem[dr_addr_aligned]};
             dr_ready = 1'b1;
-            $display("read %x from %x",dr_data,dr_addr_aligned);
+            //$display("read %x from %x",dr_data,dr_addr_aligned);
         end else begin
             dr_data = 32'b0;
             dr_ready = 1'b0;
