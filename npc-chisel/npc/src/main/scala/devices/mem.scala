e2bcbb21359a5f5ee3bb7facab8b91f9114a2b84
tracer-ysyx <tracer@ysyx.org>
2025-09-21 15:27:27 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:27:27 up 12 days,  1:04,  4 users,  load average: 7.88, 8.12, 8.42

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -126,10 +126,11 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |     if (dr_en && dr_addr >= 32'h80000000 && dr_addr < 32'h90000000) begin
    |            dr_data = {mem[dr_addr_aligned+3],mem[dr_addr_aligned+2],mem[dr_addr_aligned+1],mem[dr_addr_aligned]};
    |            dr_ready = 1'b1;
-   |            //$display("read %x from %x",dr_data,dr_addr_aligned);
+   |            
    |     end else if (dr_en && (dr_addr == 32'ha0000048 || dr_addr == 32'ha000004c)) begin
    |         current_time = $time;
    |         dr_data = (dr_addr == 32'ha0000048)? current_time[31:0]:current_time[63:32];
+   |         $display("read %x from %x",dr_data,dr_addr_aligned);
    |     end else begin 
    |         dr_data = 32'b0;
    |         dr_ready = 1'b0;
