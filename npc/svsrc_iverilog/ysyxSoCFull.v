24633813670a9be34696ba06b591ac8005c94d5d
tracer-ysyx <tracer@ysyx.org>
2025-09-21 15:14:19 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:14:19 up 12 days, 51 min,  4 users,  load average: 9.32, 9.02, 8.93

--- npc/svsrc_iverilog/ysyxSoCFull.v
+++ npc/svsrc_iverilog/ysyxSoCFull.v
@@ -2342,7 +2342,7 @@ module ysyx_24100012_AXI4LiteMem #(
         end
     end
      
-  always @(posedge clock)
+  always @(posedge clock) begin
      if (dr_en && dr_addr >= 32'h80000000 && dr_addr < 32'h90000000) begin
             dr_data = {mem[dr_addr_aligned+3],mem[dr_addr_aligned+2],mem[dr_addr_aligned+1],mem[dr_addr_aligned]};
             dr_ready = 1'b1;
