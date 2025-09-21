2c78dc7f37f509ee1945cb9a786980f2290fb397
tracer-ysyx <tracer@ysyx.org>
2025-09-21 15:27:01 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:27:01 up 12 days,  1:03,  4 users,  load average: 8.43, 8.25, 8.47

--- npc/svsrc_iverilog/ysyxSoCFull.v
+++ npc/svsrc_iverilog/ysyxSoCFull.v
@@ -2302,7 +2302,7 @@ module ysyx_24100012_AXI4LiteMem #(
   );
   reg [7:0] mem [MEM_SIZE:0];
   reg [2047:0] path = 0;
-  wire [63:0] current_time;
+  reg [63:0] current_time;
   initial begin
     if (!$value$plusargs("image=%s", path)) begin
       path = "./iverilog_scripts/dummy-riscv32e-npc.hex";
@@ -2348,7 +2348,7 @@ module ysyx_24100012_AXI4LiteMem #(
             //$display("read %x from %x",dr_data,dr_addr_aligned);
      end else if (dr_en && (dr_addr == 32'ha0000048 || dr_addr == 32'ha000004c)) begin
          current_time = $time;
-         dr_data = (dr_addr == 32'ha0000048) current_time[31:0]:current_time[63:32];
+         dr_data = (dr_addr == 32'ha0000048)? current_time[31:0]:current_time[63:32];
      end else begin 
          dr_data = 32'b0;
          dr_ready = 1'b0;
