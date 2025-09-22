084a494a79897319715ef0d76a756b3bc2ae98d1
tracer-ysyx <tracer@ysyx.org>
2025-09-22 22:32:04 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:32:04 up 13 days,  8:08,  4 users,  load average: 8.12, 7.98, 8.01

--- npc-chisel/npc/src/main/scala/Core.scala
+++ npc-chisel/npc/src/main/scala/Core.scala
@@ -44,7 +44,7 @@ class Core(implicit val conf: Config)extends Module
       // thisIn.valid := validReg
       // thisIn.bits := RegEnable(prevOut.bits,prevOut.valid && thisIn.ready )
       thisIn.bits := RegEnable(prevOut.bits,0.U.asTypeOf(chiselTypeOf(prevOut.bits)),prevOut.valid && thisIn.ready )
-      thisIn.valid := (prevOut.valid && thisIn.ready)
+      thisIn.valid := RegNext(prevOut.valid && thisIn.ready,false.B)
   }
   val io = IO(new CoreIo())
 
