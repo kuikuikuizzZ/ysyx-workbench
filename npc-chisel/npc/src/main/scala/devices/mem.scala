94eadd53a819931ee5f65f36fabb32949607d011
tracer-ysyx <tracer@ysyx.org>
2025-09-22 16:02:43 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:02:43 up 13 days,  1:39,  4 users,  load average: 9.31, 8.87, 8.41

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -89,7 +89,7 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |      path = "./iverilog_scripts/dummy-riscv32e-npc.hex";
    |    end
    |    $display("Reading image from %s", path);
-   |    $readmemh( path, mem,0,200000);
+   |    $readmemh( path, mem,0,4000000);
    |  end
    |    reg wen_reg;
    |    reg [31:0] wdata_reg,wmask_wide_reg,rdata_reg;
