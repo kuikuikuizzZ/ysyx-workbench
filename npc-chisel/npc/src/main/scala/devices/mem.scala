072965cc15f10dbe1fa12aca0afbcbd2eeb94c88
tracer-ysyx <tracer@ysyx.org>
2025-09-22 10:19:36 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:19:36 up 12 days, 19:56,  4 users,  load average: 8.35, 8.25, 8.10

--- npc-chisel/npc/src/main/scala/devices/mem.scala
+++ npc-chisel/npc/src/main/scala/devices/mem.scala
@@ -98,7 +98,7 @@ class ysyx_24100012_AXI4LiteMem(implicit val conf: Config) extends BlackBox with
    |    wire [31:0] wdata_masked = (wdata_reg & wmask_wide_reg) | (rdata_reg & ~wmask_wide_reg);
    |    assign dr_addr_aligned = {dr_addr[ADDR_WIDTH-1:2],2'b0}-32'h80000000 ;
    |    always @(posedge clock) begin
-   |        if (dw_en && dw_addr ==  32'ha00003f8) begin
+   |        if (dw_en && dw_addr ==  32'ha00003f8 && !dw_ready) begin
    |            $write("%c",dw_data[7:0]);
    |            dw_ready = 1'b1;
    |        end else if (dw_en && dw_addr >= 32'h80000000 && dw_addr < 32'h90000000) begin
