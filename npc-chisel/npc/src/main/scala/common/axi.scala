54699a2c6c3f70fd06e5f912fe3c85731b6a5b29
tracer-ysyx <tracer@ysyx.org>
2025-09-22 23:37:31 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:37:31 up 13 days,  9:14,  4 users,  load average: 7.74, 7.91, 7.96

--- npc-chisel/npc/src/main/scala/common/axi.scala
+++ npc-chisel/npc/src/main/scala/common/axi.scala
@@ -318,7 +318,7 @@ class AXI4BurstSlave (implicit val conf: Config) extends Module {
         }
         
         is(s_read_data) {
-            when(io.out.dr.ready && io.axi_io.r.ready) {
+            when(io.out.dr.ready ) {
                 burst_counter := burst_counter + 1.U
                 burst_addr := next_burst_addr(burst_addr, burst_size, burst_type)
                 
@@ -346,7 +346,7 @@ class AXI4BurstSlave (implicit val conf: Config) extends Module {
         }
         
         is(s_write_resp) {
-            when(io.axi_io.b.ready) {
+            when(io.out.dw.ready) {
                 state := s_idle
             }
         }
