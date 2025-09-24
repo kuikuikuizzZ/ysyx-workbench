9d3b75777073d041aba92eff3d982c2b6ebcd10c
tracer-ysyx <tracer@ysyx.org>
2025-09-24 21:53:37 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:53:37 up 15 days,  7:30,  3 users,  load average: 7.94, 7.87, 7.96

--- npc/scripts/npc-chisel.mk
+++ npc/scripts/npc-chisel.mk
@@ -19,15 +19,16 @@ NAME = V$(TOP_NAME)
 
 
 # SV源文件
-ifdef CONFIG_IVERILOG
-	SVSOURCES = $(wildcard $(NPC_HOME)/svsrc_iverilog/*.v $(NPC_HOME)/svsrc_iverilog/*.sv)
-else 
-	ifdef CONFIG_SOC
-		SVSOURCES = $(wildcard $(NPC_HOME)/build/*.v $(NPC_HOME)/build/*.sv)
-	else
-		SVSOURCES = $(wildcard $(NPC_HOME)/svsrc_no_soc/*.v $(NPC_HOME)/svsrc_no_soc/*.sv)
-	endif
-endif
+SVSOURCES = $(wildcard $(NPC_HOME)/build/*.v $(NPC_HOME)/build/*.sv)
+# ifdef CONFIG_IVERILOG
+# 	SVSOURCES = $(wildcard $(NPC_HOME)/svsrc_iverilog/*.v $(NPC_HOME)/svsrc_iverilog/*.sv)
+# else 
+# 	ifdef CONFIG_SOC
+# 		SVSOURCES = $(wildcard $(NPC_HOME)/build/*.v $(NPC_HOME)/build/*.sv)
+# 	else
+# 		SVSOURCES = $(wildcard $(NPC_HOME)/svsrc_no_soc/*.v $(NPC_HOME)/svsrc_no_soc/*.sv)
+# 	endif
+# endif
 
 
 BINARY = $(BUILD_DIR)/$(NAME)
