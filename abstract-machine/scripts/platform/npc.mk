4b753f2feb7016b2ab06babc4a3baf33336b9dce
tracer-ysyx <tracer@ysyx.org>
2025-09-25 14:03:00 +0800

    > generate verilog ysyx_24100012 吴文辉 Linux uenui-MS-7E07 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:00 up 15 days, 23:39,  4 users,  load average: 8.07, 8.15, 8.06

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -34,7 +34,7 @@ image: image-dep
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 	sed -i -E 's/pc_reg[[:space:]]*<=[[:space:]]*32'\''h30000000/pc_reg <= 32'\''h80000000/g' $(NPC_HOME)/build/ysyx_24100012.v
-# 	$(MAKE) -C $(NPC_HOME) $(ARCH)_defconfig
+	$(MAKE) -C $(NPC_HOME) $(ARCH)_defconfig
 
 run: insert-arg
 	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
